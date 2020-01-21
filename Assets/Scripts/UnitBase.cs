3ec685c53093aefeca4b72cf93bf728f45d5f2c4
Jack Ruch <ruch.jack@yahoo.com>
2020-01-21 16:38:39 -0500

    Added feature from battle meeting

--- Assets/Scripts/UnitBase.cs
+++ Assets/Scripts/UnitBase.cs
@@ -5,16 +5,17 @@ using UnityEngine.AI;
 
 public class UnitBase : MonoBehaviour
 {
+    
 
     NavMeshAgent agent;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
