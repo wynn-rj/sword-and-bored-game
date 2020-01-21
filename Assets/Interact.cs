64d7bdb3d1b40fd5d51213edb6694e6b128ef6a5
ThBrokenToaster <ruch.jack@yahoo.com>
2020-01-21 14:30:31 -0500

    Added Simple Grid Movement Mechanics
    Contains mostly temporary assets

---
+++ Assets/Interact.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public GameObject indicator;
+    public LayerMask lm;
+
+    public UnitBase activePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, lm))
+        {
+            Vector3 spot = hit.collider.GetComponent<Tile>().GetPos();
+            Debug.Log(hit.collider.name);
+            indicator.transform.position = spot;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                activePlayer.SetDestination(spot);
+            }
+        }
+    }
+}
