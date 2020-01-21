8728a449056137f29947df6359664f6c33c3519b
Jack Ruch <ruch.jack@yahoo.com>
2020-01-21 16:38:39 -0500

    Added feature from battle meeting

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    UnitBase activePlayer;
+
+
+    public UnitBase GetActivePlayer()
+    {
+        return activePlayer;
+    }
+
+
+}
