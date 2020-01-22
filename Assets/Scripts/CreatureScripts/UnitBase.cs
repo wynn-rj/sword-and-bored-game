b284422a2ef51ba6a61a157cd164084cddc113aa
ThBrokenToaster <ruch.jack@yahoo.com>
2020-01-21 20:02:12 -0500

    Addded the beginnings of the modular ability system

---
+++ Assets/Scripts/CreatureScripts/UnitBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitBase : MonoBehaviour
+{
+    
+
+    NavMeshAgent agent;
+
+    
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent)
+        {
+            Debug.Log("Yeet Lum");
+        }
+    }
+    
+
+    public void MoveTo(Vector3 pos)
+    {
+        agent.destination = pos;
+    }
+
+    public int Roll(int dice)
+    {
+        return Random.Range(1, dice);
+    }
+
+    public int Roll()
+    {
+        return Roll(20);
+    }
+}
