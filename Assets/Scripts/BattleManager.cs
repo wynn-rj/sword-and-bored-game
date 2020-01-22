34960ecc4b4b7527991328fea2f7a8fc9ca733f9
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-01-22 12:59:53 -0500

    Create new Scene for Manager Testing

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,10 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class BattleManager : MonoBehaviour
+[Serializable]
+public class BattleManager
 {
-    public TurnManager turnManager;
+    private readonly TurnController _turnController;
+    public GameObject ActivePlayer{ get {return _activePlayer;}}
+
+    [SerializeField]
+    private GameObject _activePlayer;
+
+    public BattleManager(GameObject[] entities)
+    {
+        _turnController = new TurnController(entities, new RandomShuffler<GameObject>());
+    }
+
+    public void InitializeBattle()
+    {
+        this.setNextPlayer();
+    }
+
+    public void EndCurrentTurn()
+    {
+        this.setNextPlayer();
+    }
+
+    private void setNextPlayer()
+    {
+        _activePlayer = _turnController.nextEntity();
+    }
+
 
 
 }
