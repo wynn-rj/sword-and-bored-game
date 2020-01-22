f4776b559ad8c588912ccc45acc4bfe9a655caea
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-01-22 13:01:03 -0500

    Add rudimentary turn controller

---
+++ Assets/Scripts/TurnController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TurnController
+{
+    private readonly GameObject[] _entities;
+    int pointer = 0;
+
+    public TurnController(GameObject[] entities)
+    {
+        _entities = entities;
+    }
+
+    public TurnController(GameObject[] entities, IShuffler<GameObject> shuffler)
+    : this(shuffler.shuffle(entities)) { }
+
+    public GameObject nextEntity()
+    {
+        if(_entities.Length <= 0)
+        {
+            return null;
+        }
+        GameObject nextEntity = _entities[pointer];
+        pointer += 1;
+        pointer %= _entities.Length;
+
+        return nextEntity;
+    }
+
+}
