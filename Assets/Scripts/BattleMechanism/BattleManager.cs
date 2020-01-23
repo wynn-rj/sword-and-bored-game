abbc3adb5a0abd945085830a958924b4741e0e8f
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-01-22 22:50:06 -0500

    Add battle state manager and prelimary states

---
+++ Assets/Scripts/BattleMechanism/BattleManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using SwordAndBored.TurnMechanism;
+
+namespace SwordAndBored.BattleMechanism
+{
+    public class BattleManager : MonoBehaviour
+    {
+        public static BattleManager Instance { get; private set; }
+        public TurnOrderController TurnOrderController;
+        public GameObject[] Entities = new GameObject[2];
+
+        public BattleManagerState State;
+
+        public GameObject ActivePlayer;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Start()
+        {
+            State = new BattleManagerInitializeState(this, Entities);
+        }
+
+    }
+}
