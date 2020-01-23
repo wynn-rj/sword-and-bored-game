504c7bd394818d5e81fc701591285de55507644f
gilliland177 <gilliland.177@osu.edu>
2020-01-22 23:21:03 -0500

    Adds options, credits, and confirm exit menus Creates additional canvases and a confirmation to exit the game

---
+++ Assets/Scripts/MenuFunctions/CreditsMenuFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenuFunctions : MonoBehaviour
+{
+    public GameObject mainMenuCanvas;
+    public void MainMenuButtonPressed()
+    {
+        mainMenuCanvas.SetActive(true);
+        gameObject.SetActive(false);        
+    }
+}
---
+++ Assets/Scripts/MenuFunctions/MainMenuFunctions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    public GameObject optionsCanvas, creditsCanvas;
+    public GameObject confirmQuitPanel;
+    public void StartButtonPressed()
+    {       
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitButtonPressed()
+    {
+        confirmQuitPanel.SetActive(true);
+    }
+
+    public void OptionsButtonPressed()
+    {
+        optionsCanvas.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void CreditsButtonPressed()
+    {
+        creditsCanvas.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void CancelQuitPressed()
+    {
+        confirmQuitPanel.SetActive(false);
+    }
+
+
+    public void ConfirmQuitPressed()
+    {
+        Debug.Log("Pressed Confirm Quit");
+        Application.Quit();
+    }
+
+
+}
