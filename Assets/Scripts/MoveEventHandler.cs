77f5c5000c46104d1578d5a4553a916316b4ee26
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-01-22 22:51:36 -0500

    add rudimentary move event handler

---
+++ Assets/Scripts/MoveEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEventHandler : MonoBehaviour
+{
+    public delegate void MoveEventAction(Vector3 pos);
+    public event MoveEventAction MoveCallbackEvent = delegate { };
+
+    [SerializeField]
+    private GameObject tileIndictor;
+
+    [SerializeField]
+    private LayerMask lm;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, lm))
+        {
+            Vector3 spot = hit.collider.GetComponent<Tile>().GetPos();
+            tileIndictor.transform.position = spot;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                MoveCallbackEvent(spot);
+                //activePlayer.MoveTo(spot);
+            }
+        }
+    }
+}
