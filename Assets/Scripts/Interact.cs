b18acca66e6937dc267e984b768a70e74614f7fc
Jack Ruch <ruch.jack@yahoo.com>
2020-01-26 15:21:16 -0500

    Added more of the combat system

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -2,30 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interact : MonoBehaviour
+
+namespace SwordAndBored.BattleMechanism
 {
-    public GameObject tileIndictor;
-    public LayerMask lm;
-
-    public CreatureBase activePlayer;
-    
-    void Start()
+    public class Interact : MonoBehaviour
     {
-        
-    }
+        public GameObject tileIndictor;
+        public LayerMask tileMapLayerMask;
+        public LayerMask selectingCreaturesLayerMask;
 
-    
-    void Update()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, lm))
+        public UniqueCreature activePlayer;
+
+        public CycleTurnManager turnManager;
+        int abilityInUse = 0;
+
+        bool moving = true;
+
+        private KeyCode[] keyCodes = {
+         KeyCode.Alpha1,
+         KeyCode.Alpha2,
+         KeyCode.Alpha3,
+         KeyCode.Alpha4,
+         KeyCode.Alpha5,
+         KeyCode.Alpha6,
+         KeyCode.Alpha7,
+         KeyCode.Alpha8,
+         KeyCode.Alpha9,
+        };
+
+        void Update()
         {
-            Vector3 spot = hit.collider.GetComponent<Tile>().GetPos();
-            tileIndictor.transform.position = spot;
-            if (Input.GetButtonDown("Fire1"))
+            activePlayer = turnManager.activePlayer;
+
+            if (moving)
             {
-                activePlayer.MoveTo(spot);
+                movePlayer();
+            } else
+            {
+                useAbility(abilityInUse);
+            }
+            
+
+
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                turnManager.nextTurn();
+            }
+
+        }
+
+
+        void movePlayer()
+        {
+
+            for (int i = 0; i < keyCodes.Length; i++)
+            {
+                if (Input.GetKeyDown(keyCodes[i]))
+                {
+                    abilityInUse = i + 1;
+                    Debug.Log(i + 1);
+                    moving = false;
+                }
+            }
+
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, tileMapLayerMask))
+            {
+                Tile currentTile = hit.collider.GetComponent<Tile>();
+                Vector3 spot = currentTile.GetPos();
+                tileIndictor.transform.position = spot;
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    activePlayer.MoveTo(spot);
+                    activePlayer.SetTile(currentTile);
+                }
+            }
+        }
+
+        void useAbility(int abilityNum)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, selectingCreaturesLayerMask))
+            {
+
+                GameObject target = hit.collider.gameObject;
+
+                
             }
         }
     }
---
+++ Assets/Scripts/TurnMechanism/CycleTurnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordAndBored.TurnMechanism;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SwordAndBored.BattleMechanism
+{
+    public class CycleTurnManager : MonoBehaviour
+    {
+        public GameObject[] units;
+        TurnOrderController manager;
+        public UniqueCreature activePlayer;
+        public TextMeshProUGUI text;
+
+        void Start()
+        {
+            manager = new TurnOrderController(units, new RandomShuffler<GameObject>());
+            activePlayer = manager.NextEntity().GetComponent<UniqueCreature>();
+            text.text = "Current Player: " + activePlayer.gameObject.name;
+        }
+
+        public void nextTurn()
+        {
+            activePlayer = manager.NextEntity().GetComponent<UniqueCreature>();
+            text.text = "Current Player: " + activePlayer.gameObject.name;
+        }
+    }
+}
