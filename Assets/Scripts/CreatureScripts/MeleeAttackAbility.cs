98d0a7e6e4898d6fe8f7c68aa06cd09e94afbd10
Jack Ruch <ruch.jack@yahoo.com>
2020-01-27 19:22:42 -0500

    Fixed Attack Animation

--- Assets/Scripts/CreatureScripts/MeleeAttackAbility.cs
+++ Assets/Scripts/CreatureScripts/MeleeAttackAbility.cs
@@ -10,24 +10,15 @@ public class MeleeAttackAbility : Ability
     int hitMod;
     public int range;
     UniqueCreature user;
-    Animator anim;
 
     public override void Initialize(GameObject obj)
     {
         user = obj.GetComponent<UniqueCreature>();
         hitMod = user.hitMod;
-        if (user.anim)
-        {
-            anim = user.anim;
-        }
     }
 
     public override void TriggerAbility(GameObject target)
     {
-        if (anim)
-        {
-            anim.SetTrigger("Attack");
-        }
         UniqueCreature enemy = target.GetComponent<UniqueCreature>();
         int toHit = Roll() + hitMod;
         if (toHit > enemy.AC)
