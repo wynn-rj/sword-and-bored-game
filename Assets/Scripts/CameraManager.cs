38a2b1c065413362d16b897d3fc330f55cb8c38a
gdiago <gonzalodiago@gmail.com>
2020-01-27 20:09:27 -0500

    Adds camera switching between players in battlefield view

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using Cinemachine;
+using SwordAndBored.BattleMechanism;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public CycleTurnManager turnManager;
+    public GameObject[] cameras = new GameObject[5];
+    private int priority = 1;
+
+    void Start()
+    {
+        for(int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 0;
+        }
+    }
+
+    
+    void Update()
+    {
+        SetPriority();
+    }
+
+    private void SetPriority()
+    {
+        if (turnManager.activePlayer.gameObject.transform.GetChild(3).GetComponent<CinemachineVirtualCamera>().Priority < priority)
+        {
+            turnManager.activePlayer.gameObject.transform.GetChild(3).GetComponent<CinemachineVirtualCamera>().Priority = priority + 1;
+        }
+        priority++;
+    }
+}
