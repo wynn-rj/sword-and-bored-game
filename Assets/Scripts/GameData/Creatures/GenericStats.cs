30dc72a1a93dd3285eac6936d8874b750056b408
Robert J Wynn <wynn.134@osu.edu>
2020-02-02 17:48:35 -0500

    Add simple stats class

---
+++ Assets/Scripts/GameData/Creatures/GenericStats.cs
@@ -0,0 +1,15 @@
+﻿namespace SwordAndBored.GameData.Creatures
+{
+    class GenericStats : IStats
+    {
+        public int Health { get; set; }
+        public int Speed { get; set; }
+        public int PhysicalAttack { get; set; }
+        public int PhysicalDefense { get; set; }
+        public int MagicalAttack { get; set; }
+        public int MagicalDefense { get; set; }
+        public int Movement { get; set; }
+        public float BaseAccuracy { get; set; }
+        public float BaseEvasion { get; set; }
+    }
+}
