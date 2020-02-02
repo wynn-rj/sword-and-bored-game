cafe2f7d9e4ddfc9bd24fce2f9e1891889ea40f8
Robert J Wynn <wynn.134@osu.edu>
2020-02-02 17:48:22 -0500

    Add stats interface

---
+++ Assets/Scripts/GameData/Creatures/IStats.cs
@@ -0,0 +1,18 @@
+﻿namespace SwordAndBored.GameData.Creatures
+{
+    /// <summary>
+    /// A collection of stats for a creature or similar object
+    /// </summary>
+    interface IStats
+    {
+        int Health { get; set; }
+        int Speed { get; set; }
+        int PhysicalAttack { get; set; }
+        int PhysicalDefense { get; set; }
+        int MagicalAttack { get; set; }
+        int MagicalDefense { get; set; }
+        int Movement { get; set; }
+        float BaseAccuracy { get; set; }
+        float BaseEvasion { get; set; }
+    }
+}
