3ceec1e131e8ad78cba2a02b88982fe44c60c139
Robert J Wynn <wynn.134@osu.edu>
2020-02-02 17:47:32 -0500

    Add equipment interface

---
+++ Assets/Scripts/GameData/Equipment/IEquipment.cs
@@ -0,0 +1,18 @@
+﻿using SwordAndBored.GameData.Abilities;
+using SwordAndBored.GameData.Modifiers;
+
+namespace SwordAndBored.GameData.Equipment
+{
+    interface IEquipment : IDescriptable
+    {
+        /// <summary>
+        /// The abilities granted by this equipment
+        /// </summary>
+        IAbility[] Abilities { get; }
+
+        /// <summary>
+        /// The modifiers granted by this equipment
+        /// </summary>
+        IModifier[] Modifiers { get; }
+    }
+}
