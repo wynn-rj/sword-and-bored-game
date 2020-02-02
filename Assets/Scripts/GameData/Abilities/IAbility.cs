2e3eb14939af7095353371306847038fbe4a6d48
Robert J Wynn <wynn.134@osu.edu>
2020-02-02 17:43:53 -0500

    Add ability interface

---
+++ Assets/Scripts/GameData/Abilities/IAbility.cs
@@ -0,0 +1,10 @@
+﻿namespace SwordAndBored.GameData.Abilities
+{
+    interface IAbility : IDescriptable
+    {
+        /// <summary>
+        /// The name of the image used to represent this ability
+        /// </summary>
+        string ImageName { get; }
+    }
+}
