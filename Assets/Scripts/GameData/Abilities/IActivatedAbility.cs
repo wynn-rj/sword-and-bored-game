9033f709acde43eb3fffeaddb1bdca2a9b5c76df
Robert J Wynn <wynn.134@osu.edu>
2020-02-02 17:44:53 -0500

    Add activated and passive abilities

---
+++ Assets/Scripts/GameData/Abilities/IActivatedAbility.cs
@@ -0,0 +1,20 @@
+﻿using SwordAndBored.GameData.Modifiers;
+
+namespace SwordAndBored.GameData.Abilities
+{
+    /// <summary>
+    /// An ability that has to be activated to apply a benefit to the user
+    /// 
+    /// Examples: A weapon attack or Hiding 
+    /// </summary>
+    interface IActivatedAbility : IAbility
+    {
+
+        /// <summary>
+        /// A list of modifiers that are applied to the object that has this ability when it is active
+        /// </summary>
+        IModifier[] ActiveModifiers { get; }
+
+        bool IsActive { get; set; }
+    }
+}
