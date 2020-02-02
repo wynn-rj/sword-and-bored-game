2d3ea8c48547d33d441ec949ae1b9f792a72911b
Robert J Wynn <wynn.134@osu.edu>
2020-02-02 17:46:32 -0500

    Add skill tree interface

---
+++ Assets/Scripts/GameData/Abilities/Skills/ISkillTier.cs
@@ -0,0 +1,12 @@
+﻿namespace SwordAndBored.GameData.Abilities.Skills
+{
+    /// <summary>
+    /// A collection of abilities to choose from, where only one or none can be selected at a time
+    /// </summary>
+    interface ISkillTier
+    {
+        IAbility[] Choices { get; }
+
+        IAbility SelectedSkill { get; set; }
+    }
+}
