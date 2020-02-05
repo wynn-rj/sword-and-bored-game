e5dccceebdcfef2c3d746c20868b3ea6d643d598
Robert J Wynn <wynn.134@osu.edu>
2020-02-05 14:58:43 -0500

    Move image to new interface

--- Assets/Scripts/GameData/Abilities/IAbility.cs
+++ Assets/Scripts/GameData/Abilities/IAbility.cs
@@ -1,10 +1,6 @@
 ﻿namespace SwordAndBored.GameData.Abilities
 {
-    interface IAbility : IDescriptable
-    {
-        /// <summary>
-        /// The name of the image used to represent this ability
-        /// </summary>
-        string ImageName { get; }
+    interface IAbility : IDescriptable, IImageRepresentable
+    {        
     }
 }
