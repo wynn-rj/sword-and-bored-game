44b2211672c277df1137a59464159f2e2ffd004a
Toeecko <rorygilliland@ameritech.net>
2020-02-05 10:12:27 -0500

    established basic db connection, even through builds

---
+++ Assets/Scripts/GameData/Database/TestConnection.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using TMPro;
+
+namespace SwordAndBored.GameData.Database { 
+    public class TestConnection : MonoBehaviour
+    {
+        public TMP_Text text;
+        // Start is called before the first frame update
+        void Start()
+        {
+            string conn = "URI=file:" + Application.dataPath + "/TestConn.db"; //Path to database.
+            IDbConnection dbconn;
+            dbconn = (IDbConnection) new SqliteConnection(conn);
+            dbconn.Open(); //Open connection to the database.
+            IDbCommand dbcmd = dbconn.CreateCommand();
+            string sqlQuery = "SELECT * FROM Test1";
+            dbcmd.CommandText = sqlQuery;
+            IDataReader reader = dbcmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int value = reader.GetInt32(0);
+                Debug.Log("value= " + value);
+                text.SetText("value= " + value);
+            }
+            reader.Close();
+            dbcmd.Dispose();
+            dbconn.Close();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                string conn = "URI=file:" + Application.dataPath + "/TestConn.db"; //Path to database.
+                IDbConnection dbconn;
+                dbconn = (IDbConnection)new SqliteConnection(conn);
+                dbconn.Open(); //Open connection to the database.
+                IDbCommand dbcmd = dbconn.CreateCommand();
+                string sqlQuery = "SELECT * FROM Test1";
+                dbcmd.CommandText = sqlQuery;
+                IDataReader reader = dbcmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int value = reader.GetInt32(0);
+                    Debug.Log("value= " + value);
+                    text.SetText("value= " + value);
+                }
+                reader.Close();
+                dbcmd.Dispose();
+                dbconn.Close();
+            }
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                string conn = "URI=file:" + Application.dataPath + "/TestConn.db"; //Path to database.
+                IDbConnection dbconn;
+                dbconn = (IDbConnection)new SqliteConnection(conn);
+                dbconn.Open(); //Open connection to the database.
+                IDbCommand dbcmd = dbconn.CreateCommand();
+                string sqlQuery = "INSERT INTO TEST1 VALUES (3)";
+                dbcmd.CommandText = sqlQuery;
+                dbcmd.ExecuteScalar();
+                dbcmd.Dispose();
+                dbconn.Close();
+            }
+        }
+    }
+}
