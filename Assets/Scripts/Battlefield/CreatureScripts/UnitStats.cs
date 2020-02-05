e856750b3c3a9b5629dc2f5da0b3e8612a5e3ae1
Jack Ruch <ruch.jack@yahoo.com>
2020-02-05 14:17:04 -0500

    Refactored unit scripts
    split the functionality of each unit into multiple scripts

---
+++ Assets/Scripts/Battlefield/CreatureScripts/UnitStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+
+    public int health;
+    public int attack;
+    public int specialAttack;
+    public int defense;
+    public int specialDefense;
+    public int speedIntit;
+    public int movement;
+
+}
