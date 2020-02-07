efba380c70909d2eaddfdd47fb0d23da08403db4
cthomas68 <c.l.thomas68@gmail.com>
2020-02-07 13:41:28 -0500

    Set up initial base class inheritance

---
+++ Assets/Scripts/Strategy/BaseManagement/AbstractProductionBuilding.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractProductionBuilding : AbstractBuilding
+{
+    public AbstractProductionBuilding(Vector3 position) : base(position) { }
+}
