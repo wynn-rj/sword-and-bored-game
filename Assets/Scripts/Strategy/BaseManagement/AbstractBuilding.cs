ab44ee918dc592af687831df2b4ebcec85baef48
cthomas68 <c.l.thomas68@gmail.com>
2020-02-07 13:41:28 -0500

    Set up initial base class inheritance

---
+++ Assets/Scripts/Strategy/BaseManagement/AbstractBuilding.cs
@@ -0,0 +1,18 @@
+﻿using SwordAndBored.GameData;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractBuilding : IBuilding, IModelable, IDescriptable
+{
+    public float BuildTime { get; set; }
+
+    public string ModelName { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public AbstractBuilding(Vector3 position) { }
+
+}
---
+++ Assets/Scripts/Strategy/BaseManagement/Barracks.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barracks : AbstractProductionBuilding
+{
+    public Barracks(Vector3 position) : base(position) { }
+}
