b6854b1d70e458174313aae029953b00ea9df172
cthomas68 <c.l.thomas68@gmail.com>
2020-02-07 13:41:28 -0500

    Set up initial base class inheritance

---
+++ Assets/Scripts/Strategy/BaseManagement/BuildingFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingFactory
+{
+    private static BuildingFactory instance;
+
+    private BuildingFactory() { }
+
+    public static BuildingFactory Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new BuildingFactory();
+            }
+
+            return instance;
+        }
+    }
+
+    public IBuilding CreateBarracks(Vector3 position)
+    {
+        return new Barracks(position);
+    }
+
+    public IBuilding CreateGranary(Vector3 position)
+    {
+        return null;
+    }
+}
