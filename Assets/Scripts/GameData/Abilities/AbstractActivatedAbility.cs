d43661f197eb1d338de0e495cd578c30864f533c
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 15:07:54 -0500

    Create an abstract passive and activated ability

---
+++ Assets/Scripts/GameData/Abilities/AbstractActivatedAbility.cs
@@ -0,0 +1,11 @@
+﻿using SwordAndBored.GameData.Modifiers;
+
+namespace SwordAndBored.GameData.Abilities.Skills
+{
+    abstract class AbstractActivatedAbility : GenericAbility, IActivatedAbility
+    {
+        public IModifier[] ActiveModifiers { get; protected set; }
+
+        public bool IsActive { get; set; }
+    }
+}
