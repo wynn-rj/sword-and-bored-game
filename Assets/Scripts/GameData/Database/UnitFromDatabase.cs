6421f4f3845bb6481b1f44502352e24de7f17f00
Toeecko <rorygilliland@ameritech.net>
2020-02-07 23:06:39 -0500

    creates class for table objects for description, stats

---
+++ Assets/Scripts/GameData/Database/UnitFromDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
+using SwordAndBored.GameData.Database;
+using TMPro;
+
+public class UnitFromDatabase : MonoBehaviour
+{
+    public TMP_Text textBox;
+    private DatabaseConnection conn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        conn = new DatabaseConnection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SqliteDataReader reader = conn.ExecuteQuery(conn.GetQueryStringForTable("Units"));
+            while (reader.Read())
+            {
+                for(int i=0; i<reader.FieldCount; i++)
+                {
+                    textBox.text += reader.GetValue(i) + " ";
+                }
+                textBox.text += "\n";
+            }
+        }
+    }
+}
