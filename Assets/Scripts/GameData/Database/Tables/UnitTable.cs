e33b1ed0a833f3b6ce8a7321a65bc7a07c70313e
Toeecko <rorygilliland@ameritech.net>
2020-02-08 00:24:22 -0500

    adds unit from database, adds 2nd unit in test db

--- Assets/Scripts/GameData/Database/Tables/UnitTable.cs
+++ Assets/Scripts/GameData/Database/Tables/UnitTable.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Mono.Data.Sqlite;
+
 namespace SwordAndBored.GameData.Database.Tables {
     public class UnitTable
     {
@@ -6,5 +7,41 @@ namespace SwordAndBored.GameData.Database.Tables {
         public DescriptorTable Descriptor { get; set; }
         public RoleTable Role { get; set; }
         public StatsTable Stats { get; set; }
+
+        public UnitTable(int inputID)
+        {
+            DatabaseConnection conn = new DatabaseConnection();
+            SqliteDataReader reader = conn.QueryRowFromTableWithID("Units", inputID);
+
+            ID = inputID;
+            if (reader.Read())
+            {
+                int descriptorID = reader.GetInt32(reader.GetOrdinal("Descriptor_FK"));
+                Descriptor = new DescriptorTable(descriptorID);
+
+                int roleID = reader.GetInt32(reader.GetOrdinal("Role_FK"));
+                Role = new RoleTable(roleID);
+
+
+                int statsID = reader.GetInt32(reader.GetOrdinal("Stats_FK"));
+                Stats = new StatsTable(statsID);
+            }
+            conn.CloseConnection();
+        }
+
+        override public string ToString()
+        {
+            return "{Unit: " + ID + ", Descriptior: " + Descriptor.ToString() + ", Role: " + Role.ShortString() + ", Stats: " + Stats.ToString() + "}";
+        }
+
+        public string LongString()
+        {
+            return "Unit: {ID: " + ID + ", Descriptior: " + Descriptor.LongString() + ", Role: " + Role.LongString() + ", Stats: " + Stats.LongString() + "}";
+        }
+
+        public string ShortString()
+        {
+            return "{Unit: " + ID + ", Descriptior: " + Descriptor.Name + ", Role: " + Role.ID + ", Stats: " + Stats.ID + "}";
+        }
     }
 }
