b41197d6b40523f929a0a37d817521a40fd8db53
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 14:13:37 -0500

    Create an abstract skill tree

---
+++ Assets/Scripts/GameData/Abilities/Skills/AbstractSkillTree.cs
@@ -0,0 +1,12 @@
+﻿namespace SwordAndBored.GameData.Abilities.Skills
+{
+    abstract class AbstractSkillTree : ISkillTree
+    {
+        public ISkillTier[] Tiers { get; }
+
+        public AbstractSkillTree(int tierDepth)
+        {
+            Tiers = new ISkillTier[tierDepth];
+        }
+    }
+}
