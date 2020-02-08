39693d331ccebce7fe3ad5d74dbcfffdc6268137
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 13:57:03 -0500

    Create an abstract skill tier

---
+++ Assets/Scripts/GameData/Abilities/Skills/AbstractSkillTier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwordAndBored.GameData.Abilities.Skills
+{
+    abstract class AbstractSkillTier : ISkillTier
+    {
+        public virtual IAbility[] Choices { get; }        
+
+        public virtual IAbility[] Selected { get; }
+
+        public AbstractSkillTier(IAbility[] choices)
+        {
+            Choices = (IAbility[])choices.Clone();
+        }
+
+        public AbstractSkillTier(int choiceCount)
+        {
+            Choices = new IAbility[choiceCount];
+        }
+
+        public abstract void SelectSkill(int choicesIndex);
+    }
+}
