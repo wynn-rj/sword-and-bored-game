807f56f0af936d982efac42c8a4ba9c9a76ce26c
Toeecko <rorygilliland@ameritech.net>
2020-02-07 23:06:39 -0500

    creates class for table objects for description, stats

---
+++ Assets/Scripts/GameData/Database/DatabaseConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+namespace SwordAndBored.GameData.Database
+{
+    public class DatabaseConnection
+    {
+        public SqliteConnection conn;
+
+        public DatabaseConnection()
+        {
+            CreateDatabaseConnection();
+        }
+        private void CreateDatabaseConnection()
+        {
+            conn = new SqliteConnection("URI=file:" + Application.dataPath + "/StreamingAssets/GameData.db");
+            conn.Open();
+        }
+
+        public SqliteDataReader ExecuteQuery(string query)
+        {
+            SqliteCommand command = conn.CreateCommand();
+            command.CommandText = query;
+            return command.ExecuteReader();
+        }
+
+        public string GetQueryStringForTable(string tableName)
+        {
+            return "SELECT * FROM " + tableName;
+        }
+    }
+}
