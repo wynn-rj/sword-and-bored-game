8e8f0eada5aca10aed03e4f1f27ffb6b684330cd
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 15:08:14 -0500

    Create a factory for building abilities
    The factory's functionality is currently temporary

---
+++ Assets/Scripts/GameData/Abilities/AbilityFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace SwordAndBored.GameData.Abilities
+{
+    class AbilityFactory
+    {
+        public static IAbility BuildAbility(string abilityName)
+        {
+            //TODO: Interface this with the database once that is available
+            return BuildGenericAbility(abilityName, "A temporary description", "image");
+        }
+
+        private static GenericAbility BuildGenericAbility(string name, string desc, string image)
+        {
+            GenericAbility ability = new GenericAbility()
+            {
+                Name = name,
+                Description = desc,
+                ImageName = image
+            };
+            return ability;
+        }
+    }
+}
