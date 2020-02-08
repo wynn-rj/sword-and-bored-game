2d933f329f327381066e7d0b71f2e48ade77ff34
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 14:42:02 -0500

    Add single selection skill tree

---
+++ Assets/Scripts/GameData/Abilities/Skills/SingleSelectSkillTree.cs
@@ -0,0 +1,23 @@
+﻿namespace SwordAndBored.GameData.Abilities.Skills
+{
+    class SingleSelectSkillTree : ISkillTree
+    {
+        public ISkillTier[] Tiers => SingleSkillTiers;
+
+        protected SingleSelectSkillTier[] SingleSkillTiers { get; }
+
+        public SingleSelectSkillTree(int tierDepth)
+        {
+            SingleSkillTiers = new SingleSelectSkillTier[tierDepth];
+        }
+
+        public SingleSelectSkillTree(IAbility[][] abilityTiers) : this(abilityTiers.Length)
+        {
+            int i = 0;
+            foreach (IAbility[] abilityList in abilityTiers)
+            {
+                SingleSkillTiers[i++] = new SingleSelectSkillTier(abilityList);
+            }
+        }
+    }
+}
