6f6b359bf8bf4f08b41b724925b7724e0d3ec3ec
Toeecko <rorygilliland@ameritech.net>
2020-02-07 23:17:13 -0500

    renamed from xxxdatabase to xxxtable

---
+++ Assets/Scripts/GameData/Database/Tables/StatsTable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SwordAndBored.GameData.Database.Tables
+{
+    public class StatsTable
+    {
+        public int ID { get; }
+        public int Physical_Attack { get; set; }
+        public int Physical_Defense { get; set; }
+        public int Magic_Attack { get; set; }
+        public int Magic_Defense { get; set; }
+        public int HP { get; set; }
+        public int Initiative { get; set; }
+        public int Movement { get; set; }
+        public int Evasion { get; set; }
+        public int Accuracy { get; set; }
+    }
+}
