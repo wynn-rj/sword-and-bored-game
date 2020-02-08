cca31b69461568d159f07a4db2e93329cf564e4a
Toeecko <rorygilliland@ameritech.net>
2020-02-08 13:33:29 -0500

    attack modifiers implemented

--- Assets/Scripts/GameData/Database/Tables/AttackModifiersTable.cs
+++ Assets/Scripts/GameData/Database/Tables/AttackModifiersTable.cs
@@ -12,5 +12,46 @@ namespace SwordAndBored.GameData.Database.Tables
         public int Bleed_Damage { get; set; }
         public int Bleed_Chance { get; set; }
         public int Stun_Chance { get; set; }
+
+        public AttackModifiersTable(int inputID)
+        {
+            DatabaseConnection conn = new DatabaseConnection();
+            DatabaseReader reader = conn.QueryRowFromTableWithID("Attack_Modifiers", inputID);
+
+            ID = inputID;
+            if (reader.NextRow())
+            {
+                Fire_Damage = reader.GetIntFromCol("Fire_Damage");
+                Fire_Chance = reader.GetIntFromCol("Fire_Chance");
+                Posion_Damage = reader.GetIntFromCol("Posion_Damage");
+                Posion_Chance = reader.GetIntFromCol("Posion_Chance");
+                Bleed_Damage = reader.GetIntFromCol("Bleed_Damage");
+                Bleed_Chance = reader.GetIntFromCol("Bleed_Chance");
+                Stun_Chance = reader.GetIntFromCol("Stun_Chance");
+            }
+            reader.CloseReader();
+            conn.CloseConnection();
+        }
+
+        override public string ToString()
+        {
+            return "{AttackModifiers: " + ID + ", Fire: " + Fire_Damage + "(" + Fire_Chance + "%)" +
+                ", Posion: " + Posion_Damage + "(" + Posion_Chance + "%)" +
+                ", Bleed: " + Bleed_Damage + "(" + Bleed_Chance + "%)" + ", Stun: " + Stun_Chance + "}";
+        }
+
+        public string ShortString()
+        {
+            return "{AttackModifiers: " + ID + ", Fire: " + Fire_Damage + "(" + Fire_Chance + "%)" +
+                ", Posion: " + Posion_Damage + "(" + Posion_Chance + "%)" +
+                ", Bleed: " + Bleed_Damage + "(" + Bleed_Chance + "%)" + ", Stun: " + Stun_Chance + "}";
+        }
+
+        public string LongString()
+        {
+            return "{AttackModifiers: " + ID + ", Fire: " + Fire_Damage + "(" + Fire_Chance + "%)" + 
+                ", Posion: " + Posion_Damage + "(" + Posion_Chance + "%)" +
+                ", Bleed: " + Bleed_Damage + "(" + Bleed_Chance + "%)" + ", Stun: " + Stun_Chance +  "}";
+        }
     }
 }
