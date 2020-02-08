491b96bb36e67e98b162b86bee344e570a735a9e
Toeecko <rorygilliland@ameritech.net>
2020-02-07 23:17:13 -0500

    renamed from xxxdatabase to xxxtable

--- Assets/Scripts/GameData/Database/Tables/DescriptorTable.cs
+++ Assets/Scripts/GameData/Database/Tables/DescriptorTable.cs
@@ -7,36 +7,5 @@ namespace SwordAndBored.GameData.Database.Tables
         public string Name { get; set; }
         public string Description { get; set; }
         public string FlavorText { get; set; }
-
-        public DescriptorTable(int inputID)
-        {
-            DatabaseConnection conn = new DatabaseConnection();
-            DatabaseReader reader = conn.QueryRowFromTableWithID("Descriptors", inputID);
-
-            ID = inputID;
-            if (reader.NextRow())
-            {
-                Name = reader.GetStringFromCol("Name");
-                Description = reader.GetStringFromCol("Description");
-                FlavorText = reader.GetStringFromCol("Flavor_Text");
-            }
-            reader.CloseReader();
-            conn.CloseConnection();
-        }
-
-        override public string ToString()
-        {
-            return "{Descriptor: " + ID + ", Name: " + Name + "}";
-        }
-
-        public string LongString()
-        {
-            return "Descriptor: {ID: " + ID + ", Name: " + Name + ", Description: " + Description + ", FlavorText: " + FlavorText + "}";
-        }
-
-        public string ShortString()
-        {
-            return "{Descriptor: " + ID + "}";
-        }
     }
 }
