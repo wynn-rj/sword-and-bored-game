48254551f4c3dabd64e878472eedf522e71e8371
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 15:21:08 -0500

    Add helper class for pulling modifiers from abilities

---
+++ Assets/Scripts/GameData/Abilities/AbilityHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using SwordAndBored.GameData.Modifiers;
+
+namespace SwordAndBored.GameData.Abilities
+{
+    static class AbilityHelper
+    {
+        public static IModifier[] PullModifiersFromAbilities(IAbility[] abilities)
+        {
+            List<IModifier> modifiers = new List<IModifier>();
+
+            foreach (IAbility ability in abilities)
+            {
+                if (ability is IActivatedAbility activatedAbility)
+                {
+                    if (activatedAbility.IsActive)
+                    {
+                        modifiers.AddRange(activatedAbility.ActiveModifiers);
+                    }
+                }
+                if (ability is IPassiveAbility passiveAbility)
+                {
+                    modifiers.AddRange(passiveAbility.PassiveModifiers);
+                }
+            }
+            return modifiers.ToArray();
+        }
+    }
+}
