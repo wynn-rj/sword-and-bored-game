d25a0f685e8cd759144c279bb4b9eb65c74ec1b3
Toeecko <rorygilliland@ameritech.net>
2020-02-08 13:02:45 -0500

    creates new files for attack modifier and physical abilities, also removes testConnection

---
+++ Assets/Scripts/GameData/Database/Tables/PhysicalAbilitiesTable.cs
@@ -0,0 +1,16 @@
+﻿using Mono.Data.Sqlite;
+
+namespace SwordAndBored.GameData.Database.Tables
+{
+    public class PhysicalAbilitiesTable
+    {
+        public int ID { get; }
+        public DescriptorTable Descriptor { get; set; }
+        public WeaponTable Weapon { get; set; }
+        public AttackModifiersTable Stats { get; set; }
+
+        public int Damage { get; set; }
+        public int Range { get; set; }
+        public int Accuracy { get; set; }
+    }
+}
