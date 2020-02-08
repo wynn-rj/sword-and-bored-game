e79f6b48a49138522486b89e625b0e2f144030f6
Toeecko <rorygilliland@ameritech.net>
2020-02-08 12:33:54 -0500

    adds weapon get(s) from db

---
+++ Assets/Scripts/GameData/Database/Tables/WeaponTable.cs
@@ -0,0 +1,39 @@
+﻿using Mono.Data.Sqlite;
+
+namespace SwordAndBored.GameData.Database.Tables
+{
+    public class WeaponTable
+    {
+        public int ID { get; }
+        public DescriptorTable Descriptor { get; set; }
+
+        public WeaponTable(int inputID)
+        {
+            DatabaseConnection conn = new DatabaseConnection();
+            SqliteDataReader reader = conn.QueryRowFromTableWithID("Weapons", inputID);
+
+            ID = inputID;
+            if (reader.Read())
+            {
+                int descriptorID = reader.GetInt32(reader.GetOrdinal("Descriptor_FK"));
+                Descriptor = new DescriptorTable(descriptorID);
+            }
+            conn.CloseConnection();
+        }
+
+        override public string ToString()
+        {
+            return "{Weapon: " + ID + ", Descriptior: " + Descriptor.ToString() + "}";
+        }
+
+        public string LongString()
+        {
+            return "Weapon: {ID: " + ID + ", Descriptior: " + Descriptor.LongString() + "}";
+        }
+
+        public string ShortString()
+        {
+            return "{Weapon: " + ID + ", Descriptior: " + Descriptor.Name + "}";
+        }
+    }
+}
