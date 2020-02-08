e925b438d185d53f95fa5b769a5e6b790595e545
Toeecko <rorygilliland@ameritech.net>
2020-02-07 23:06:39 -0500

    creates class for table objects for description, stats

--- Assets/Scripts/GameData/Database/TestConnection.cs
+++ Assets/Scripts/GameData/Database/TestConnection.cs
@@ -11,13 +11,13 @@ namespace SwordAndBored.GameData.Database {
         void Start()
         {
             string conn = "URI=file:" + Application.dataPath + "/StreamingAssets/GameData.db"; //Path to database.
-            IDbConnection dbconn;
-            dbconn = (IDbConnection) new SqliteConnection(conn);
+            SqliteConnection dbconn;
+            dbconn = new SqliteConnection(conn);
             dbconn.Open(); //Open connection to the database.
-            IDbCommand dbcmd = dbconn.CreateCommand();
+            SqliteCommand dbcmd = dbconn.CreateCommand();
             string sqlQuery = "SELECT * FROM Units";
             dbcmd.CommandText = sqlQuery;
-            IDataReader reader = dbcmd.ExecuteReader();
+            SqliteDataReader reader = dbcmd.ExecuteReader();
             while (reader.Read())
             {
                 int value = reader.GetInt32(0);
@@ -33,22 +33,13 @@ namespace SwordAndBored.GameData.Database {
         {
             if (Input.GetKeyDown(KeyCode.D))
             {
-                string conn = "URI=file:" + Application.dataPath + "/StreamingAssets/GameData.db"; //Path to database.
+                string conn = "URI=file:" + Application.dataPath + "/StreamingAssets/GameData.db";
+                Debug.Log(Application.dataPath);    //Path to database.
                 IDbConnection dbconn;
                 dbconn = (IDbConnection)new SqliteConnection(conn);
                 dbconn.Open(); //Open connection to the database.
-                IDbCommand dbcmd = dbconn.CreateCommand();
-                string sqlQuery = "SELECT * FROM Units";
-                dbcmd.CommandText = sqlQuery;
-                IDataReader reader = dbcmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    int value = reader.GetInt32(0);
-                    Debug.Log("value= " + value);
-                    text.SetText("value= " + value);
-                }
-                reader.Close();
-                dbcmd.Dispose();
+                text.SetText(conn);
+
                 dbconn.Close();
             }
         }
