94fee698957a202482161a7d276fac3132d3f451
Toeecko <rorygilliland@ameritech.net>
2020-02-07 23:37:58 -0500

    gets a full descriptor object from database

--- Assets/Scripts/GameData/Database/Tables/DescriptorTable.cs
+++ Assets/Scripts/GameData/Database/Tables/DescriptorTable.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Mono.Data.Sqlite;
+
 namespace SwordAndBored.GameData.Database.Tables
 {
     public class DescriptorTable
@@ -7,5 +8,24 @@ namespace SwordAndBored.GameData.Database.Tables
         public string Name { get; set; }
         public string Description { get; set; }
         public string FlavorText { get; set; }
+
+        public DescriptorTable(int inputID)
+        {
+            DatabaseConnection conn = new DatabaseConnection();
+            SqliteDataReader reader = conn.QueryRowFromTableWithID("Descriptors", inputID);
+
+            ID = inputID;
+            if (reader.Read())
+            {
+                Name = reader.GetString(reader.GetOrdinal("Name"));
+                Description = reader.GetString(reader.GetOrdinal("Description"));
+                FlavorText = reader.GetString(reader.GetOrdinal("Flavor_Text"));
+            }
+        }
+
+        override public string ToString()
+        {
+            return "{ID: " + ID + ", Name: " + Name + ", Description: " + Description + ", FlavorText: " + FlavorText + "}";
+        }
     }
 }
