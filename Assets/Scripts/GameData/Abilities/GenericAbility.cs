4b363b47ab8a592f7dbd1dc01d8b212613350ce5
Robert J Wynn <wynn.134@osu.edu>
2020-02-08 15:07:40 -0500

    Create a generic ability class

---
+++ Assets/Scripts/GameData/Abilities/GenericAbility.cs
@@ -0,0 +1,11 @@
+﻿namespace SwordAndBored.GameData.Abilities
+{
+    class GenericAbility : IAbility
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual string ImageName { get; set; }
+    }
+}
