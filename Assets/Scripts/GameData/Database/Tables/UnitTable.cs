c62f6bb5c1029a1c64d4e4c1e5f7b3c94b885c8b
Toeecko <rorygilliland@ameritech.net>
2020-02-08 12:33:54 -0500

    adds weapon get(s) from db

--- Assets/Scripts/GameData/Database/Tables/UnitTable.cs
+++ Assets/Scripts/GameData/Database/Tables/UnitTable.cs
@@ -8,6 +8,8 @@ namespace SwordAndBored.GameData.Database.Tables {
         public RoleTable Role { get; set; }
         public StatsTable Stats { get; set; }
 
+        public WeaponTable Weapon { get; set; }
+
         public UnitTable(int inputID)
         {
             DatabaseConnection conn = new DatabaseConnection();
@@ -25,23 +27,26 @@ namespace SwordAndBored.GameData.Database.Tables {
 
                 int statsID = reader.GetInt32(reader.GetOrdinal("Stats_FK"));
                 Stats = new StatsTable(statsID);
+
+                int weaponID = reader.GetInt32(reader.GetOrdinal("Weapon_FK"));
+                Weapon = new WeaponTable(weaponID);
             }
             conn.CloseConnection();
         }
 
         override public string ToString()
         {
-            return "{Unit: " + ID + ", Descriptior: " + Descriptor.ToString() + ", Role: " + Role.ShortString() + ", Stats: " + Stats.ToString() + "}";
+            return "{Unit: " + ID + ", Descriptior: " + Descriptor.ToString() + ", Role: " + Role.ShortString() + ", Stats: " + Stats.ToString() + ", Weapon: " + Weapon.ToString() + "}";
         }
 
         public string LongString()
         {
-            return "Unit: {ID: " + ID + ", Descriptior: " + Descriptor.LongString() + ", Role: " + Role.LongString() + ", Stats: " + Stats.LongString() + "}";
+            return "Unit: {ID: " + ID + ", Descriptior: " + Descriptor.LongString() + ", Role: " + Role.LongString() + ", Stats: " + Stats.LongString() + ", Weapon: " + Weapon.ToString() + "}";
         }
 
         public string ShortString()
         {
-            return "{Unit: " + ID + ", Descriptior: " + Descriptor.Name + ", Role: " + Role.ID + ", Stats: " + Stats.ID + "}";
+            return "{Unit: " + ID + ", Descriptior: " + Descriptor.Name + ", Role: " + Role.ID + ", Stats: " + Stats.ID + ", Weapon: " + Weapon.ToString() + "}";
         }
     }
 }
