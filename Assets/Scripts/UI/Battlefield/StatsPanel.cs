eb32f8696b11d2fb730442af834cac197305a26a
Toeecko <rorygilliland@ameritech.net>
2020-02-09 16:18:14 -0500

    creates statsUI script to pull current unit turn and display stats

---
+++ Assets/Scripts/UI/Battlefield/StatsPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using SwordAndBored.Battlefield.TurnMechanism;
+using SwordAndBored.Battlefield.CreaturScripts;
+using TMPro;
+
+namespace SwordAndBored.UI.Battlefield {
+    public class StatsPanel : MonoBehaviour
+    {
+        public GameObject statsPanel;
+        public TurnManager turnManager;
+
+        public TMP_Text nameText, healthText, pAttackText, pDefenseText,
+            mAttackText, mDefenseText, movementText, initiativeText, evasionText,
+            accuracyText, roleText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                statsPanel.SetActive(!statsPanel.activeSelf);
+            }
+
+            if(statsPanel.activeSelf)
+            {
+                UniqueCreature activePlayer = turnManager.activePlayer.gameObject.GetComponent<UniqueCreature>();
+                nameText.SetText(activePlayer.creatureName);
+            }
+        }
+    }
+}
