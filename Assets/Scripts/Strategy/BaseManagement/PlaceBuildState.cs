dd8286049ea8ae23844509d22f51fd0b5e7c623d
cthomas68 <c.l.thomas68@gmail.com>
2020-02-09 15:52:51 -0500

    Add shadow model to place state

--- Assets/Scripts/Strategy/BaseManagement/PlaceBuildState.cs
+++ Assets/Scripts/Strategy/BaseManagement/PlaceBuildState.cs
@@ -10,18 +10,22 @@ public class PlaceBuildState : AbstractBaseState
     private Ray ray;
 
     IBuilding building;
+    GameObject shadowModel;
 
     public PlaceBuildState(BaseManager bm, IBuilding building) : base(bm)
     {
         ToggleBuildingsList();
         this.building = building;
+        //Instantiate behind camera out of view for now
+        this.shadowModel = BaseManager.Instantiate(Resources.Load("Buildings/" + building.ShadowModelName) as GameObject, new Vector3(0, 11, -11), Quaternion.identity);
     }
 
     public override void PlaceBuilding()
     {
+        BaseManager.Destroy(shadowModel);
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube.transform.position = position;
-        //BaseManager.Instantiate(PrimitiveType.Cube, position, Quaternion.identity);
+        //BaseManager.Instantiate(PrimitiveType.Cube, position, Quaternion.identity);  
         BaseManager.BaseManagementState = new IdleBaseState(BaseManager);
 
         base.PlaceBuilding();
@@ -34,6 +38,7 @@ public class PlaceBuildState : AbstractBaseState
         if (Physics.Raycast(ray, out hit, 50))
         {
             position = hit.point;
+            shadowModel.transform.position = position;
         }
 
         if (Input.GetMouseButtonDown(0))
