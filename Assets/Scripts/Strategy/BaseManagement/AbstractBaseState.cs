21cd53ed5e5732acb75b8def10216dc02bb2eac8
cthomas68 <c.l.thomas68@gmail.com>
2020-02-09 14:44:25 -0500

    Create abstract and place build states

---
+++ Assets/Scripts/Strategy/BaseManagement/AbstractBaseState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractBaseState : IBaseManagementState
+{
+    protected BaseManager BaseManager { get; set; }
+
+    public AbstractBaseState(BaseManager bm)
+    {
+        this.BaseManager = bm;
+    }
+
+    public virtual void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ToggleBuildingsList();
+            BaseManager.BaseManagementState = new SelectBuildState(BaseManager);
+        }
+    }
+
+    protected virtual void ToggleBuildingsList()
+    {
+        BaseManager.Canvas.gameObject.SetActive(!BaseManager.Canvas.gameObject.activeSelf);
+    }
+}
