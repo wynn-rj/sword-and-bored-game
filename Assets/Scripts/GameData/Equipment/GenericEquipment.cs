1f8f41826cdf9b3e50a3dabc58768230fd8e8009
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 21:19:51 -0500

    Create Abstract and Generic equipment

---
+++ Assets/Scripts/GameData/Equipment/GenericEquipment.cs
@@ -0,0 +1,18 @@
+﻿using SwordAndBored.GameData.Abilities;
+using SwordAndBored.GameData.Modifiers;
+
+namespace SwordAndBored.GameData.Equipment
+{
+    class GenericEquipment : AbstractEquipment
+    {
+        public override IAbility[] Abilities { get; }
+
+        public override IModifier[] Modifiers { get; }
+
+        public GenericEquipment(IAbility[] abilities, IModifier[] modifiers)
+        {
+            Abilities = abilities;
+            Modifiers = modifiers; 
+        }
+    }
+}
