eac1aa1f70a9c0c646029fe7ded0176edd2544ef
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 22:12:33 -0500

    Add weighted probability table

---
+++ Assets/Scripts/Utilities/Random/ProbabilityTables/WeightedProbabilityTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwordAndBored.Utilities.Random.ProbabilityTables
+{
+    class WeightedProbabilityTable<T> : AbstractProbabilityTable<T>
+    {
+        private double weightSum;
+        private List<Tuple<T, double>> weightedTable;
+        public WeightedProbabilityTable()
+        {
+            weightSum = 0;
+            weightedTable = new List<Tuple<T, double>>();
+        }
+
+        public void AddItem(T item, double weight)
+        {
+            weightedTable.Add(new Tuple<T, double>(item, weight));
+        }
+            
+        public override T GetItem()
+        {
+            double pulledValue = Odds.PercentDouble() * weightSum;
+            int index = -1;
+            do {
+                pulledValue -= weightedTable[++index].Item2
+            } while (pulledValue > 0);
+            return weightedTable[index].Item1;
+        }
+    }
+}
