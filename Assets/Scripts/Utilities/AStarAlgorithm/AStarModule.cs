672b2415dd017466815264ec08316e4881f3139a
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-02-09 22:44:58 -0500

    create general layout of 2d grid based Astar with no blocked tiles or weighted distances

--- Assets/Scripts/Utilities/AStarAlgorithm/AStarModule.cs
+++ Assets/Scripts/Utilities/AStarAlgorithm/AStarModule.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System;
 
 namespace SwordAndBored.Utilities.AStarAlgorithm
 {
-    public class AStarModule
+    public class AStarModule<T>
     {
-        public AStarModule()
-        {
+        private T[][] grid;
 
+        public AStarModule(T[][] grid)
+        {
+            this.grid = grid;
+        }
+
+        public List<T> findPath(Tuple<int, int> startPoint, Tuple<int, int> destination)
+        {
+            T dest = grid[destination.Item1][destination.Item2];
+
+            return new List<T>();
+        }
+
+        private struct Node
+        {
+            public T Data { get; set; }
+            public Tuple<int, int> Location { get; set; }
+            public T Parent { get; set; }
         }
     }
 }
