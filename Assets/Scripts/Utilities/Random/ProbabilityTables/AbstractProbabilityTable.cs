92f26dd9b2e736fb34dd10e45e9d27f4f9185ae8
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 22:11:41 -0500

    Create abstract probability table

---
+++ Assets/Scripts/Utilities/Random/ProbabilityTables/AbstractProbabilityTable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.Utilities.Random.ProbabilityTables
+{
+    abstract class AbstractProbabilityTable<T> : IProbabilityTable<T>
+    {
+        public abstract T GetItem();
+
+        public IEnumerable<T> GetItems(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return GetItem();
+            }
+        }
+    }
+}
