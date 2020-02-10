b2f0afe0fac0589a4dd08c9a210ab26fb81a115a
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 21:20:25 -0500

    Add abstract character and creature

---
+++ Assets/Scripts/GameData/Creatures/AbstractCharacter.cs
@@ -0,0 +1,94 @@
+﻿using SwordAndBored.GameData.Abilities;
+using SwordAndBored.GameData.Abilities.Skills;
+using SwordAndBored.GameData.Equipment;
+using SwordAndBored.GameData.Modifiers;
+using SwordAndBored.GameData.Roles;
+using System.Collections.Generic;
+
+namespace SwordAndBored.GameData.Creatures
+{
+    abstract class AbstractCharacter : AbstractCreature, ICharacter
+    {
+        public abstract IRole Role { get; }
+
+        public abstract int XP { get; set; }
+
+        public virtual int Level { get; protected set; }
+
+        public abstract ISkillTree SkillTree { get; protected set; }
+
+        public IEquipment ArmorSlot
+        {
+            get => armorSlot;
+            set
+            {
+                armorSlot = value;
+                RecalculateAll();
+            }
+        }
+        public IEquipment WeaponSlot
+        {
+            get => weaponSlot;
+            set
+            {
+                weaponSlot = value;
+                RecalculateAll();
+            }
+        }
+
+        public IEquipment[] AdditionalEquipmentSlots { get; set; }
+
+        public override IEquipment[] Equipment
+        {
+            get
+            {
+                HashCheck();
+                if (recalculateEquipment)
+                {
+                    List<IEquipment> equipmentList = new List<IEquipment>(AdditionalEquipmentSlots);
+                    equipmentList.Add(ArmorSlot);
+                    equipmentList.Add(WeaponSlot);
+                    additionalEquipmentHash = AdditionalEquipmentSlots.GetHashCode();
+                    recalculateEquipment = false;
+                    equipment = equipmentList.ToArray();
+                }
+                return equipment;
+            }
+        }
+
+        public override IAbility[] Abilities
+        {
+            get
+            {
+                HashCheck();
+                return base.Abilities;
+            }
+        }
+
+        public override IModifier[] Modifiers
+        {
+            get
+            {
+                HashCheck();
+                return base.Modifiers;
+            }
+        }
+
+        protected bool recalculateEquipment = true;
+
+        private IEquipment armorSlot;
+        private IEquipment weaponSlot;
+        private IEquipment[] equipment;
+        private int additionalEquipmentHash = -1;
+
+        private void RecalculateAll()
+        {
+            recalculateEquipment = recalculateModifiers = recalculateAbiltiies = true;
+        }
+
+        private void HashCheck()
+        {
+            recalculateEquipment |= AdditionalEquipmentSlots.GetHashCode() != additionalEquipmentHash;
+        }
+    }
+}
