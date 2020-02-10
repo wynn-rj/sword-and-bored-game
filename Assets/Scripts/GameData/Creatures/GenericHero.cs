a148dcde226b6b47338d212f02cf55cb0287d093
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 21:20:37 -0500

    Add generic character, creature, and hero

---
+++ Assets/Scripts/GameData/Creatures/GenericHero.cs
@@ -0,0 +1,17 @@
+﻿using SwordAndBored.GameData.Abilities;
+using SwordAndBored.GameData.Roles;
+using System;
+
+namespace SwordAndBored.GameData.Creatures
+{
+    class GenericHero : GenericCharacter, IHero
+    {
+        public IAbility HeroAbility { get; }
+
+        public GenericHero(Func<int, int> xpThresholdFunction, IRole role, IAbility heroAbility, IStats stats = null) :
+            base(xpThresholdFunction, role, stats)
+        {
+            HeroAbility = heroAbility ?? AbilityFactory.BuildAbility("Generic Hero Ability");
+        }       
+    }
+}
