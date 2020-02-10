36a50c92e271e8f3f377fd2e82833def16264382
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 21:20:37 -0500

    Add generic character, creature, and hero

---
+++ Assets/Scripts/GameData/Creatures/GenericCreature.cs
@@ -0,0 +1,17 @@
+﻿using SwordAndBored.GameData.Equipment;
+
+namespace SwordAndBored.GameData.Creatures
+{
+    class GenericCreature : AbstractCreature
+    {
+        public override IEquipment[] Equipment { get; }
+
+        public override IStats Stats { get; }
+
+        public GenericCreature(IEquipment[] equipment = null, IStats stats = null)
+        {
+            Stats = stats ?? new GenericStats();
+            Equipment = equipment ?? new IEquipment[0];
+        }
+    }
+}
