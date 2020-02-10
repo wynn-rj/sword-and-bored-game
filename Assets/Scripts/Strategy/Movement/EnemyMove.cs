0fafb5df33d12b8a9abacc00a4d4051c3753f247
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-10 15:54:14 -0500

    Cleaned up filed

---
+++ Assets/Scripts/Strategy/Movement/EnemyMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.Translate(Vector3.forward * 0.05f);
+    }
+}
