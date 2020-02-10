e43acb421181b68c0bca7c232be72cfc9d43df02
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 22:12:09 -0500

    Create an equally likely probability table

---
+++ Assets/Scripts/Utilities/Random/ProbabilityTables/EqualProbabilityTable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.Utilities.Random.ProbabilityTables
+{
+    /// <summary>
+    /// An IProbabilityTable where every item has an equal weight of being selected
+    /// </summary>
+    /// <typeparam name="T">The type of each item</typeparam>
+    class EqualProbabilityTable<T> : IProbabilityTable<T>
+    {
+        private readonly List<T> table;
+
+        public EqualProbabilityTable()
+        {
+            table = new List<T>();
+        }
+
+        public void AddItem(T item) => table.Add(item);
+
+        public void AddItems(IEnumerable<T> items) => table.AddRange(items);
+
+        public T GetItem() => Odds.SelectAtRandom(table.ToArray());
+    }
+}
