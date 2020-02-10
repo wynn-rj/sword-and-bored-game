ea89476494456e9e7ae758237c9a2e713d079941
cthomas68 <c.l.thomas68@gmail.com>
2020-02-09 22:23:54 -0500

    Add HUD and attachable folders and files

---
+++ Assets/Scripts/Strategy/BaseManagement/Attachable/HomeBase.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeBase : MonoBehaviour
+{
+    public BaseManager bm;
+
+    public Canvas canvasObject;
+    public int tier;
+
+    public Text tierDisplayed;
+
+    Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    void Start()
+    {
+        tier = 1;
+        renderer.material.color = Color.gray;
+        tierDisplayed.text = tier.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Enter");
+
+        renderer.material.color = Color.yellow;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            canvasObject.gameObject.SetActive(true);
+            Debug.Log("I was clicked.");
+            bm.GetComponent<BaseManager>().SetAllCanvasInactive();
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            UpgradeTier();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Exit");
+        renderer.material.color = Color.grey;
+    }
+
+    private void UpgradeTier()
+    {
+        Mathf.Clamp(++tier, 1, 3);
+    }
+
+    public void ExitCanvas()
+    {
+        canvasObject.gameObject.SetActive(false);
+    }
+}
