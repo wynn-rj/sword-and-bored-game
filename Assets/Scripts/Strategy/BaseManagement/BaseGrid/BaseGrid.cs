02df26b3b71be1554fa82d929e1e802cfb6b7c9d
cthomas68 <c.l.thomas68@gmail.com>
2020-02-10 04:01:31 -0500

    Add snappy grid

--- Assets/Scripts/Strategy/BaseManagement/BaseGrid/BaseGrid.cs
+++ Assets/Scripts/Strategy/BaseManagement/BaseGrid/BaseGrid.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseGrid
+public class BaseGrid : MonoBehaviour
 {
     [SerializeField] private int width;
     [SerializeField] private int height;
+    [SerializeField] private float cellSize;
 
+    private bool[,] baseGrid;
 
-
-    private static BaseGrid instance;
-
-    private BaseGrid() { }
-
-    public static BaseGrid Instance
+    void Start ()
     {
-        get
-        {
-            if (instance == null)
-            {
-                instance = new BaseGrid();
-            }
-
-            return instance;
-        }
+        this.cellSize = 1;
+        this.width = 10;
+        this.height = 10;
     }
 
     public Vector3 ReturnGridPoint(Vector3 position)
     {
-        return Vector3.zero;
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / cellSize);
+        int yCount = Mathf.RoundToInt(position.y / cellSize);
+        int zCount = Mathf.RoundToInt(position.z / cellSize);
+
+        Vector3 finalPosition = new Vector3((float)xCount * cellSize, (float)yCount * cellSize, (float)zCount * cellSize);
+
+        finalPosition += transform.position;
+
+        return finalPosition;
     }
 }
