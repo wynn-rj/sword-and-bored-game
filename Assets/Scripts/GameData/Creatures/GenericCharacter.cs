76ce2ac520702c57672b899e87929272325c1fd6
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 21:20:37 -0500

    Add generic character, creature, and hero

---
+++ Assets/Scripts/GameData/Creatures/GenericCharacter.cs
@@ -0,0 +1,53 @@
+﻿using SwordAndBored.GameData.Abilities;
+using SwordAndBored.GameData.Abilities.Skills;
+using SwordAndBored.GameData.Roles;
+using System;
+
+namespace SwordAndBored.GameData.Creatures
+{
+    class GenericCharacter : AbstractCharacter
+    {
+        public override IRole Role { get; }
+
+        public override int XP { 
+            get => xp;
+            set
+            {
+                xp = value;
+                Level = xpThresholdFunction(xp);
+            }
+        }
+        public override ISkillTree SkillTree { get; protected set; }
+
+        public override IStats Stats { get; }
+
+        protected int xp;
+        protected Func<int, int> xpThresholdFunction = null;
+
+        public GenericCharacter(Func<int, int>xpThresholdFunction, IRole role, IStats stats = null)
+        {
+            this.xpThresholdFunction = xpThresholdFunction ?? ((int x) => x);
+            Role = role ?? new GenericRole(new SingleSelectSkillTree(0));
+            SkillTree = role.RoleSkillTree;
+            Stats = stats ?? new GenericStats();
+        }
+
+        public IHero PromoteToHero(IAbility heroAbility)
+        {
+            IHero hero = new GenericHero(xpThresholdFunction, Role, heroAbility, Stats);
+            GenericCharacter heroAsCharacter = hero as GenericCharacter;
+            heroAsCharacter.SkillTree = SkillTree;
+            heroAsCharacter.internalAbilities = internalAbilities;
+            heroAsCharacter.internalModifiers = internalModifiers;
+            hero.XP = XP;
+            hero.Name = Name;
+            hero.Description = Description;
+            hero.WeaponSlot = WeaponSlot;
+            hero.ArmorSlot = ArmorSlot;
+            hero.AdditionalEquipmentSlots = AdditionalEquipmentSlots;
+            hero.ModelName = ModelName;
+
+            return hero;
+        }
+    }
+}
