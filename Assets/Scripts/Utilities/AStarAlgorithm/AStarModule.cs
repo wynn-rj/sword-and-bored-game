46e2735b01640af59bde8ec6a492840623dc089a
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-02-09 22:12:02 -0500

    add a star algorithm and module files

---
+++ Assets/Scripts/Utilities/AStarAlgorithm/AStarModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.Utilities.AStarAlgorithm
+{
+    public class AStarModule
+    {
+        public AStarModule()
+        {
+
+        }
+    }
+}
