f873607c6ceb56f8ce4fd43e19cb4d4577d4f65f
Robert J Wynn <wynn.134@osu.edu>
2020-02-09 21:20:04 -0500

    Add abstract and generic roles

---
+++ Assets/Scripts/GameData/Roles/AbstractRole.cs
@@ -0,0 +1,11 @@
+﻿using SwordAndBored.GameData.Abilities.Skills;
+
+namespace SwordAndBored.GameData.Roles
+{
+    abstract class AbstractRole : IRole
+    {
+        public abstract ISkillTree RoleSkillTree { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
