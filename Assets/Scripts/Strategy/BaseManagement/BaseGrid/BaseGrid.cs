66b5a3a9a4237917c62235fa94582babc310d105
cthomas68 <c.l.thomas68@gmail.com>
2020-02-10 17:00:45 -0500

    Update file namespaces

--- Assets/Scripts/Strategy/BaseManagement/BaseGrid/BaseGrid.cs
+++ Assets/Scripts/Strategy/BaseManagement/BaseGrid/BaseGrid.cs
@@ -2,33 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseGrid : MonoBehaviour
+namespace SwordAndBored.StrategyView.BaseManagement
 {
-    [SerializeField] private int width;
-    [SerializeField] private int height;
-    [SerializeField] private float cellSize;
-
-    private bool[,] baseGrid;
-
-    void Start ()
+    public class BaseGrid : MonoBehaviour
     {
-        this.cellSize = 1;
-        this.width = 10;
-        this.height = 10;
-    }
+        [SerializeField] private int width;
+        [SerializeField] private int height;
+        [SerializeField] private float cellSize;
 
-    public Vector3 ReturnGridPoint(Vector3 position)
-    {
-        position -= transform.position;
+        private bool[,] baseGrid;
 
-        int xCount = Mathf.RoundToInt(position.x / cellSize);
-        int yCount = Mathf.RoundToInt(position.y / cellSize);
-        int zCount = Mathf.RoundToInt(position.z / cellSize);
+        void Start()
+        {
+            this.cellSize = 1;
+            this.width = 10;
+            this.height = 10;
+        }
 
-        Vector3 finalPosition = new Vector3((float)xCount * cellSize, (float)yCount * cellSize, (float)zCount * cellSize);
+        public Vector3 ReturnGridPoint(Vector3 position)
+        {
+            position -= transform.position;
 
-        finalPosition += transform.position;
+            int xCount = Mathf.RoundToInt(position.x / cellSize);
+            int yCount = Mathf.RoundToInt(position.y / cellSize);
+            int zCount = Mathf.RoundToInt(position.z / cellSize);
 
-        return finalPosition;
+            Vector3 finalPosition = new Vector3((float)xCount * cellSize, (float)yCount * cellSize, (float)zCount * cellSize);
+
+            finalPosition += transform.position;
+
+            return finalPosition;
+        }
     }
 }
--- Assets/Scripts/Strategy/BaseManagement/IdleBaseState.cs
+++ Assets/Scripts/Strategy/BaseManagement/IdleBaseState.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleBaseState : AbstractBaseState
+namespace SwordAndBored.StrategyView.BaseManagement
 {
-    public IdleBaseState(BaseManager bm) : base (bm)
+    public class IdleBaseState : AbstractBaseState
     {
-        BaseManager.SetActiveCanvas(0);
-    }
-
-    public override void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.B))
+        public IdleBaseState(BaseManager bm) : base(bm)
         {
-            BaseManager.ToggleActiveCanvas();
-            BaseManager.BaseManagementState = new SelectBuildState(BaseManager);
+            BaseManager.SetActiveCanvas(0);
         }
 
-        base.Update();
+        public override void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                BaseManager.ToggleActiveCanvas();
+                BaseManager.BaseManagementState = new SelectBuildState(BaseManager);
+            }
+
+            base.Update();
+        }
     }
 }
+
