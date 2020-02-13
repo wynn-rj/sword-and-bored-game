371e6377b0fdedc76ab6991127c3bbd808a84e31
Jack Ruch <ruch.jack@yahoo.com>
2020-02-13 13:21:58 -0500

    Fixed the gridholder to auto-populate its array

---
+++ Assets/Scripts/Battlefield/TurnMechanism/EditorButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using SwordAndBored.Battlefield.TurnMechanism;
+
+#if (UNITY_EDITOR) 
+[CustomEditor(typeof(TileManager))]
+public class EditorButton : Editor
+{
+    public override void OnInspectorGUI() //2
+    {
+        base.DrawDefaultInspector();
+
+
+        TileManager manage = (TileManager)target; //1
+
+        GUILayout.Space(20f); //2
+        GUILayout.Label("Custom Editor Elements", EditorStyles.boldLabel); //3
+        
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Generate Tile Map")) //8
+        {
+            manage.GenerateTileMap();
+            Debug.Log("TileMap Generated");
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Erase Tile Map")) //8
+        {
+            manage.EraseTileMap();
+            Debug.Log("TileMap Deleted");
+        }
+
+        GUILayout.EndHorizontal();
+
+    }
+}
+#endif
