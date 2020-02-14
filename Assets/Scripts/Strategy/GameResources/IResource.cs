b3cb5ab7ff1b4c92c2783ad69de35fcf702eacc7
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-13 22:46:51 -0500

    Added files made in previous commit

---
+++ Assets/Scripts/Strategy/GameResources/IResource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.GameResources
+{
+
+
+    public interface IResource
+    {
+        int amount { get; set; }
+        bool CanAffordPurchase(IPayment payment);
+        void Purchase(IPayment payment);
+    }
+}
