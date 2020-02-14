957f96a5b8986e66f7773b7d7cbe2e02855dcd03
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-13 22:46:51 -0500

    Added files made in previous commit

---
+++ Assets/Scripts/Strategy/GameResources/Gold.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.GameResources
+{
+    public class Gold : IResource
+    {
+        public int amount { get; set; }
+        
+        public Gold(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public bool CanAffordPurchase(IPayment payment)
+        {
+            if (payment.cost <= amount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Purchase(IPayment payment)
+        {
+            this.amount -= payment.cost;
+        }
+    }
+}
