8eb6cc7ae9b1512af8fc6a74bc0143f67c60a488
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-13 22:50:25 -0500

    Created Payment class

---
+++ Assets/Scripts/Strategy/GameResources/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.GameResources
+{
+    public class Payment : IPayment
+    {
+        public IResource resourceType { get; set; }
+        public int cost { get; set; }
+    }
+}
