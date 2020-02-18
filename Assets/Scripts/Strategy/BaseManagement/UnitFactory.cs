fe3807ecec9edce75ee9d1f6f42d5cc695753d7b
cthomas68 <c.l.thomas68@gmail.com>
2020-02-17 23:28:19 -0500

    Update existing base man. files

--- Assets/Scripts/Strategy/BaseManagement/UnitFactory.cs
+++ Assets/Scripts/Strategy/BaseManagement/UnitFactory.cs
@@ -1,15 +1,33 @@
-﻿using System.Collections;
+﻿using SwordAndBored.GameData.Abilities.Skills;
+using SwordAndBored.GameData.Creatures;
+using SwordAndBored.GameData.Roles;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UnitFactory : MonoBehaviour
 {
-    public Text Role;
+    public Text UnitRole;
+
+    public Text UnitName;
+
+    private IDictionary<string, IRole> roleDict = new Dictionary<string, IRole>()
+    {
+        {"Warrior", new GenericRole(new SingleSelectSkillTree(5)) },
+        {"Mage", new GenericRole(new SingleSelectSkillTree(5)) },
+        {"Scout", new GenericRole(new SingleSelectSkillTree(5)) },
+    };
 
     public void TrainUnit()
-    { 
+    {
         // Create ICharacter to track in strategy view
         // Add record to database
+        // Add to strategy view tracker
+
+        IRole role = roleDict[UnitRole.text];
+        role.Name = UnitName.text;
+
+        ICharacter character = new GenericCharacter(null, role, null);
     }
 }
