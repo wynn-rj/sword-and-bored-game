66a3d1111f4fc5ec0a238b02a1bb6cf3602701a8
cthomas68 <c.l.thomas68@gmail.com>
2020-02-17 21:03:52 -0500

    Create unit factory script

---
+++ Assets/Scripts/Strategy/BaseManagement/UnitFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitFactory : MonoBehaviour
+{
+    public Text Role;
+
+    public void TrainUnit()
+    { 
+        // Create ICharacter to track in strategy view
+        // Add record to database
+    }
+}
