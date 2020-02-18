18570a3db6058d30e6166e4bcfdcbf0be9e0f5ed
cthomas68 <c.l.thomas68@gmail.com>
2020-02-17 23:27:51 -0500

    Create unit manager script

---
+++ Assets/Scripts/Strategy/BaseManagement/UnitManager.cs
@@ -0,0 +1,43 @@
+﻿using SwordAndBored.GameData.Creatures;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.BaseManagement
+{
+    public class UnitManager : MonoBehaviour
+    {
+        public IList<ICharacter> unitList;
+
+        private void Awake()
+        {
+            unitList = new List<ICharacter>();
+        }
+
+        /// <summary>
+        /// Retrieves all unit data from the database
+        /// </summary>
+        public void GetAllData()
+        {
+
+        }
+
+        /// <summary>
+        /// Writes all unit data to the database
+        /// </summary>
+        public void SetAllData()
+        {
+
+        }
+
+        public void RegisterUnit()
+        {
+
+        }
+
+        public IList<ICharacter> GetUnitsList()
+        {
+            return unitList;
+        }
+    }
+}
