dbbf06104b2226768dc43f8edcdf6ef860d1001f
ThBrokenToaster <ruch.jack@yahoo.com>
2020-02-18 21:11:19 -0500

    Added the framework for enemy spawning

---
+++ Assets/Scripts/Battlefield/EnemyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordAndBored.Battlefield.CreaturScripts;
+using SwordAndBored.Battlefield;
+using Cinemachine;
+using SwordAndBored.Battlefield.CameraUtilities;
+using SwordAndBored.Battlefield.TurnMechanism;
+using SwordAndBored.GameData.Database;
+using SwordAndBored.GameData.Database.Tables;
+
+
+namespace SwordAndBored.Battlefield
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        public GameObject playerPrefab;
+       public CameraManager camManager;
+        public TurnManager turnManager;
+        public Transform unitHolder;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
