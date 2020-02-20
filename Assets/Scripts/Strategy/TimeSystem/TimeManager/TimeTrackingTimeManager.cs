1bb71a68c4ee96d2574f0166f31d6a06954729f0
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 12:31:18 -0500

    Add a time manager that tracks the current turn

---
+++ Assets/Scripts/Strategy/TimeSystem/TimeManager/TimeTrackingTimeManager.cs
@@ -0,0 +1,24 @@
+﻿using SwordAndBored.Strategy.TimeSystem.Subscribers;
+
+namespace SwordAndBored.Strategy.TimeSystem.TimeManager
+{
+    class TimeTrackingTimeManager : AbstractTimeManager
+    {
+        public int TimeStep { get; protected set; } = 0;
+
+        public override void AdvanceTimeStep()
+        {
+            foreach (IPostTimeStepSubscriber postTimeStepSubscriber in postTimeStepSubscribers)
+            {
+                postTimeStepSubscriber.PostTimeStepUpdate();
+            }
+
+            TimeStep++;
+
+            foreach (IPreTimeStepSubscriber preTimeStepSubscriber in preTimeStepSubscribers)
+            {
+                preTimeStepSubscriber.PreTimeStepUpdate();
+            }
+        }
+    }
+}
