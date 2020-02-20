5c2e0f04f2b46defd7c4c7bab564b9edab157709
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 12:28:16 -0500

    Add observer pattern interface

---
+++ Assets/Scripts/Utilities/IObserver.cs
@@ -0,0 +1,34 @@
+﻿namespace SwordAndBored.Utilities
+{
+    interface IObserver<T>
+    {
+        /// <summary>
+        /// Adds the subscriber to the observer's list of objects to be notified
+        /// </summary>
+        /// <param name="subscriber">The object that is subscribing to the observer</param>
+        void Subscribe(T subscriber);
+
+        /// <summary>
+        /// Attempts to remove the subscriber from the list of subscribed objects
+        /// </summary>
+        /// <param name="subscriber">The object to remove</param>
+        /// <returns>Whether the object was successfully removed</returns>
+        bool Unsubscribe(T subscriber);
+    }
+
+    interface IObserver
+    {
+        /// <summary>
+        /// Adds the subscriber to the observer's list of objects to be notified
+        /// </summary>
+        /// <param name="subscriber">The object that is subscribing to the observer</param>
+        void Subscribe(object subscriber);
+
+        /// <summary>
+        /// Attempts to remove the subscriber from the list of subscribed objects
+        /// </summary>
+        /// <param name="subscriber">The object to remove</param>
+        /// <returns>Whether the object was successfully removed</returns>
+        bool Unsubscribe(object subscriber);
+    }
+}
