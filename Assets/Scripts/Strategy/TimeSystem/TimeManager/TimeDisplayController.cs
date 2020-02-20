90c07418d74bf5efe1f139c3dda1bcc6316b5cb1
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 16:07:49 -0500

    Add TimeDisplayController script

---
+++ Assets/Scripts/Strategy/TimeSystem/TimeManager/TimeDisplayController.cs
@@ -0,0 +1,36 @@
+﻿using SwordAndBored.Strategy.TimeSystem.Subscribers;
+using UnityEngine;
+using UnityEngine.UI;
+using SwordAndBored.Utilities.Debug;
+
+namespace SwordAndBored.Strategy.TimeSystem.TimeManager
+{
+    public class TimeDisplayController : MonoBehaviour, IPreTimeStepSubscriber
+    {
+        public TimeTrackingTimeManager timeManager;
+
+        private Text text;
+
+        void Start()
+        {
+            text = GetComponent<Text>();
+            AssertHelper.IsSetInEditor(timeManager, this);
+            timeManager.Subscribe(this);
+            text.text = string.Format("Turn: {0}", timeManager.TimeStep);            
+        }
+
+        public void PreTimeStepUpdate()
+        {
+            text.text = string.Format("Turn: {0}", timeManager.TimeStep);
+        }
+
+        public void OnDestroy()
+        {
+            if (timeManager)
+            {
+                timeManager.Unsubscribe(this);
+            }
+        }
+    }
+}
+
