328510c8b6ad1ec91f6232589ee9b16091445f43
cthomas68 <c.l.thomas68@gmail.com>
2020-02-19 21:08:44 -0500

    Create scriptable object units for UI entries

---
+++ Assets/Scripts/Strategy/BaseManagement/UnitEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit Base Entry", menuName = "Unit Entry")]
+public class UnitEntry : ScriptableObject
+{
+    public string RoleName;
+    public string RoleDescription;
+    public Sprite RoleArtwork;
+}
