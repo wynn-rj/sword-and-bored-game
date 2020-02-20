58bf9b73be42e2376c6db09b818219046e2dbd51
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 14:25:21 -0500

    Add is set in editor assert function

---
+++ Assets/Scripts/Utilities/Debug/AssertHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SwordAndBored.Utilities.Debug
+{
+    static class AssertHelper
+    {
+        /// <summary>
+        /// Checks to make sure an object has been set in the editor on debug builds
+        /// </summary>
+        /// <typeparam name="T">The type of object that is being checked</typeparam>
+        /// <param name="obj">The object that is being checked</param>
+        /// <param name="context">The unity object where the object being checked should have been set</param>
+        [Conditional("DEBUG")]
+        public static void IsSetInEditor<T>(T obj, UnityEngine.Object context) where T : class
+        {
+            if (obj is null)
+            {
+                UnityEngine.Debug.LogError(typeof(T).Name + " is not set for " + context.GetType().Name, context);
+            }
+        }
+    }
+}
