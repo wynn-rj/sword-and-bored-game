ce57575280391300e2160ddbf092ef40590ebeb1
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 16:59:34 -0500

    Add interface documentation

--- Assets/Scripts/Strategy/TimeSystem/Subscribers/IPostTimeStepSubscriber.cs
+++ Assets/Scripts/Strategy/TimeSystem/Subscribers/IPostTimeStepSubscriber.cs
@@ -1,7 +1,13 @@
 ﻿namespace SwordAndBored.Strategy.TimeSystem.Subscribers
 {
+    /// <summary>
+    /// A subscriber that is notified after a time step ends but before the next one starts
+    /// </summary>
     public interface IPostTimeStepSubscriber
     {
+        /// <summary>
+        /// The function the time manager calls after the time step ends but before the next one starts
+        /// </summary>
         void PostTimeStepUpdate();
     }
 }
--- Assets/Scripts/Strategy/TimeSystem/Subscribers/ITimeStepSubscriber.cs
+++ Assets/Scripts/Strategy/TimeSystem/Subscribers/ITimeStepSubscriber.cs
@@ -1,5 +1,8 @@
 ﻿namespace SwordAndBored.Strategy.TimeSystem.Subscribers
 {
+    /// <summary>
+    /// A subscriber that is notified when a time step ends and a new one starts
+    /// </summary>
     public interface ITimeStepSubscriber : IPostTimeStepSubscriber, IPreTimeStepSubscriber
     {
     }
