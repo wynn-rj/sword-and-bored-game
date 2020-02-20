b2344e83966e2927cbad32575438cb6af9cd2c58
Toeecko <rorygilliland@ameritech.net>
2020-02-20 12:05:01 -0500

    separates modifiers to attack and defense

--- Assets/Scripts/GameData/Modifiers/IModifier.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SwordAndBored.GameData.Modifiers
-{
-    interface IModifier
-    {
-        //TODO: Implement. 
-    }
-}
---
+++ Assets/Scripts/GameData/Modifiers/IModifierDefense.cs
@@ -0,0 +1,10 @@
+﻿namespace SwordAndBored.GameData.Modifiers
+{
+    public interface IModifierDefense
+    {
+        public int Fire_Resist { get; set; }
+        public int Posion_Resist { get; set; }
+        public int Bleed_Resist { get; set; }
+        public int Stun_Resist { get; set; }
+    }
+}
