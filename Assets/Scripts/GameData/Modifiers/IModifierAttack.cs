844dda2a72dacfb9bdbcf44f284af9597e06d47f
Toeecko <rorygilliland@ameritech.net>
2020-02-20 12:05:01 -0500

    separates modifiers to attack and defense

---
+++ Assets/Scripts/GameData/Modifiers/IModifierAttack.cs
@@ -0,0 +1,13 @@
+﻿namespace SwordAndBored.GameData.Modifiers
+{
+    public interface IModifierAttack
+    {
+        public int Fire_Damage { get; set; }
+        public int Fire_Chance { get; set; }
+        public int Posion_Damage { get; set; }
+        public int Posion_Chance { get; set; }
+        public int Bleed_Damage { get; set; }
+        public int Bleed_Chance { get; set; }
+        public int Stun_Chance { get; set; }
+    }
+}
