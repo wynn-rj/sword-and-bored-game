29ace6ec841deaed957468b0dab44e014bcd8fcb
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 12:30:13 -0500

    Create time manager interface

---
+++ Assets/Scripts/Strategy/TimeSystem/TimeManager/ITimeManager.cs
@@ -0,0 +1,10 @@
+﻿using SwordAndBored.Utilities;
+using SwordAndBored.Strategy.TimeSystem.Subscribers;
+
+namespace SwordAndBored.Strategy.TimeSystem.TimeManager
+{
+    interface ITimeManager : IObserver<ITimeStepSubscriber>, IObserver<IPreTimeStepSubscriber>, IObserver<IPostTimeStepSubscriber>
+    {
+        void AdvanceTimeStep();
+    }
+}
