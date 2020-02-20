9e56bed33904ad3c3dfe59dc66bbf89de7b5216d
Toeecko <rorygilliland@ameritech.net>
2020-02-20 12:25:49 -0500

    moves some equipment files to unused, separates equipment into weapon, armor, and spellbook types

---
+++ Assets/Scripts/GameData/Equipment/IWeapon.cs
@@ -0,0 +1,10 @@
+﻿using SwordAndBored.GameData.Abilities;
+using SwordAndBored.GameData.Modifiers;
+
+namespace SwordAndBored.GameData.Equipment
+{
+    interface IWeapon : IEquipment
+    {
+        public IAbility[] Abilities { get; set; }
+    }
+}
