b4da5ece91b4559e1b25b80b38e46723e303d235
cthomas68 <c.l.thomas68@gmail.com>
2020-02-19 21:08:44 -0500

    Create scriptable object units for UI entries

---
+++ Assets/Scripts/Strategy/BaseManagement/UnitEntryDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitEntryDisplay : MonoBehaviour
+{
+    public UnitEntry unitEntry;
+
+    public Text EntryRole;
+
+    public Text EntryDescription;
+
+    public Image EntryImage;
+
+    void Start()
+    {
+        EntryRole.text = unitEntry.RoleName;
+        EntryDescription.text = unitEntry.RoleDescription;
+        EntryImage.sprite = unitEntry.RoleArtwork;
+    }
+}
