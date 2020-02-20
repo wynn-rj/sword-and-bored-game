776ebe1ca0a744674d25d61c7cc5e02a6b4a809a
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 16:59:34 -0500

    Add interface documentation

--- Assets/Scripts/Strategy/TimeSystem/TimeManager/ITimeManager.cs
+++ Assets/Scripts/Strategy/TimeSystem/TimeManager/ITimeManager.cs
@@ -3,8 +3,14 @@ using SwordAndBored.Strategy.TimeSystem.Subscribers;
 
 namespace SwordAndBored.Strategy.TimeSystem.TimeManager
 {
+    /// <summary>
+    /// An observer that notifies subscribers when a time step ends and a new time step starts
+    /// </summary>
     public interface ITimeManager : IObserver<ITimeStepSubscriber>, IObserver<IPreTimeStepSubscriber>, IObserver<IPostTimeStepSubscriber>
     {
+        /// <summary>
+        /// End the current time step and go to the next one
+        /// </summary>
         void AdvanceTimeStep();
     }
 }
