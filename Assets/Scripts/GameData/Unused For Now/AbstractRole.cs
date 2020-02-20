9caca00dbaf6805dfac72daf93bf2282bfaa2dd4
Toeecko <rorygilliland@ameritech.net>
2020-02-20 12:04:20 -0500

    adds folder for unused files, files may be helpful in future, but overcomplicates things for now

---
+++ Assets/Scripts/GameData/Unused For Now/AbstractRole.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * using SwordAndBored.GameData.Abilities.Skills;
+
+namespace SwordAndBored.GameData.Roles
+{
+    abstract class AbstractRole : IRole
+    {
+        public abstract ISkillTree RoleSkillTree { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
+*/
---
+++ Assets/Scripts/GameData/Unused For Now/GenericRole.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * using SwordAndBored.GameData.Abilities.Skills;
+
+namespace SwordAndBored.GameData.Roles
+{
+    class GenericRole : AbstractRole
+    {
+        public override ISkillTree RoleSkillTree { get; }
+
+        public GenericRole(ISkillTree skillTree)
+        {
+            RoleSkillTree = skillTree;
+        }
+    }
+}
+*/
