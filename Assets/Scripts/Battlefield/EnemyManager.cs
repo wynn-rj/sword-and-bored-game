c98d40ef30a6901d1ae8139b7a468929b77ad47d
Jack Ruch <ruch.jack@yahoo.com>
2020-02-20 14:41:00 -0500

    Continued working on enemy spawning

--- Assets/Scripts/Battlefield/EnemyManager.cs
+++ Assets/Scripts/Battlefield/EnemyManager.cs
@@ -14,21 +14,73 @@ namespace SwordAndBored.Battlefield
 {
     public class EnemyManager : MonoBehaviour
     {
+        public int stageDifficulty;
+        public int numberOfEnemies;
+
+        List<GameObject> enemies = new List<GameObject>();
+
         public GameObject playerPrefab;
-       public CameraManager camManager;
+        public CameraManager camManager;
         public TurnManager turnManager;
         public Transform unitHolder;
-
-        // Start is called before the first frame update
+        
         void Start()
         {
-        
-        }
+            //Database connection goes here
+            DatabaseConnection conn = new DatabaseConnection();
+            DatabaseReader reader = conn.QueryAllFromTable("Units");
+            int numUnits = 0;
+            while (reader.NextRow())
+            {
+                UnitTable unitTable = new UnitTable(reader.GetIntFromCol("ID"));
 
-        // Update is called once per frame
+                GameObject unit = Instantiate(playerPrefab, new Vector3(numUnits, 1.5f, 5), Quaternion.identity);
+                UniqueCreature uniqueCreature = unit.GetComponent<UniqueCreature>();
+                UnitAbilitiesContainer abilities = unit.GetComponent<UnitAbilitiesContainer>();
+                UnitStats stats = unit.GetComponent<UnitStats>();
+                //TurnBrain brain = unit.GetComponent<TurnBrain>();
+                CinemachineVirtualCamera cam = uniqueCreature.currentCamera;
+                //camManager.cameras.Add(cam.gameObject);
+                //turnManager.units.Add(unit);
+                //turnManager.activePlayer = brain;
+
+                //UniqueCreature
+                uniqueCreature.creatureName = unitTable.Descriptor.Name;
+                uniqueCreature.maxHealth = unitTable.Stats.HP;
+                uniqueCreature.maxMovement = unitTable.Stats.Movement;
+
+                //abilities
+                for (int j = 0; j < 5; j++)
+                {
+                    //abilities.abilities.add();
+                }
+
+                //stats
+                stats.health = unitTable.Stats.HP;
+                stats.attack = unitTable.Stats.Physical_Attack;
+                stats.magicAttack = unitTable.Stats.Magic_Attack;
+                stats.defense = unitTable.Stats.Physical_Defense;
+                stats.magicDefense = unitTable.Stats.Magic_Defense;
+                stats.movement = unitTable.Stats.Movement;
+                stats.speedIntit = unitTable.Stats.Initiative;
+                stats.accuracy = unitTable.Stats.Accuracy;
+                stats.evasion = unitTable.Stats.Evasion;
+                stats.role = unitTable.Role.Descriptor.Name;
+
+                //brain
+                //brain.tileIndictor = indicator;
+                //sbrain.startCoordinates = new Vector2(numUnits, 5);
+
+                unit.transform.parent = unitHolder;
+
+                enemies.Add(unit);
+                numUnits += 2;
+            }
+        }
+        
         void Update()
         {
-        
+            
         }
     }
 }
