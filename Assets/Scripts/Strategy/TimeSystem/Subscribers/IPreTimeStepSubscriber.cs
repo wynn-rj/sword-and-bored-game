ee6238b5879ebcafed3ea22cf7c55663406807f0
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 16:59:34 -0500

    Add interface documentation

--- Assets/Scripts/Strategy/TimeSystem/Subscribers/IPreTimeStepSubscriber.cs
+++ Assets/Scripts/Strategy/TimeSystem/Subscribers/IPreTimeStepSubscriber.cs
@@ -1,7 +1,13 @@
 ﻿namespace SwordAndBored.Strategy.TimeSystem.Subscribers
 {
+    /// <summary>
+    /// A subscriber that is notified when a new time step starts
+    /// </summary>
     public interface IPreTimeStepSubscriber
     {
+        /// <summary>
+        /// The function the time manager calls when a new time step starts
+        /// </summary>
         void PreTimeStepUpdate();
     }
 }
