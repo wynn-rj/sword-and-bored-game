e242332143a68574e29a21ac9454b891371e2868
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 16:08:17 -0500

    Add TimeManagerKeyboardController script

---
+++ Assets/Scripts/Strategy/TimeSystem/TimeManager/TimeManagerKeyboardController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using SwordAndBored.Utilities.Debug;
+
+namespace SwordAndBored.Strategy.TimeSystem.TimeManager
+{
+    public class TimeManagerKeyboardController : MonoBehaviour
+    {
+        public TimeTrackingTimeManager timeManager;
+        public KeyCode nextTurnKey = KeyCode.Return;
+
+#if DEBUG
+        void Start()
+        {
+            AssertHelper.IsSetInEditor(timeManager, this);
+        }
+#endif
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(nextTurnKey))
+            {
+                timeManager.AdvanceTimeStep();
+            }
+        }
+    }
+}
