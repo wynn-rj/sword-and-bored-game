2311c6639c18c72fd14d406c8ca8b05774974cf7
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 18:43:00 -0500

    Add preliminary script for changing scenes

---
+++ Assets/Scripts/Strategy/Transitions/ToBaseTransitiion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using SwordAndBored.Strategy.TimeSystem.TimeManager;
+using SwordAndBored.Utilities.Debug;
+
+namespace SwordAndBored.Strategy.Transitions
+{
+    class ToBaseTransitiion : MonoBehaviour
+    {
+        public TimeTrackingTimeManager timeManager;
+
+#if DEBUG
+        void Awake()
+        {
+            AssertHelper.IsSetInEditor(timeManager, this);
+        }
+#endif
+
+        private void GoToBase()
+        {
+            SceneSharing.timeStep = timeManager.TimeStep;
+            SceneManager.LoadSceneAsync("BaseManagement", LoadSceneMode.Single);
+        }
+    }
+}
