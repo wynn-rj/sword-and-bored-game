55faf594cc35cd2ba23b721e6e04abad6123c7a0
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 22:57:25 -0500

    Update generation constants

--- Assets/Scripts/Strategy/ProceduralTerrain/Constants.cs
+++ Assets/Scripts/Strategy/ProceduralTerrain/Constants.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class Constants
 {
     public static readonly int hexRadius = 1;
     public static readonly int mapWidth = 200;
     public static readonly int mapHeight = 200;
-    public static readonly int mountainHeightThreshold = 7;
+    public static readonly int mountainHeightThreshold = 14;
     public static readonly int riverHeightThreshold = 7;
     public static readonly float riverWaterLevelThreshold = .8f;
     public static readonly float snowMountainWaterLvlThreshold = .9f;
     public static readonly float plainsWaterLevelThreshold = .8f;
     public static readonly float forestWaterLevelThreshold = .9f;
-    public static readonly int snowMountainHeightThreshold = 10;
+    public static readonly int snowMountainHeightThreshold = 20;
     public static readonly int xMargin = Mathf.FloorToInt(mapWidth * .05f);
     public static readonly int yMargin = Mathf.FloorToInt(mapHeight * .05f);
 }
