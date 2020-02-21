70d42e571da8fa38e6fe9e92673e8f25d92bdb0b
Robert J Wynn <wynn.134@osu.edu>
2020-02-21 01:20:20 -0500

    Create selection component

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/TileComponents/NestingSelectionActionComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents
+{
+    class NestingSelectionActionComponent : AbstractSelectionComponent
+    {
+        public IList<ISelectionComponent> InternalComponents { get; } = new List<ISelectionComponent>();
+        public override void Select()
+        {
+            foreach (ISelectionComponent selectionComponent in InternalComponents)
+            {
+                selectionComponent.Select();
+            }
+        }
+    }
+}
