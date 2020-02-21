532c1a2695232f58c3112e186c439cb60c03bf44
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 22:56:48 -0500

    Remove unneeded logic in terrain components

--- Assets/Scripts/Strategy/ProceduralTerrain/Map/Terrain/GenericTerrainComponent.cs
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/Terrain/GenericTerrainComponent.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Terrain
+﻿namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Terrain
 {
     class GenericTerrainComponent : Grid.AbstractCellComponent, ITerrainComponent
     {
