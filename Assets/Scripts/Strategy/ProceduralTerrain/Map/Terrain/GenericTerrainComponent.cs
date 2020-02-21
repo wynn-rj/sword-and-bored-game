75786edf9a0ae70dd63e952f52648f61951075e9
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 22:49:05 -0500

    Change abstract terrain tile to generic

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/Terrain/GenericTerrainComponent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Terrain
+{
+    class GenericTerrainComponent : Grid.AbstractCellComponent, ITerrainComponent
+    {
+        public int Height { get; set; }
+        public float WaterLevel { get; set; }
+    }
+}
