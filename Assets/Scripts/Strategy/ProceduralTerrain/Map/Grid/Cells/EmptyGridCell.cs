a591029742605b2a21a8c6864f61c398c828a82b
Robert J Wynn <wynn.134@osu.edu>
2020-02-21 01:20:57 -0500

    Use nesting selection component in empty grid

--- Assets/Scripts/Strategy/ProceduralTerrain/Map/Grid/Cells/EmptyGridCell.cs
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/Grid/Cells/EmptyGridCell.cs
@@ -1,4 +1,4 @@
-using System;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents;
 using System.Collections.Generic;
 
 namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells
@@ -10,26 +10,47 @@ namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells
         public IEnumerable<ICellComponent> Components => ComponentList;
 
         private List<ICellComponent> ComponentList;
+        private NestingSelectionActionComponent selectionComponents;
 
         public EmptyGridCell(int x, int y, float gridRadius)
         {
             Position = new HexPoint(x, y, gridRadius);
             ComponentList = new List<ICellComponent>();
+            selectionComponents = new NestingSelectionActionComponent();
+            ComponentList.Add(selectionComponents);
+            selectionComponents.Parent = this;
         }
 
         public void AddComponent(ICellComponent component)
         {
-            ComponentList.Add(component);
+            if (component is ISelectionComponent selectionComponent)
+            {
+                selectionComponents.InternalComponents.Add(selectionComponent);
+            }
+            else
+            {
+                ComponentList.Add(component);
+            }
             component.Parent = this;
         }
 
         public bool RemoveComponent(ICellComponent component)
         {
+            if (component is ISelectionComponent selectionComponent)
+            {
+                return selectionComponents.InternalComponents.Remove(selectionComponent);
+            }
             return ComponentList.Remove(component);
         }
 
         public bool RemoveComponent<T>() where T : ICellComponent
         {
+            if (typeof(T) is ISelectionComponent)
+            {
+                int length = selectionComponents.InternalComponents.Count;
+                selectionComponents.InternalComponents.Clear();
+                return length != 0;
+            }
             return ComponentList.Remove(GetComponent<T>());
         }        
 
