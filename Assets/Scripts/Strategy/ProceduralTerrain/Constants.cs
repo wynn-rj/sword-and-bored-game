e8cf8eb6e1e486db6dc45ee7dcbb1bb439a5d0ba
Robert J Wynn <wynn.134@osu.edu>
2020-02-20 23:41:55 -0500

    Fix Proper scaling

--- Assets/Scripts/Strategy/ProceduralTerrain/Constants.cs
+++ Assets/Scripts/Strategy/ProceduralTerrain/Constants.cs
@@ -2,9 +2,9 @@
 
 public static class Constants
 {
-    public static readonly int hexRadius = 1;
-    public static readonly int mapWidth = 200;
-    public static readonly int mapHeight = 200;
+    public static readonly int hexRadius = 2;
+    public static readonly int mapWidth = 100;
+    public static readonly int mapHeight = 100;
     public static readonly int mountainHeightThreshold = 14;
     public static readonly int riverHeightThreshold = 7;
     public static readonly float riverWaterLevelThreshold = .8f;
