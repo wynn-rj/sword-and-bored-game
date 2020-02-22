c6625db3bf2392b8963c88bac303b5a7be5eda8a
ThBrokenToaster <ruch.jack@yahoo.com>
2020-02-21 23:37:51 -0500

    Fixed shader and material issues

--- Assets/Scripts/Battlefield/CreatureScripts/UnitBase.cs
+++ Assets/Scripts/Battlefield/CreatureScripts/UnitBase.cs
@@ -51,7 +51,6 @@ namespace SwordAndBored.Battlefield.CreaturScripts
             if (currentTile)
             {
                 currentTile.unitOnTile = null;
-                currentTile = null;
             }
             agent.destination = pos;
         }
