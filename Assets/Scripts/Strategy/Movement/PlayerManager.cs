b8fcc7f08022e78992f1ce2649eae2c12e0c1f63
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-22 18:08:30 -0500

    Made player manager, cleaned up code

---
+++ Assets/Scripts/Strategy/Movement/PlayerManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordAndBored.Strategy.TimeSystem.TimeManager;
+using SwordAndBored.Strategy.TimeSystem.Subscribers;
+using SwordAndBored.Strategy.Movement;
+using SwordAndBored.Strategy.ProceduralTerrain;
+
+public class PlayerManager : MonoBehaviour, IPostTimeStepSubscriber
+{
+    public List<GameObject> children;
+    public Material defaultMaterial;
+    public Material selectedMaterial;
+    public TimeTrackingTimeManager turnManager;
+    public TileManager tileManager;
+
+    private GameObject selectedPlayer;
+
+    void Start()
+    {
+        Transform[] tList = gameObject.GetComponentsInChildren<Transform>();
+        foreach (Transform t in transform)
+        {
+            children.Add(t.gameObject);
+        }
+        turnManager.AddPostTimeStepSubscriber(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            OnClick();
+        }
+    }
+
+    void OnClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit) && children.Contains(hit.collider.gameObject))
+        {
+            selectedPlayer = hit.collider.gameObject;
+            selectedPlayer.GetComponent<MeshRenderer>().material = selectedMaterial;
+            foreach (GameObject g in children)
+            {
+                if (g != hit.collider.gameObject)
+                {
+                    g.GetComponent<MeshRenderer>().material = defaultMaterial;
+                }
+            }
+        }
+    }
+
+    public void SetPlayerAndTarget()
+    {
+        if (tileManager.GetComponent<TileSelect>().center != new Vector3(-50f, -50f, -50f) && !selectedPlayer.GetComponent<PlayerMove>().usedMoveThisTurn)
+        {
+            selectedPlayer.GetComponent<PlayerMove>().targetPosition = tileManager.GetComponent<TileSelect>().center;
+            selectedPlayer.GetComponent<Rigidbody>().isKinematic = false;
+            selectedPlayer.GetComponent<PlayerMove>().usedMoveThisTurn = true;
+            selectedPlayer.GetComponent<PlayerMove>().SetTargetPosition();
+        }
+        selectedPlayer.GetComponent<MeshRenderer>().material = defaultMaterial;
+    }
+
+    public void PostTimeStepUpdate()
+    {
+        foreach(GameObject g in children)
+        {
+            g.GetComponent<PlayerMove>().usedMoveThisTurn = false;
+        }
+    }
+}
--- Assets/Scripts/Strategy/Movement/PlayerMove.cs
+++ Assets/Scripts/Strategy/Movement/PlayerMove.cs
@@ -7,49 +7,25 @@ using SwordAndBored.Strategy.TimeSystem.TimeManager;
 using SwordAndBored.Strategy.TimeSystem.Subscribers;
 
 
-namespace SwordAndBored.StrategyView.Movement
+namespace SwordAndBored.Strategy.Movement
 {
-    public class PlayerMove : MonoBehaviour, IPostTimeStepSubscriber
+    public class PlayerMove : MonoBehaviour//, IPostTimeStepSubscriber
     {
         public TileManager tileManager;
-        public Material selectedMaterial;
-        public TimeTrackingTimeManager turnManager;
+        public bool usedMoveThisTurn = false;
 
-        Vector3 targetPosition;
+        public Vector3 targetPosition;
         Vector3 lookAtTarget;
         Quaternion playerRot;
         float rotSpeed = 5;
         float speed = 10;
         bool moving = false;
 
-        bool usedMoveThisTurn = false;
-        
-        public Material defaultMaterial;
-
-        void Start()
-        {
-            turnManager.AddPostTimeStepSubscriber(this);
-        }
-
         void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                OnClick();
-            }
             if (moving)
             {
                 Move();
-            }
-        }
-
-        void OnClick()
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if(Physics.Raycast(ray, out RaycastHit hit) && hit.collider.gameObject == this.gameObject)
-            {
-                this.GetComponent<MeshRenderer>().material = selectedMaterial;
             }
         }
 
@@ -58,21 +34,10 @@ namespace SwordAndBored.StrategyView.Movement
         /// </summary>
         public void SetTargetPosition()
         {
-            this.GetComponent<MeshRenderer>().material = defaultMaterial;
-
-            //New logic needed here, something to keep the target from being a null reference
-            if (tileManager.GetComponent<TileSelect>().center != new Vector3(-50f,-50f,-50f) && !usedMoveThisTurn)
-            {
-                targetPosition = tileManager.GetComponent<TileSelect>().center;
-                this.GetComponent<Rigidbody>().isKinematic = false;
-
-                usedMoveThisTurn = true;
-
                 this.transform.LookAt(targetPosition);
                 lookAtTarget = new Vector3(targetPosition.x - transform.position.x, transform.position.y, targetPosition.z - transform.position.z);
                 playerRot = Quaternion.LookRotation(lookAtTarget);
                 moving = true;
-            }
         }
 
         /// <summary>
@@ -97,12 +62,6 @@ namespace SwordAndBored.StrategyView.Movement
                 this.GetComponent<Rigidbody>().isKinematic = true;
             }
         }
-
-        public void PostTimeStepUpdate()
-        {
-            usedMoveThisTurn = false;
-        }
-
     }
 
 }
