cb27d86d3a46f3fffe5e37b9dcd10e43274fc930
gdiago <gonzalodiago@gmail.com>
2020-02-22 00:57:28 -0500

    Add translation and zoom functionality to free cam

--- Assets/Scripts/Battlefield/Camera/FreeCameraManager.cs
+++ Assets/Scripts/Battlefield/Camera/FreeCameraManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,4 +22,84 @@ public class FreeCameraManager : MonoBehaviour
     public float zoomSpeed = 4f;
     public float nearZoomLimit = 2f;
     public float farZoomLimit = 16f;
+    public float startingZoom = 5f;
+
+    IZoomStrategy zoomStrategy;
+    Vector3 frameMove;
+    float frameRotate;
+    float frameZoom;
+    CinemachineVirtualCamera cam;
+
+    private void Awake()
+    {
+        cam = GetComponentInChildren<CinemachineVirtualCamera>();
+        cam.transform.localPosition = new Vector3(0f, Mathf.Abs(cameraOffset.y), -Mathf.Abs(cameraOffset.x));
+        zoomStrategy = new OrthographicZoomStrategy(cam, startingZoom);
+        cam.transform.LookAt(transform.position + Vector3.up * lookAtOffset);
+    }
+
+    private void OnEnable()
+    {
+        KeyboardInputManager.OnMoveInput += UpdateFrameMove;
+        KeyboardInputManager.OnRotateInput += UpdateFrameRotate;
+        KeyboardInputManager.OnZoomInput += UpdateFrameZoom;
+    }
+
+    private void OnDisable()
+    {
+        KeyboardInputManager.OnMoveInput -= UpdateFrameMove;
+        KeyboardInputManager.OnRotateInput -= UpdateFrameRotate;
+        KeyboardInputManager.OnZoomInput -= UpdateFrameZoom;
+    }
+
+    private void UpdateFrameZoom(float zoomAmount)
+    {
+        frameZoom += zoomAmount;
+    }
+
+    private void UpdateFrameRotate(float rotateAmount)
+    {
+        frameRotate += rotateAmount;
+    }
+
+    private void UpdateFrameMove(Vector3 moveVector)
+    {
+        frameMove += moveVector;
+    }
+
+    private void LateUpdate()
+    {
+        if(frameMove != Vector3.zero)
+        {
+            Vector3 speedModFrameMove = new Vector3(frameMove.x * lateralSpeed, frameMove.y, frameMove.z * inOutSpeed);
+            transform.position += transform.TransformDirection(speedModFrameMove) * Time.deltaTime;
+            LockPositionInBounds();
+            frameMove = Vector3.zero;
+        }
+
+        if(frameRotate != 0f)
+        {
+            transform.Rotate(Vector3.up, frameRotate * Time.deltaTime * rotateSpeed);
+        }
+
+        if(frameZoom < 0f)
+        {
+            zoomStrategy.ZoomIn(cam, Time.deltaTime * Mathf.Abs(frameZoom) * zoomSpeed, nearZoomLimit);
+        }
+
+        if(frameZoom > 0f)
+        {
+
+        }
+
+    }
+
+    private void LockPositionInBounds()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, minBounds.x, maxBounds.x),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z, minBounds.y,maxBounds.y)
+            );
+    }
 }
