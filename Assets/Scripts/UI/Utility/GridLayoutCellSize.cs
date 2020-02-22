d5df31858e9294895661bae38f17c7e064248a7d
cthomas68 <c.l.thomas68@gmail.com>
2020-02-22 17:06:28 -0500

    Update UI script namespaces

---
+++ Assets/Scripts/UI/Utility/GridLayoutCellSize.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwordAndBored.UI.Utility
+{
+    public class GridLayoutCellSize : MonoBehaviour
+    {
+        public int numberOfCells;
+        public int numberOfColumns;
+
+        private float width;
+        private float height;
+
+        private int padding;
+
+        private RectTransform rectTransform;
+
+        void Start()
+        {
+            numberOfCells = 6;
+            numberOfColumns = 3;
+            padding = 10;
+
+            rectTransform = gameObject.GetComponent<RectTransform>();
+            width = rectTransform.rect.width;
+            height = rectTransform.rect.height;
+
+            int numberOfRows = Mathf.CeilToInt(numberOfCells / numberOfColumns);
+
+            Vector2 newCellSize = new Vector2(Mathf.CeilToInt(width / numberOfColumns) - (1.5f * padding), Mathf.CeilToInt(height / numberOfRows) - padding);
+            gameObject.GetComponent<GridLayoutGroup>().cellSize = newCellSize;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Utility/OnHoverGrowUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SwordAndBored.UI.Utility
+{
+    public class OnHoverGrowUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private Vector3 Scale;
+
+        private Vector3 originalScale;
+        private RectTransform rt;
+
+        private void Awake()
+        {
+            Scale = transform.localScale + new Vector3(0.01f, 0.01f, 0.01f);
+            rt = gameObject.GetComponent<RectTransform>();
+            originalScale = transform.localScale;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            transform.localScale = Scale;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.localScale = originalScale;
+        }
+    }
+}
