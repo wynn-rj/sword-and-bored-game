d5f6605a21759b4fb0e72309fcb0ca462877399c
gdiago <gonzalodiago@gmail.com>
2020-02-22 00:23:52 -0500

    Adds keyboard input manager for free cam

--- Assets/Scripts/Battlefield/Camera/Input/KeyboardInputManager.cs
+++ Assets/Scripts/Battlefield/Camera/Input/KeyboardInputManager.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyboardInputManager : MonoBehaviour
+public class KeyboardInputManager : InputManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static event MoveInputHandler OnMoveInput;
+    public static event RotateInputHandler OnRotateInput;
+    public static event ZoomInputHandler OnZoomInput;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Camera Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            OnMoveInput?.Invoke(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            OnMoveInput?.Invoke(Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            OnMoveInput?.Invoke(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            OnMoveInput?.Invoke(Vector3.right);
+        }
+
+        //Camera Rotation
+        if (Input.GetKey(KeyCode.Q))
+        {
+            OnRotateInput?.Invoke(1f);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnRotateInput?.Invoke(-1f);
+        }
+
+        //Camera Zoom
+        if (Input.GetKey(KeyCode.Z))
+        {
+            OnRotateInput?.Invoke(-1f);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            OnRotateInput?.Invoke(1f);
+        }
     }
 }
