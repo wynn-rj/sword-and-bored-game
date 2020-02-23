5da64a93b85fde17f93d6500a9b480ea8858d15a
Robert J Wynn <wynn.134@osu.edu>
2020-02-23 16:25:14 -0500

    Add enemy collision detector

---
+++ Assets/Scripts/Strategy/Transitions/EnemyEncounterDetector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using SwordAndBored.SceneManagement;
+
+namespace SwordAndBored.Strategy.Transitions
+{
+    public class EnemyEncounterDetector : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.name);
+            if (other.name.Contains("Player"))
+            {
+                SceneManager.LoadSceneAsync(GameScenes.BATTLEFIELD);
+            }
+        }
+    }
+}
