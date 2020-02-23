453f4da1c09fac1fd73240563825f1a4cce23ef4
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-22 23:28:08 -0500

    Copied over some work from other branch

---
+++ Assets/Scripts/Strategy/GameResources/ResourceDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.GameResources
+{
+    public class ResourceDisplay : MonoBehaviour
+    {
+        public ResourceSubscriber resourceSubscriber;
+        public Text text;
+
+        void UpdateDisplay()
+        {
+            text.text = "Gold: " + ResourceSubscriber.Amount();
+        }
+    }
+}
