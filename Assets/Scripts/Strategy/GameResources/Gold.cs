91624e21254533158e628adcc54df7b7b6c6b9de
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-23 00:31:40 -0500

    Added objects to scene to test display

--- Assets/Scripts/Strategy/GameResources/Gold.cs
+++ Assets/Scripts/Strategy/GameResources/Gold.cs
@@ -42,10 +42,10 @@ namespace SwordAndBored.Strategy.GameResources
 
         void UpdateSubscribers()
         {
-            foreach (IResourceSubscriber rs in Subscribers)
+            /*foreach (IResourceSubscriber rs in Subscribers)
             {
                 rs.UpdateAmount();
-            }
+            }*/
         }
     }
 }
