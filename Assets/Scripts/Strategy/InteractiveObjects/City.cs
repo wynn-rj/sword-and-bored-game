6f74ab432e790aa6e28d3c3c91852e011db8f088
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-23 17:23:01 -0500

    Simple city that adds gold

---
+++ Assets/Scripts/Strategy/InteractiveObjects/City.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordAndBored.Strategy.GameResources;
+
+namespace SwordAndBored.Strategy.InteractiveObjects
+{
+    public class City : MonoBehaviour, IInteractiveObject
+    {
+        public Gold gold;
+
+        public void addGold()
+        {
+            gold.Amount += 100;
+        }
+
+        //collision detection in here or on player to call addGold() method
+        //This class can be reworked to add other resource/item additions if needed
+    }
+}
