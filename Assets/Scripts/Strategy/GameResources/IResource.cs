54eebe775ced1fb30d5499d33cbd53efd90111f2
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-02-23 00:31:40 -0500

    Added objects to scene to test display

--- Assets/Scripts/Strategy/GameResources/IResource.cs
+++ Assets/Scripts/Strategy/GameResources/IResource.cs
@@ -6,7 +6,7 @@ namespace SwordAndBored.Strategy.GameResources
 {
     public interface IResource
     {
-        List<IResourceSubscriber> Subscribers { get; set; }
+        //List<IResourceSubscriber> Subscribers { get; set; }
         //The amount of resource you have
         int Amount { get; set; }
 
@@ -14,5 +14,6 @@ namespace SwordAndBored.Strategy.GameResources
          * returns true if the Purchase can be afforded
          */
         bool CanAffordPurchase(IPayment payment);
+        void AddSubscriber(IResourceSubscriber resourceSubscriber);
     }
 }
