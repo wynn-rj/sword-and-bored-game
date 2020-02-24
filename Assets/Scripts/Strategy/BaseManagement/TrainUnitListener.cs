b22cf288c111719725812ec1282cf7498c515c5f
cthomas68 <c.l.thomas68@gmail.com>
2020-02-23 19:54:18 -0500

    Add listener for training units

---
+++ Assets/Scripts/Strategy/BaseManagement/TrainUnitListener.cs
@@ -0,0 +1,39 @@
+﻿using SwordAndBored.StrategyView.BaseManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwordAndBored.Strategy.BaseManagement
+{
+    public class TrainUnitListener : MonoBehaviour
+    {
+        public UnitFactory UnitFactory;
+        public Button confirmButton;
+        public Text UnitRole;
+        public InputField UnitName;
+
+        private Button trainButton;
+
+        void Start()
+        {
+            trainButton = GetComponent<Button>();
+            trainButton.onClick.AddListener(() => StageUnitForTraining());
+
+            confirmButton.onClick.AddListener(() => ConfirmUnitTraining());
+        }
+
+        void StageUnitForTraining()
+        {    
+            UnitFactory.UnitRole = UnitRole.text;
+            UnitFactory.StageUnitForTraining();
+        }
+
+        void ConfirmUnitTraining()
+        {
+            UnitFactory.UnitName = UnitName.text;
+            UnitFactory.ConfirmUnitTraining();
+            UnitName.text = "";
+        }
+    }
+}
