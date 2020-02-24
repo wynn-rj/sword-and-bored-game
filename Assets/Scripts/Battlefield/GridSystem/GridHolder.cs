f227afdd750a3c0646f4055612f01de3abff2720
ThBrokenToaster <ruch.jack@yahoo.com>
2020-02-23 23:47:48 -0500

    Merged everyones changes into the town map scene

--- Assets/Scripts/Battlefield/GridSystem/GridHolder.cs
+++ Assets/Scripts/Battlefield/GridSystem/GridHolder.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class GridHolder : MonoBehaviour
 {
-    public Tile[,] tiles = new Tile[50, 50];
-    
+    public int size = 50;
+
+    public Tile[,] tiles;
+
+    private void Start()
+    {
+        int k = 0;
+        tiles = new Tile[size, size];
+        Tile[] children = GetComponentsInChildren<Tile>();
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size && k < children.Length; j++)
+            {
+                tiles[i, j] = children[k];
+                k++;
+            }
+        }    
+    }
 }
