e93171dfedd6f34d059d44892890f378054ef874
cthomas68 <c.l.thomas68@gmail.com>
2020-02-23 19:48:54 -0500

    Add GUI interfaces

---
+++ Assets/Scripts/UI/Base GUI/ICanvas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SwordAndBored.UI.BaseGUI
+{
+    public interface ICanvas
+    {
+        GameObject Canvas { get; set; }
+
+        void LoadCanvas();
+
+        void UnloadCanvas();
+    }
+}
