6836aba5aaa9726fea7135935ecc30800628041e
ThBrokenToaster <ruch.jack@yahoo.com>
2020-02-23 23:47:48 -0500

    Merged everyones changes into the town map scene

--- Assets/Scripts/SceneManagement/GameScenes.cs
+++ Assets/Scripts/SceneManagement/GameScenes.cs
@@ -2,7 +2,7 @@
 {
     static class GameScenes
     {
-        public const string BATTLEFIELD = "AddStatsUI";
+        public const string BATTLEFIELD = "Town Map";
         public const string STRATEGYMAP = "StrategyMap";
         public const string BASEVIEW = "BaseManagement";
         public const string MAINMENU = "MainMenu";
