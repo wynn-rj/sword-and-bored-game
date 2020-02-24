842e10ccbafc3d154d52b7d42dcaca66050989ed
cthomas68 <c.l.thomas68@gmail.com>
2020-02-23 19:51:39 -0500

    Add temporary set inactive script

---
+++ Assets/Scripts/Strategy/BaseManagement/SetInactive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.BaseManagement
+{
+    public class SetInactive : MonoBehaviour
+    {
+        public void ToggleOwnActivity()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
