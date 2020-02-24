826ebfcdf0dcfeb83e589d017aea35f9ba0c14da
cthomas68 <c.l.thomas68@gmail.com>
2020-02-23 19:48:54 -0500

    Add GUI interfaces

---
+++ Assets/Scripts/UI/Base GUI/IClickable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwordAndBored.UI.BaseGUI
+{
+    public interface IClickable
+    {
+        Button ClickableObject { get; set; }
+
+        void OnClick();
+
+        void AddListener(Action action);
+
+        void AddListener(Action<int> action, int index);
+
+        void AddListener(Action<GameObject> func, GameObject arg);
+    }
+}
