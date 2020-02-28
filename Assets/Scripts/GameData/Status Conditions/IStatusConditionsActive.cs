37aa9463832e9f2b3c9461d65a51b92db46991d0
Toeecko <rorygilliland@ameritech.net>
2020-02-28 10:46:33 -0500

    adds active status condition interface

---
+++ Assets/Scripts/GameData/Status Conditions/IStatusConditionsActive.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SwordAndBored.GameData.StatusConditions
+{
+    interface IStatusConditionsActive : IDatabaseObject
+    {
+        public bool IsStunned { get; set; }
+        public bool IsOnFire { get; set; }
+        public int FireDamage { get; set; }
+        public bool IsPoisoned { get; set; }
+        public int PoisonDamage { get; set; }
+        public bool IsBleeding { get; set; }
+        public int BleedDamage { get; set; }
+    }
+}
