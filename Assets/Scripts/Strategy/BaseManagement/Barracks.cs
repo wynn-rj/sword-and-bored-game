b8415a2f148e99a70c08ffe984e44cfdabe33156
Toeecko <rorygilliland@ameritech.net>
2020-02-29 14:56:49 -0500

    Removes old namespace

---
+++ Assets/Scripts/Strategy/BaseManagement/Barracks.cs
@@ -0,0 +1,21 @@
+﻿//using SwordAndBored.GameData.Creatures;
+using SwordAndBored.GameData.Units;
+using SwordAndBored.Strategy.BaseManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.BaseManagement.Buildings
+{
+    public class Barracks : GenericStrongholdCell
+    {
+        private IList<IUnit> buildableUnits;
+
+        private void Awake()
+        {
+            GameObject newCanvas = Instantiate(Canvas, Vector3.zero, Quaternion.identity);
+            SetCanvas(newCanvas);
+            Canvas.gameObject.SetActive(false);     
+        }
+    }
+}
