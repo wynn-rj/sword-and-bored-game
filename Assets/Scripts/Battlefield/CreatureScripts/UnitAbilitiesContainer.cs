78b789ebf04e4d7feb24b30d10a7a7a74ad5f755
Jack Ruch <ruch.jack@yahoo.com>
2020-02-29 14:17:36 -0500

    basic ability is functional

--- Assets/Scripts/Battlefield/CreatureScripts/UnitAbilitiesContainer.cs
+++ Assets/Scripts/Battlefield/CreatureScripts/UnitAbilitiesContainer.cs
@@ -23,6 +23,7 @@ namespace SwordAndBored.Battlefield.CreaturScripts
         {
             unit.animator.SetTrigger("Attack");
             abilities[i].TriggerAbility(target);
+            Debug.Log(abilities[i].AttackName);
             unit.action = false;
         }
     }
