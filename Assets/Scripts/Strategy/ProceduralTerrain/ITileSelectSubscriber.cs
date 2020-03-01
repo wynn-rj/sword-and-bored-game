0a70105ff0e8a15336356f3cf9068bb119bc545a
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:42:17 -0500

    Add a subscriber for when a tile is selected

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/ITileSelectSubscriber.cs
@@ -0,0 +1,9 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain
+{
+    public interface ITileSelectSubscriber
+    {
+        void OnTileSelect(IHexGridCell selectedTile);
+    }
+}
