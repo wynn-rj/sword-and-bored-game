1fbe96c7566b0e367ee546a97cf14eb3745a2147
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:31:15 -0500

    Make creep not a terrain component
    Creep is now an addition to the terrain component and can be toggled

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/Terrain/CreepComponent.cs
@@ -0,0 +1,45 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Terrain
+{
+    class CreepComponent : AbstractCellComponent
+    {
+        
+        public bool IsCreepActive
+        {
+            get => isCreepActive;
+            set => ChangeCreepState(value);
+        }
+
+        private GameObject tileHolder;
+        private bool isCreepActive;
+
+        public CreepComponent(bool isCreepActive)
+        {
+            this.isCreepActive = isCreepActive;
+        }
+
+        public void SetTileHolder(GameObject tileHolder)
+        {
+            this.tileHolder = tileHolder;
+            ChangeCreepState(isCreepActive);
+        }
+
+        private void ChangeCreepState(bool isActive)
+        {
+            isCreepActive = isActive;
+            foreach (Transform child in tileHolder.transform)
+            {
+                if (child.name == Constants.creepObjectName)
+                {
+                    child.gameObject.SetActive(isActive);
+                } 
+                else if (child.name == Constants.terrainObjectName)
+                {
+                    child.gameObject.SetActive(!isActive);
+                }
+            }
+        }
+    }
+}
