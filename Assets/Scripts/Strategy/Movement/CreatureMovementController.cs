fd8c4a287d49cebc7cf832820900e842dbce3073
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:48:46 -0500

    Create creature movement controller

---
+++ Assets/Scripts/Strategy/Movement/CreatureMovementController.cs
@@ -0,0 +1,129 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents;
+using SwordAndBored.Strategy.TimeSystem.Subscribers;
+using SwordAndBored.Utilities.Debug;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.Movement
+{
+    public abstract class CreatureMovementController : MonoBehaviour, ITimeStepSubscriber
+    {
+        private const float SPEED = 10;
+               
+        protected readonly Queue<IHexGridCell> path = new Queue<IHexGridCell>();
+        protected CreatureComponent creatureComponent;
+
+        [SerializeField] private int distanceCanTravel;
+        private Vector3? targetPosition;
+        private float height;
+        private static readonly object nextLocationLock = new object();
+
+        public IHexGridCell StartLocation { private get; set; }
+
+        public IHexGridCell Location { get; protected set; }
+
+        protected abstract int ResetMovement();
+
+        public virtual void PreTimeStepUpdate()
+        {
+            distanceCanTravel = ResetMovement();
+        }
+
+        public virtual void PostTimeStepUpdate()
+        {
+            TraversePath();
+            while (!(targetPosition is null)) ;
+        }
+
+        protected virtual void TraversePath()
+        {
+            if (distanceCanTravel == 0 || path.Count == 0)
+            {
+                targetPosition = null;
+                return;
+            }
+
+            if (!SafeUpdateLocation(path.Dequeue()))
+            {
+                Debug.Log("Movement blocked");
+                return;
+            }
+            distanceCanTravel--;
+            targetPosition = Location.Position.CenterAsVector3(height);
+        }
+        
+        protected virtual void Start()
+        {
+            AssertHelper.Assert(StartLocation != null, "No start location given", this);
+            AssertHelper.Assert(!StartLocation.HasComponent<CreatureComponent>(), 
+                "Tried to place creature on creature at " + StartLocation.Position, this);
+            UpdateLocation(StartLocation);
+            targetPosition = null;
+            height = transform.position.y;
+            distanceCanTravel = ResetMovement();
+        }
+
+        protected virtual void Update()
+        {
+            if (!(targetPosition is null))
+            {
+                MoveTowardsTarget();
+            }
+        }
+
+        protected virtual bool UpdateLocation(IHexGridCell location)
+        {
+            if (location.HasComponent<CreatureComponent>())
+            {
+                return false;
+            }
+            if (Location != null)
+            {
+                AssertHelper.Assert(creatureComponent != null, "Missing creature component", this);
+                bool success = Location.RemoveComponent(creatureComponent);
+                AssertHelper.Assert(success, "Failed to remove creature component", this);
+            }
+            else
+            {
+                creatureComponent = new CreatureComponent(this);
+            }
+            Location = location;
+            Location.AddComponent(creatureComponent);
+            return true;
+        }
+
+        protected void OnDestroy()
+        {
+            Location.RemoveComponent(creatureComponent);
+        }
+
+        protected virtual void ArrivedAtNewLocation() { }
+
+        private void MoveTowardsTarget()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition.Value, SPEED * Time.deltaTime);
+            transform.LookAt(targetPosition.Value);
+
+            if (transform.position == targetPosition)
+            {
+                ArrivedAtNewLocation();
+                TraversePath();
+            }
+        }
+
+        private bool SafeUpdateLocation(IHexGridCell location)
+        {
+            Monitor.Enter(nextLocationLock);
+            try
+            {
+                return UpdateLocation(location);    
+            } 
+            finally
+            {
+                Monitor.Exit(nextLocationLock);
+            }
+        }
+    }
+}
