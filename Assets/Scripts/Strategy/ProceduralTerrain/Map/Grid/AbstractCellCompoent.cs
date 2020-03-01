2061903ede76abb48e329a2112c61b7b6742db58
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:33:15 -0500

    Add overridable OnParentSet hook to abstract component

--- Assets/Scripts/Strategy/ProceduralTerrain/Map/Grid/AbstractCellCompoent.cs
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/Grid/AbstractCellCompoent.cs
@@ -4,6 +4,19 @@ namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Grid
 {
     public abstract class AbstractCellComponent : ICellComponent
     {
-        public IHexGridCell Parent { get; set; }
+        private IHexGridCell parent;
+
+        public IHexGridCell Parent
+        {
+            get => parent;
+            set
+            {
+                IHexGridCell oldParent = parent;
+                parent = value;
+                OnParentSet(oldParent);
+            }
+        }
+
+        protected virtual void OnParentSet(IHexGridCell oldParent) { }
     }
 }
