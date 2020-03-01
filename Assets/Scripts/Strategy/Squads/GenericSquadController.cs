c91b6ee44f8af12a13d7c1e56a9059981463f8c9
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:49:28 -0500

    Create squad movement controller

---
+++ Assets/Scripts/Strategy/Squads/GenericSquadController.cs
@@ -0,0 +1,13 @@
+﻿using SwordAndBored.GameData.Units;
+using SwordAndBored.Strategy.Movement;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+
+namespace SwordAndBored.Strategy.Squads
+{
+    public abstract class GenericSquadController : CreatureMovementController
+    {
+        public IUnit[] Units { get; set; }
+
+        public abstract void GoTo(IHexGridCell location);
+    }
+}
---
+++ Assets/Scripts/Strategy/Squads/SquadController.cs
@@ -0,0 +1,53 @@
+﻿using SwordAndBored.Strategy.Movement;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents;
+using SwordAndBored.Utilities.Debug;
+
+namespace SwordAndBored.Strategy.Squads
+{
+    public class SquadController : GenericSquadController
+    {
+        public override void GoTo(IHexGridCell location)
+        {
+            //Make A* call here
+            path.Enqueue(location);
+            TraversePath();
+        }
+
+        protected override int ResetMovement()
+        {
+            //TODO: Get squad average speed
+            return 3;
+        }
+
+        protected override bool UpdateLocation(IHexGridCell location)
+        {
+            CreatureComponent otherCreature = location.GetComponent<CreatureComponent>();
+            if (!(otherCreature is null) && !(otherCreature.Creature is EnemyMovementController))
+            {
+                return false;
+            }
+            if (Location != null)
+            {
+                AssertHelper.Assert(creatureComponent != null, "Missing creature component", this);
+                bool success = Location.RemoveComponent(creatureComponent);
+                AssertHelper.Assert(success, "Failed to remove creature component", this);
+            }
+            else
+            {
+                creatureComponent = new CreatureComponent(this);
+            }
+            Location = location;
+            Location.AddComponent(creatureComponent);
+            return true;
+        }
+
+#if DEBUG 
+        protected override void Start()
+        {
+            base.Start();
+            AssertHelper.Assert(Units != null && Units.Length > 0, "Squad controller has no squad", this);
+        }
+#endif
+    }
+}
