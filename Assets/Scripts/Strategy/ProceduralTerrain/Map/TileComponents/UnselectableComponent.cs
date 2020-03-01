337e33c029913bc46ce3cb174f9af62b1ad6a094
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:29:28 -0500

    Create an unselectable tile

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/TileComponents/UnselectableComponent.cs
@@ -0,0 +1,11 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents
+{
+    /// <summary>
+    /// A cell that can't be selected
+    /// </summary>
+    class UnselectableComponent : AbstractCellComponent
+    {
+    }
+}
