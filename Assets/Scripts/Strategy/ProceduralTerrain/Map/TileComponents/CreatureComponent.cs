24c74854ac1ad1d3b43bd07bcd5d6fb0a4251921
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:51:43 -0500

    Add component to track creatures on hex

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/TileComponents/CreatureComponent.cs
@@ -0,0 +1,15 @@
+﻿using SwordAndBored.Strategy.Movement;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents
+{
+    public class CreatureComponent : AbstractCellComponent
+    {
+        public CreatureMovementController Creature { get; }
+
+        public CreatureComponent(CreatureMovementController creature)
+        {
+            Creature = creature;
+        }
+    }
+}
