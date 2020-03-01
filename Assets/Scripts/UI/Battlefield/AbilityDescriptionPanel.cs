0f2ea362f8e7e7b90e9f9645b8acc3d0a6b13589
Toeecko <rorygilliland@ameritech.net>
2020-02-29 21:01:04 -0500

    adds  more to descrition panel text

--- Assets/Scripts/UI/Battlefield/AbilityDescriptionPanel.cs
+++ Assets/Scripts/UI/Battlefield/AbilityDescriptionPanel.cs
@@ -4,12 +4,20 @@ using TMPro;
 
 public class AbilityDescriptionPanel : MonoBehaviour
 {
-    public TMP_Text nameText;
-    public TMP_Text descriptionText;
+    public TMP_Text nameText, descriptionText, damageText, rangeText, accuracyText;
 
     public void DisplayDataInUI(Ability ability)
     {
         nameText.text = ability.name;
         descriptionText.text = ability.description;
+        if(ability.aoe)
+        {
+            rangeText.text = $"{ability.range} {ability.length}x{ability.width} Area";
+        } else
+        {
+            rangeText.text = $"{ability.range} Single Target";
+        }
+        accuracyText.text = ability.accuraccy.ToString();
+        damageText.text = ability.damage.ToString();
     }
 }
