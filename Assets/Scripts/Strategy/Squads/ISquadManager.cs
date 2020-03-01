0a56bf2c683de2747b5fad7fcd66b34dd1a8fdf5
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:47:49 -0500

    Create squad manager interface

---
+++ Assets/Scripts/Strategy/Squads/ISquadManager.cs
@@ -0,0 +1,25 @@
+﻿using SwordAndBored.GameData.Units;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+
+namespace SwordAndBored.Strategy.Squads
+{
+    interface ISquadManager
+    {
+        GenericSquadController SelectedSquad { get; }
+
+        /// <summary>
+        /// Create a squad of units on a give hex cell
+        /// </summary>
+        /// <param name="units">The units in the squad</param>
+        /// <param name="location">The cell to place the squad on</param>
+        /// <returns>The squad game object</returns>
+        GenericSquadController DeploySquad(IUnit[] units, IHexGridCell location);
+
+        /// <summary>
+        /// Retrieves a squad of units and destroys the squad
+        /// </summary>
+        /// <param name="location">The cell the squad is on</param>
+        /// <returns>The units that were in the squad</returns>
+        IUnit[] CollectSquad(IHexGridCell location);
+    }
+}
