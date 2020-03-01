f824e088d62fb79f3e1cae3d1b9e21ae43358d33
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:29:28 -0500

    Create an unselectable tile

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/Terrain/AbstractUnselectableTerrainComponent.cs
@@ -0,0 +1,17 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Terrain
+{
+    abstract class AbstractUnselectableTerrainComponent : AbstractTerrainComponent
+    {
+        protected override void OnParentSet(IHexGridCell oldParent)
+        {
+            if (oldParent != null)
+            {
+                oldParent.RemoveComponent<UnselectableComponent>();
+            }
+            Parent.AddComponent(new UnselectableComponent());
+        }
+    }
+}
