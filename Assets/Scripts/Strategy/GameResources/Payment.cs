62c5fed82536335c0d4f652fe0b6fa6581ba6959
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-03-01 17:05:29 -0500

    Implemented simplified resource system into scene

--- Assets/Scripts/Strategy/GameResources/Payment.cs
+++ Assets/Scripts/Strategy/GameResources/Payment.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ namespace SwordAndBored.Strategy.GameResources
 {
     public class Payment : IPayment
     {
-        public IResource Resource { get; set; }
+        public String Resource { get; set; }
         public int Cost { get; set; }
     }
 }
