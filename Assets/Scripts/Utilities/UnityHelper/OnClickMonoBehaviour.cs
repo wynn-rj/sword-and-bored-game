fa6f229dbcf5ee0d313612ac93ce53b2cd720ae7
Robert J Wynn <wynn.134@osu.edu>
2020-02-29 22:43:30 -0500

    Create abstract class for detecting clicks

---
+++ Assets/Scripts/Utilities/UnityHelper/OnClickMonoBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SwordAndBored.Utilities.UnityHelper
+{
+    public abstract class OnClickMonoBehaviour : MonoBehaviour
+    {
+        protected int clickMask;
+        protected float maxRayCastDistance;
+        protected Camera clickCamera;
+
+        private readonly int mouseButton;
+
+        protected OnClickMonoBehaviour(int mouseButton = 0)
+        {
+            this.mouseButton = mouseButton;
+            clickMask = Physics.DefaultRaycastLayers;
+            maxRayCastDistance = Mathf.Infinity;            
+        }
+
+        private void Start()
+        {
+            clickCamera = Camera.main;
+        }
+
+        protected virtual void Update()
+        {
+            if (Input.GetMouseButtonDown(mouseButton))
+            {
+                if (EventSystem.current.IsPointerOverGameObject())
+                {
+                    return;
+                }
+                Ray ray = clickCamera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit, maxRayCastDistance, clickMask))
+                {
+                    OnClick(hit);
+                }
+            }
+        }
+
+        protected abstract void OnClick(RaycastHit hit);         
+    }
+}
