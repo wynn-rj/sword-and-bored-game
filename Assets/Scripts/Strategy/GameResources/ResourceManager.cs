4c79f816f9533dcffb6b04fac7e094eb78f8a5bf
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-03-01 16:54:07 -0500

    Simplified resource system

---
+++ Assets/Scripts/Strategy/GameResources/ResourceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.GameResources
+{
+    public class ResourceManager : MonoBehaviour
+    {
+        public int GoldAmount
+        {
+            get { return gold; }
+            set
+            {
+                gold = value;
+                resourceDisplay.UpdateDisplay(gold);
+            }
+        }
+        public ResourceDisplay resourceDisplay;
+        private int gold;
+
+        public bool CanAffordPurchase(IPayment payment)
+        {
+            return payment.Cost <= gold;
+        }
+    }
+}
