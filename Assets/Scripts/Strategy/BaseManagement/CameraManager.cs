2f6c494c4b9623d38ad1991b38a257f0672c63be
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 16:57:40 -0500

    Create a camera manager for scene

---
+++ Assets/Scripts/Strategy/BaseManagement/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.BaseManagement
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public Transform StagingArea;
+
+        public Transform DefaultPosition;
+
+        private void Start()
+        {
+            UnFocusOnModel();
+        }
+
+        public void FocusOnModel()
+        {
+            transform.position = StagingArea.transform.position;
+        }
+
+        public void UnFocusOnModel()
+        {
+            transform.position = DefaultPosition.transform.position;
+        }
+    }
+}
