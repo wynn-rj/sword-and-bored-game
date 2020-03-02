e2e462381d85b156ef7c75c9a80295e25d7ac3f9
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 17:24:00 -0500

    Create unit entry scriptable object

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/UnitEntry.cs
@@ -0,0 +1,29 @@
+﻿using SwordAndBored.GameData.Units;
+using UnityEngine;
+
+public class UnitEntry : ScriptableObject
+{
+    public string UnitName;
+    public string Role;
+    public string CurrentCity;
+
+    private IStats stats;
+    private int id;
+
+    public void Init(int id, IUnit unit, IStats stats)
+    {
+        this.id = id;
+        /*
+         * TO DO: parse units and stats into string data
+         */
+        this.UnitName = unit.Name;
+        this.Role = unit.Role.Name;
+    }
+
+    public static UnitEntry CreateInstance(int id, IUnit unit, IStats stats)
+    {
+        var data = ScriptableObject.CreateInstance<UnitEntry>();
+        data.Init(id, unit, stats);
+        return data;
+    }
+}
