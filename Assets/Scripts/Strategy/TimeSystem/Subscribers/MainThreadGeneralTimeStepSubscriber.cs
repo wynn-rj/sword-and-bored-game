d2ac3d91384708dd49beccb24ee1f01735a1988b
Robert J Wynn <wynn.134@osu.edu>
2020-03-02 15:47:53 -0500

    Add time step subscribers that run on the main thread

---
+++ Assets/Scripts/Strategy/TimeSystem/Subscribers/MainThreadGeneralTimeStepSubscriber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+using SwordAndBored.Utilities.UnityHelper;
+
+namespace SwordAndBored.Strategy.TimeSystem.Subscribers
+{
+    public abstract class MainThreadGeneralTimeStepSubscriber : MonoBehaviour
+    {
+        protected Action mainThreadActionToCall;
+        private readonly Semaphore mainThreadExitControl = new Semaphore(0, 1);
+
+        protected void EnterMainThread()
+        {
+            try
+            {
+                UnityMainThreadDispatcher.Instance.RunOnMainThread(LockedMainThreadUpdate);
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+            }
+            finally
+            {
+                mainThreadExitControl.WaitOne();
+            }
+        }
+
+        private void LockedMainThreadUpdate()
+        {
+            try
+            {
+                mainThreadActionToCall?.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+            }
+            finally
+            {
+                mainThreadExitControl.Release();
+            }
+        }
+    }
+}
