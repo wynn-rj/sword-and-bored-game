06d26d2b36956ca1c6e45e343c4a0ecd3c2f274a
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 16:57:17 -0500

    Create generic building class

---
+++ Assets/Scripts/Strategy/BaseManagement/GenericBuilding.cs
@@ -0,0 +1,33 @@
+﻿using SwordAndBored.Strategy.BaseManagement;
+using SwordAndBored.Strategy.BaseManagement.Buildings;
+using SwordAndBored.UI.BaseGUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.BaseManagement.Buildings
+{
+    public class GenericBuilding : MonoBehaviour, IBuilding
+    {
+        public GenericStrongholdCell Cell;
+
+        [SerializeField] private GameObject mainCanvas;
+
+        public GameObject MainCanvas
+        {
+            get { return mainCanvas; }
+        }
+
+        public GameObject Canvas => mainCanvas;
+
+        public void LoadCanvas()
+        {
+            mainCanvas.SetActive(true);
+        }
+
+        public void UnloadCanvas()
+        {
+            mainCanvas.SetActive(false);
+        }
+    }
+}
