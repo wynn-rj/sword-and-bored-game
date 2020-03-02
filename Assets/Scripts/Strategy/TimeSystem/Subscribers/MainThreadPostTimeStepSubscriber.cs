9d43592a7f384897c4fb170853f40bb8fe23846b
Robert J Wynn <wynn.134@osu.edu>
2020-03-02 15:47:53 -0500

    Add time step subscribers that run on the main thread

---
+++ Assets/Scripts/Strategy/TimeSystem/Subscribers/MainThreadPostTimeStepSubscriber.cs
@@ -0,0 +1,13 @@
+﻿namespace SwordAndBored.Strategy.TimeSystem.Subscribers
+{
+    public abstract class MainThreadPostTimeStepSubscriber : MainThreadGeneralTimeStepSubscriber, IPostTimeStepSubscriber
+    {
+        public virtual void PostTimeStepUpdate()
+        {
+            mainThreadActionToCall = MainThreadPostTimeStepUpdate;
+            EnterMainThread();
+        }
+
+        protected abstract void MainThreadPostTimeStepUpdate();
+    }
+}
---
+++ Assets/Scripts/Strategy/TimeSystem/Subscribers/MainThreadPreTimeStepSubscriber.cs
@@ -0,0 +1,13 @@
+﻿namespace SwordAndBored.Strategy.TimeSystem.Subscribers
+{
+    public abstract class MainThreadPreTimeStepSubscriber : MainThreadGeneralTimeStepSubscriber, IPreTimeStepSubscriber
+    {
+        public virtual void PreTimeStepUpdate()
+        {
+            mainThreadActionToCall = MainThreadPreTimeStepUpdate;
+            EnterMainThread();
+        }
+
+        protected abstract void MainThreadPreTimeStepUpdate();
+    }
+}
