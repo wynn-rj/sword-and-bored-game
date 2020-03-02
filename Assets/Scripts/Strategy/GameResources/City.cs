f01b28c2051c4b0b97ae257cfe2513faef4aafb7
Jack Kuczek <kuczek.8@buckeyemail.osu.edu>
2020-03-02 14:11:03 -0500

    Refactored city class

---
+++ Assets/Scripts/Strategy/GameResources/City.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordAndBored.Strategy.TimeSystem.Subscribers;
+using SwordAndBored.Strategy.TimeSystem.TimeManager;
+
+namespace SwordAndBored.Strategy.GameResources
+{
+    public class City : MonoBehaviour, IPostTimeStepSubscriber
+    {
+        public ResourceManager resourceManager { get; set; }
+        public bool underPlayerControl = true;
+
+        void Awake()
+        {
+            underPlayerControl = true;
+        }
+        public void PostTimeStepUpdate()
+        {
+            Debug.Log("City Update");
+            Debug.Log("GoldAmount: " + resourceManager.GoldAmount);
+            resourceManager.GoldAmount += 1;
+        }
+    }
+}
