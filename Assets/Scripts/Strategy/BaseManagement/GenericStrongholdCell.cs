c2a037ee226b1bef0b6db1316281cb139837a47c
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 16:53:47 -0500

    Remove cell hold on canvas

--- Assets/Scripts/Strategy/BaseManagement/GenericStrongholdCell.cs
+++ Assets/Scripts/Strategy/BaseManagement/GenericStrongholdCell.cs
@@ -2,18 +2,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace SwordAndBored.Strategy.BaseManagement
+namespace SwordAndBored.Strategy.BaseManagement.Buildings
 {
     public class GenericStrongholdCell : MonoBehaviour, IStrongholdCell
     {
-        [SerializeField] private GameObject canvas;
         [SerializeField] private Button clickableObject;
-
-        public GameObject Canvas
-        {
-            get { return canvas; }
-            set { value = canvas; }
-        }
 
         public Button ClickableObject
         {
@@ -23,24 +16,11 @@ namespace SwordAndBored.Strategy.BaseManagement
 
         public int Index { get; set; }
 
-        protected void SetCanvas(GameObject _canvas)
-        {
-            canvas = _canvas;
-        }
-
-        public void LoadCanvas()
-        {
-            canvas.SetActive(true);
-        }
-
-        public void UnloadCanvas()
-        {
-            canvas.SetActive(false);
-        }
-
         public virtual void OnClick()
         {
-            LoadCanvas();
+            /*
+             * TO DO: implement
+             */
         }
 
         public void AddListener(Action action)
