28e63becfb4ece08d3398694fbc29a424b0d242e
cthomas68 <c.l.thomas68@gmail.com>
2020-03-01 20:43:22 -0500

    Simplify canvas interactivity

--- Assets/Scripts/Strategy/BaseManagement/BaseManager.cs
+++ Assets/Scripts/Strategy/BaseManagement/BaseManager.cs
@@ -6,44 +6,41 @@ namespace SwordAndBored.Strategy.BaseManagement
 {
     public class BaseManager : MonoBehaviour
     {
-        public List<GameObject> Cells;
+        public GameObject StrongholdCanvas;
+
+        private IList<IStrongholdCell> cells;
+
         public BuildingsManager BuildingManager;
         public UnitManager UnitManager;
-        public List<GameObject> CanvasList;
 
         private Canvas activeCanvas;
         private Canvas previouslyActiveCanvas;
   
         void Awake()
         {
-            activeCanvas = CanvasList[0].GetComponent<Canvas>();
-            previouslyActiveCanvas = CanvasList[0].GetComponent<Canvas>();    
+            activeCanvas = StrongholdCanvas.GetComponent<Canvas>();
+            previouslyActiveCanvas = StrongholdCanvas.GetComponent<Canvas>();    
         }
 
-        private void Start()
-        {
-            LoadCells();
-        }
-
-        private void LoadCells()
-        {
-            // This code will be used.
-
-            /*int index = 0;
-            foreach (GameObject cell in Cells)
-            {
-                IStrongholdCell cellComponent = cell.GetComponent<IStrongholdCell>();
-                cellComponent.AddListener(p => BuildingManager.TakeActionOnCell(index), index);
-                cellComponent.Index = index;
-                index++;
-            }*/
-        }
-
-        public void UnloadActiveCanvas(GameObject newCanvas)
+        /// <summary>
+        /// Called on button click of active stronghold cells to switch to their canvas.
+        /// </summary>
+        /// <param name="newCanvas"></param>
+        public void SwitchActiveCanvas(GameObject newCanvas)
         {
             activeCanvas.gameObject.SetActive(false);
             previouslyActiveCanvas = activeCanvas;
             activeCanvas = newCanvas.GetComponent<Canvas>();
+            activeCanvas.gameObject.SetActive(true);
+        }
+
+        public void LoadPreviousActiveCanvas()
+        {
+            activeCanvas.gameObject.SetActive(false);
+            Canvas temp = previouslyActiveCanvas;
+            previouslyActiveCanvas = activeCanvas;
+            activeCanvas = temp;
+            activeCanvas.gameObject.SetActive(true);
         }
     }
 }
