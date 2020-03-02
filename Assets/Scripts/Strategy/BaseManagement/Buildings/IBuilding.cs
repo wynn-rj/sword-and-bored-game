db3d718a3b865bc72e7d92dffdc2676920e3de69
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 16:52:15 -0500

    Create IBuilding interface

---
+++ Assets/Scripts/Strategy/BaseManagement/Buildings/IBuilding.cs
@@ -0,0 +1,11 @@
+﻿using SwordAndBored.UI.BaseGUI;
+
+namespace SwordAndBored.StrategyView.BaseManagement.Buildings
+{
+    public interface IBuilding : ICanvasDisplayable
+    {
+        /*
+         * More members may be added in the future.
+         */
+    }
+}
