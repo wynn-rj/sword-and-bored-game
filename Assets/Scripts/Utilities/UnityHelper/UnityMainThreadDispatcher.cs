d8bd2e34951902b70fb9e355e34542ad3ea3db74
Robert J Wynn <wynn.134@osu.edu>
2020-03-02 15:47:24 -0500

    Create thread safe main thread dispatcher

---
+++ Assets/Scripts/Utilities/UnityHelper/UnityMainThreadDispatcher.cs
@@ -0,0 +1,71 @@
+﻿using SwordAndBored.Utilities.Debug;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace SwordAndBored.Utilities.UnityHelper
+{
+    class UnityMainThreadDispatcher : MonoBehaviour
+    {
+        public static UnityMainThreadDispatcher Instance { 
+            get
+            {
+                AssertHelper.Assert(instance != null, "No game object present for thread dispatcher");
+                return instance;
+            }
+        }
+        private static UnityMainThreadDispatcher instance;
+        private static Queue<Action> actionQueue;
+        private static readonly object queueLock = new object();
+
+        private void Awake()
+        {
+            instance = this;
+            actionQueue = new Queue<Action>();
+        }
+
+        private void Update()
+        {
+            try
+            {
+                Monitor.Enter(queueLock);
+                while (actionQueue.Count > 0)
+                {
+                    actionQueue.Dequeue().Invoke();
+                }
+            }
+            catch (Exception exception)
+            {
+                UnityEngine.Debug.LogException(exception);
+            }
+            finally
+            {
+                Monitor.Exit(queueLock);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            instance = null;
+        }
+
+        public void RunOnMainThread(Action action)
+        {
+            if (action is null) return;
+            try
+            {
+                Monitor.Enter(queueLock);
+                actionQueue.Enqueue(action);
+            }
+            catch (Exception exception)
+            {
+                UnityEngine.Debug.LogException(exception);
+            }
+            finally
+            {
+                Monitor.Exit(queueLock);
+            }
+        }
+    }
+}
