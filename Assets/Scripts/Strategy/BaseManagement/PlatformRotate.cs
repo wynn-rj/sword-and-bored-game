d4b7632c80f243deed8581f0f33792d8d4344535
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 18:09:47 -0500

    Add platform rotation script

---
+++ Assets/Scripts/Strategy/BaseManagement/PlatformRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRotate : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 15f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+    }
+}
