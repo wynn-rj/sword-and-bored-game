79dc773f8d0e0aca736a5c47afedbe0e78152396
cthomas68 <c.l.thomas68@gmail.com>
2020-03-02 17:59:45 -0500

    Create unit entry display script

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/UnitEntryDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitEntryDisplay : MonoBehaviour
+{
+    public UnitEntry UnitEntry;
+
+    public void SetDisplay()
+    {
+        
+    }
+}
