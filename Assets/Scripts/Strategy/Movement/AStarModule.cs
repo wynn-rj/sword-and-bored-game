ddbc8af1e7f6dab073643c198d6e378bdc45998f
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-03-03 14:33:47 -0500

    write scaffolding for a star on the hex grid

---
+++ Assets/Scripts/Strategy/Movement/AStarModule.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells
+{
+    public class AStarModule
+    {
+
+        class Node
+        {
+            public IHexGridCell Cell { get; }
+            public IHexGridCell Parent { get; }
+
+            public Node(IHexGridCell cell, IHexGridCell parent)
+            {
+                Cell = cell;
+                Parent = parent;
+            }
+        }
+
+        public Stack<IHexGridCell> FindPath(IHexGridCell startCell, IHexGridCell destinationCell)
+        {
+            return new Stack<IHexGridCell>();
+        }
+    }
+}
