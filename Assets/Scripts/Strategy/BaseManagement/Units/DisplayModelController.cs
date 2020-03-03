65e7860f27933b032444702d1e529da568ba79a0
cthomas68 <c.l.thomas68@gmail.com>
2020-03-03 01:15:17 -0500

    Create display model controller

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/DisplayModelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayModelController : MonoBehaviour
+{
+    private IDictionary<string, Transform> modelsDictionary;
+
+    private void Awake()
+    {
+        modelsDictionary = new Dictionary<string, Transform>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform model = transform.GetChild(i);
+            modelsDictionary[model.name] = model;
+
+            model.gameObject.SetActive(i == 0);
+        }
+    }
+
+    public void EnableModel(Transform modelTransform)
+    {
+        foreach (KeyValuePair<string, Transform> modelPair in modelsDictionary)
+        {
+            Transform transformToToggle = modelPair.Value;
+            bool shouldBeActive = transformToToggle == modelTransform;
+            transformToToggle.gameObject.SetActive(shouldBeActive);
+        }
+    }
+
+    public Transform GetModel(string modelName)
+    {
+        return modelsDictionary[modelName];
+    }
+}
