bc92b59831f15ec74a5c1941190dfc0245257407
Toeecko <rorygilliland@ameritech.net>
2020-03-02 22:53:02 -0500

    creates town interface

---
+++ Assets/Scripts/GameData/Units/ITown.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.GameData.Units
+{
+    public interface ITown : IDatabaseObject, IDescriptable
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        List<IUnit> Units { get; set; }
+    }
+}
