6b1ceca49c4fd24ada1470a5b49b31f47b4e0ed0
cthomas68 <c.l.thomas68@gmail.com>
2020-03-03 03:14:58 -0500

    Add display connection for unit entries

--- Assets/Scripts/Strategy/BaseManagement/Units/UnitEntryDisplay.cs
+++ Assets/Scripts/Strategy/BaseManagement/Units/UnitEntryDisplay.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitEntryDisplay : MonoBehaviour
 {
     public UnitEntry UnitEntry;
 
+    public Image UnitImage;
+
+    public Text UnitName;
+
+    public List<Sprite> RoleSpriteList;
+
     public void SetDisplay()
     {
-        
+        IDictionary<string, Sprite> RoleImageDictionary = new Dictionary<string, Sprite>()
+        {
+            {"Warrior", RoleSpriteList[0]},
+            {"Scout", RoleSpriteList[1]},
+            {"Mage", RoleSpriteList[2]}
+        };
+
+        UnitImage.sprite = RoleImageDictionary[UnitEntry.Role];
+        UnitName.text = UnitEntry.UnitName;
+
     }
+
+    
 }
