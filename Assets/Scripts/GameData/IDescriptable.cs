74464e26e76a2588200c1faf7f1b5272187632d3
Toeecko <rorygilliland@ameritech.net>
2020-03-02 23:01:14 -0500

    adds deleted file back

---
+++ Assets/Scripts/GameData/IDescriptable.cs
@@ -0,0 +1,12 @@
+﻿namespace SwordAndBored.GameData
+{
+    /// <summary>
+    /// An object that has a name and a description
+    /// </summary>
+    public interface IDescriptable
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string FlavorText { get; set; }
+    }
+}
