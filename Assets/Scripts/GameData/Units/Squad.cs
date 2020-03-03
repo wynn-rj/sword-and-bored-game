2ec5cc506e914c16a4aff5648b8b3fd6bcdb6aee
Toeecko <rorygilliland@ameritech.net>
2020-03-03 12:14:05 -0500

    create squad/town reference based on tile location

--- Assets/Scripts/GameData/Units/Squad.cs
+++ Assets/Scripts/GameData/Units/Squad.cs
@@ -56,6 +56,33 @@ namespace SwordAndBored.GameData.Units
             reader.CloseReader();
         }
 
+        public Squad(int inputX, int inputY)
+        {
+            DatabaseConnection conn = new DatabaseConnection();
+            DatabaseReader reader = conn.ExecuteQuery($"SELECT * FROM Squads WHERE X = {inputX} AND Y = {inputY};");
+
+            X = inputX;
+            Y = inputY;
+            if (reader.NextRow())
+            {
+                Name = reader.GetStringFromCol("Name");
+                Description = reader.GetStringFromCol("Description");
+                FlavorText = reader.GetStringFromCol("Flavor_Text");
+                ID = reader.GetIntFromCol("ID");
+
+                reader.CloseReader();
+                reader = conn.QueryRowFromTableWhereColNameEqualsInt("Units", "Squads_FK", ID);
+                while (reader.NextRow())
+                {
+                    int dataUnitID = reader.GetIntFromCol("ID");
+                    IUnit unitInSquad = new Unit(dataUnitID);
+                    Units.Add(unitInSquad);
+                }
+            }
+            conn.CloseConnection();
+            reader.CloseReader();
+        }
+
 
         public static List<ISquad> GetAllSquads()
         {
