1a26ff71f45cb10d06098e2e7561300e4d41906f
cthomas68 <c.l.thomas68@gmail.com>
2020-03-03 03:09:37 -0500

    Add unit data display controller

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/DisplayDataController.cs
@@ -0,0 +1,31 @@
+﻿using SwordAndBored.GameData.Units;
+using TMPro;
+using UnityEngine;
+
+public class DisplayDataController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text MaxHP;
+    [SerializeField] private TMP_Text CurrentHP;
+    [SerializeField] private TMP_Text Initiative;
+    [SerializeField] private TMP_Text PhysicalAttack;
+    [SerializeField] private TMP_Text PhysicalDefense;
+    [SerializeField] private TMP_Text MagicAttack;
+    [SerializeField] private TMP_Text MagicDefense;
+    [SerializeField] private TMP_Text Role;
+    [SerializeField] private TMP_Text Name;
+
+    public void SetData(IUnit unit)
+    {
+        IStats stats = unit.Stats;
+        MaxHP.text = "Max HP: " + stats.HP.ToString();
+        CurrentHP.text = "Current HP: " + stats.HP.ToString();
+        Initiative.text = "Initiative: " + stats.Initiative.ToString();
+        PhysicalAttack.text = "Physical Attack: " + stats.Physical_Attack.ToString();
+        PhysicalDefense.text = "Physical Defense: " + stats.Physical_Defense.ToString();
+        MagicAttack.text = "Magic Attack: " + stats.Magic_Attack.ToString();
+        MagicDefense.text = "Magic Defense: " + stats.Magic_Defense.ToString();
+
+        Role.text = unit.Role.Name;
+        Name.text = unit.Name;
+    }
+}
