0c41f529e1d72d42ab4a193b3b7d83ed486bd29c
Toeecko <rorygilliland@ameritech.net>
2020-03-02 22:52:50 -0500

    creates Squad interface

---
+++ Assets/Scripts/GameData/Units/ISquad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.GameData.Units
+{
+    public interface ISquad : IDatabaseObject, IDescriptable
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        List<IUnit> Units { get; set; }
+
+        int Movement();
+    }
+}
