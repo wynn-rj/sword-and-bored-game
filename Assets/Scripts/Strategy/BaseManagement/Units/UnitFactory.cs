dd9bd0367aa3c2fb30f6877dfaf04897d71e7037
cthomas68 <c.l.thomas68@gmail.com>
2020-03-03 03:13:43 -0500

    Add random name to unit factory

--- Assets/Scripts/Strategy/BaseManagement/Units/UnitFactory.cs
+++ Assets/Scripts/Strategy/BaseManagement/Units/UnitFactory.cs
@@ -1,9 +1,12 @@
-﻿using SwordAndBored.GameData.Roles;
+`using SwordAndBored.GameData.Creatures;
+using SwordAndBored.GameData.Database;
+using SwordAndBored.GameData.Roles;
 using SwordAndBored.GameData.Units;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 namespace SwordAndBored.Strategy.BaseManagement
 {
@@ -24,7 +27,23 @@ namespace SwordAndBored.Strategy.BaseManagement
 
         public void ConfirmUnitTraining()
         {
-            character.Name = UnitName;
+            if (UnitName.Length == 0)
+            {
+                DatabaseConnection conn = new DatabaseConnection();
+                System.Random r = new System.Random();
+                int rInt = r.Next(1, 30);
+                DatabaseReader databaseReader = conn.ExecuteQuery($"SELECT Name FROM Random_Names WHERE ID = {rInt}");
+                databaseReader.NextRow();
+                string randomName = databaseReader.GetStringFromCol("Name");
+                databaseReader.CloseReader();
+                conn.CloseConnection();
+                character.Name = randomName;
+            }
+            else
+            {
+                character.Name = UnitName;
+            }
+            
             UnitManager.Instance.RegisterUnit(character);
             NameUnitCanvas.gameObject.SetActive(false);
             character.Save();
