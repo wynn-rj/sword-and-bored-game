0e5ecfebd112367c05dfe9b760d6f0a3848a16ca
Toeecko <rorygilliland@ameritech.net>
2020-03-02 23:14:10 -0500

    creates squad class

---
+++ Assets/Scripts/GameData/Units/Squad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.GameData.Units
+{
+    public class Squad : ISquad
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public List<IUnit> Units { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FlavorText { get; set; }
+
+        public int AverageMovement()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
