e206fc2eb673ab04ab0e84a84c222ca102e89b3a
cthomas68 <c.l.thomas68@gmail.com>
2020-03-03 01:17:07 -0500

    Add entry model correspondence

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/ShowEntryModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowEntryModel : MonoBehaviour
+{
+    private Transform displayModel;
+    private Action<Transform> clickAction;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(HandleButtonClick);
+    }
+
+
+    public void Initialize(Transform displayModel, Action<Transform> clickAction)
+    {
+        this.displayModel = displayModel;
+        this.clickAction = clickAction;
+    }
+
+    private void HandleButtonClick()
+    {
+        clickAction(displayModel);
+    }
+}
