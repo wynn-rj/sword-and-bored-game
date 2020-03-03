fb451531d7fa2cbbf62e2f8d67d5996909d66815
cthomas68 <c.l.thomas68@gmail.com>
2020-03-03 03:09:58 -0500

    Add unit data display correspondence

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/ShowEntryData.cs
@@ -0,0 +1,26 @@
+﻿using SwordAndBored.GameData.Units;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowEntryData : MonoBehaviour
+{
+    private IUnit unit;
+    private Action<IUnit> clickAction;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(HandleButtonClick);
+    }
+
+    public void Initialize(IUnit unit, Action<IUnit> clickAction)
+    {
+        this.unit = unit;
+        this.clickAction = clickAction;
+    }
+
+    private void HandleButtonClick()
+    {
+        clickAction(unit);
+    }
+}
