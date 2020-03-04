33077eeb3400bef2b8ab66fcdfe4affa1909ded2
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-03-04 13:02:36 -0500

    initial draft of AStarModule for HexGrid

--- Assets/Scripts/Strategy/Movement/AStarModule.cs
+++ Assets/Scripts/Strategy/Movement/AStarModule.cs
@@ -1,7 +1,8 @@
-﻿
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents;
 using System.Collections.Generic;
 
-namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells
+namespace SwordAndBored.Strategy.Movement
 {
     public class AStarModule
     {
@@ -9,18 +10,136 @@ namespace SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells
         class Node
         {
             public IHexGridCell Cell { get; }
-            public IHexGridCell Parent { get; }
+            public Node Parent { get; }
+            public float G { get; set; }
+            public float H { get; set; }
 
-            public Node(IHexGridCell cell, IHexGridCell parent)
+            public float F { get { return G + H; } }
+
+            public Node(IHexGridCell cell, Node parent, float g = 0, float h = 0)
             {
                 Cell = cell;
                 Parent = parent;
+                G = g;
+                H = h;
             }
+
+            public bool isSameCell(Node other)
+            {
+                return Cell == other.Cell;
+            }
+
         }
 
-        public Stack<IHexGridCell> FindPath(IHexGridCell startCell, IHexGridCell destinationCell)
+        public static List<IHexGridCell> FindPath(IHexGridCell startCell, IHexGridCell destinationCell)
         {
-            return new Stack<IHexGridCell>();
+            List<Node> openList = new List<Node>();
+            List<Node> closedList = new List<Node>();
+
+            Node startNode = new Node(startCell, null);
+            Node destinationNode = new Node(destinationCell, null);
+
+            openList.Add(startNode);
+
+            while (openList.Count > 0)
+            {
+                Node currentNode = openList[0];
+                foreach (Node thisNode in openList)
+                {
+                    if (thisNode.F < currentNode.F)
+                    {
+                        currentNode = thisNode;
+                    }
+                }
+                openList.Remove(currentNode);
+
+                Queue<Node> successors = GenerateChildren(currentNode, destinationNode);
+                while (successors.Count > 0)
+                {
+                    Node successor = successors.Dequeue();
+                    if (successor.isSameCell(destinationNode))
+                    {
+                        return GeneratePath(successor);
+                    }
+
+
+                    bool dontAdd = false;
+                    foreach (Node openNode in openList)
+                    {
+                        if (successor.isSameCell(openNode) && openNode.F < successor.F)
+                        {
+                            dontAdd = true;
+                            break;
+                        }
+                    }
+
+                    if (!dontAdd)
+                    {
+                        foreach (Node closedNode in closedList)
+                        {
+                            if (successor.isSameCell(closedNode) && closedNode.F < successor.F)
+                            {
+                                dontAdd = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!dontAdd)
+                    {
+                        openList.Add(successor);
+                    }
+                }
+                closedList.Add(currentNode);
+            }
+
+            return new List<IHexGridCell>();
+        }
+
+        private static List<IHexGridCell> GeneratePath(Node finalNode)
+        {
+            List<IHexGridCell> path = new List<IHexGridCell>();
+            Node currentNode = finalNode;
+
+            while (currentNode != null)
+            {
+                path.Add(currentNode.Cell);
+                currentNode = currentNode.Parent;
+
+            }
+
+            path.Reverse();
+
+            return path;
+        }
+
+
+        private static Queue<Node> GenerateChildren(Node parent, Node destination)
+        {
+            Queue<Node> childrenNodes = new Queue<Node>();
+
+            Point<float> destinationPoint = destination.Cell.Position.Center;
+            IHexGridCell[] childrenCells = parent.Cell.Neighbors;
+
+            foreach (IHexGridCell cell in childrenCells)
+            {
+                if (cell != null && cell.GetComponent<UnselectableComponent>() == null && cell.GetComponent<CreatureComponent>() == null)
+                {
+                    Point<float> thisPoint = cell.Position.Center;
+                    float distanceSquared = SquaredDistanceBetweenFloatPoints(thisPoint, destinationPoint);
+                    Node thisNode = new Node(cell, parent, g: 1.0f, h: distanceSquared);
+                    childrenNodes.Enqueue(thisNode);
+                }
+            }
+
+            return childrenNodes;
+        }
+
+
+        private static float SquaredDistanceBetweenFloatPoints(Point<float> first, Point<float> second)
+        {
+            return ((first.Y - second.Y) * (first.Y - second.Y)) + ((first.X - second.X) + (first.X - second.X));
         }
     }
+
 }
