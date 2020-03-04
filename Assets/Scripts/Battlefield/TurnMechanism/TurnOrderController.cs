0bfa5db797229c831644e6b65b1751aa6dfb3ef5
Toeecko <rorygilliland@ameritech.net>
2020-03-04 10:39:15 -0500

    Makes turn order based on character init

--- Assets/Scripts/Battlefield/TurnMechanism/TurnOrderController.cs
+++ Assets/Scripts/Battlefield/TurnMechanism/TurnOrderController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using SwordAndBored.Battlefield.CreaturScripts;
 
 namespace SwordAndBored.Battlefield.TurnMechanism
 {
@@ -9,11 +11,29 @@ namespace SwordAndBored.Battlefield.TurnMechanism
 
         public TurnOrderController(GameObject[] entities)
         {
-            this.entities = entities;
+            //this.entities = entities;
+            List<GameObject> list = new List<GameObject>();
+            list.AddRange(entities);
+            list.Sort(delegate (GameObject a, GameObject b)
+            {
+                return b.GetComponent<UniqueCreature>().stats.speedIntit.CompareTo(a.GetComponent<UniqueCreature>().stats.speedIntit);
+            }
+            );
+            this.entities = list.ToArray();
         }
 
         public TurnOrderController(GameObject[] entities, IShuffler<GameObject> shuffler)
         : this(shuffler.shuffle(entities)) { }
+        /*{
+            List<GameObject> list = new List<GameObject>();
+            list.AddRange(entities);
+            list.Sort(delegate (GameObject a, GameObject b)
+            {
+                return b.GetComponent<UniqueCreature>().stats.speedIntit.CompareTo(a.GetComponent<UniqueCreature>().stats.speedIntit);
+            }
+            );
+            this.entities = list.ToArray();
+        }*/
 
         public GameObject NextEntity()
         {
