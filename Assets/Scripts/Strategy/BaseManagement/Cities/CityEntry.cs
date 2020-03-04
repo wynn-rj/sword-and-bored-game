26f5d5041973fd9656ebb67637395a898f92929d
cthomas68 <c.l.thomas68@gmail.com>
2020-03-04 00:33:45 -0500

    Add city entries

---
+++ Assets/Scripts/Strategy/BaseManagement/Cities/CityEntry.cs
@@ -0,0 +1,22 @@
+﻿using SwordAndBored.GameData.Units;
+using UnityEngine;
+
+public class CityEntry : ScriptableObject
+{
+    public ITown town;
+
+    public string townName;
+
+    public void Init(ITown town)
+    {
+        this.town = town;
+        this.townName = town.Name;
+    }
+
+    public static CityEntry CreateInstance(ITown town)
+    {
+        var data = ScriptableObject.CreateInstance<CityEntry>();
+        data.Init(town);
+        return data;
+    }
+}
---
+++ Assets/Scripts/Strategy/BaseManagement/Cities/CityEntryDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CityEntryDisplay : MonoBehaviour
+{
+    public CityEntry cityEntry;
+    public Text cityName;
+
+    void Start()
+    {
+        cityName.text = cityEntry.name;
+    }
+}
