83148c2adb15ff7f36d7b4f93c5dbf6a9e63c1ff
Robert J Wynn <wynn.134@osu.edu>
2020-03-03 22:13:57 -0500

    Add on hover outline controller

---
+++ Assets/Scripts/UI/Utility/OnHoverOutline.cs
@@ -0,0 +1,29 @@
+﻿using SwordAndBored.Utilities.Debug;
+using UnityEngine;
+
+namespace SwordAndBored.UI.Utility
+{
+    class OnHoverOutline : MonoBehaviour
+    {
+        private Outline outline;
+
+        public Color OutlineColor
+        {
+            get => outline.OutlineColor;
+            set => outline.OutlineColor = value;
+        }
+
+        public bool OutlineEnabled
+        {
+            get => outline.enabled;
+            set => outline.enabled = value;
+        }
+
+        private void Start()
+        {
+            outline = gameObject.GetComponentInChildren<Outline>();
+            AssertHelper.Assert(outline != null, "An outline component must be set for " + transform.parent.name, this);
+            outline.enabled = false;
+        }
+    }
+}
