7a274401062df09300041bac6e70018e7404d2c4
cthomas68 <c.l.thomas68@gmail.com>
2020-03-04 00:33:12 -0500

    Add active entry listener for entries

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/SetActiveEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwordAndBored.Strategy.BaseManagement.Units
+{
+    public class SetActiveEntry : MonoBehaviour
+    {
+        private UnitEntry entry;
+        private Action<GameObject> clickAction;
+
+        private void Awake()
+        {
+            entry = GetComponent<UnitEntryDisplay>().unitEntry;
+        }
+
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(HandleButtonClick);
+        }
+
+        public void Initialize(Action<GameObject> clickAction)
+        {
+            this.clickAction = clickAction;
+        }
+
+        private void HandleButtonClick()
+        {
+            clickAction(gameObject);
+        }
+    }
+}
