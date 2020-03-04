1c1c26085da4a5b3dbf3c88e84f7a0d21456ce36
cthomas68 <c.l.thomas68@gmail.com>
2020-03-04 00:34:44 -0500

    Update barracks with dispatch method

--- Assets/Scripts/Strategy/BaseManagement/Buildings/Barracks.cs
+++ Assets/Scripts/Strategy/BaseManagement/Buildings/Barracks.cs
@@ -2,6 +2,7 @@
 using SwordAndBored.Strategy.BaseManagement.Units;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SwordAndBored.StrategyView.BaseManagement.Buildings
 {
@@ -9,17 +10,28 @@ namespace SwordAndBored.StrategyView.BaseManagement.Buildings
     {
         [SerializeField] private GameObject unitEntryPrefab;
         [SerializeField] private GameObject trainUnitCanvas;
+        [SerializeField] private GameObject dispatchUnitCanvas;
         [SerializeField] private GameObject unitListPanel;
+
+        [SerializeField] private Button trainUnitButton;
+        [SerializeField] private Button dispatchUnitButton;
+
         [SerializeField] private DisplayModelController displayModelController;
         [SerializeField] private DisplayDataController displayDataController;
 
         private IList<IUnit> activeUnitsList;
+        private IList<GameObject> unitEntriesList;
+
+        private GameObject activeEntry;
 
         public GameObject TrainUnitCanvas => trainUnitCanvas;
 
         private void Awake()
         {
             UnitManager.Instance.GetAllData();
+            unitEntriesList = new List<GameObject>();
+
+            //trainUnitButton.onClick.AddListener();
         }
 
         private void Start()
@@ -28,8 +40,11 @@ namespace SwordAndBored.StrategyView.BaseManagement.Buildings
 
             foreach (IUnit unit in activeUnitsList)
             {
-                CreateUnitEntry(unit);
+                GameObject entry = CreateUnitEntry(unit);
+                unitEntriesList.Add(entry);
             }
+
+            activeEntry = unitEntriesList[0];
         }
 
         public void EnterTrainUnitCanvas()
@@ -44,11 +59,28 @@ namespace SwordAndBored.StrategyView.BaseManagement.Buildings
             trainUnitCanvas.SetActive(false);
         }
 
+        public void EnterDispatchUnitCanvas()
+        {
+            MainCanvas.SetActive(false);
+            dispatchUnitCanvas.SetActive(true);
+        }
+
+        public void ExitDispatchUnitCanvas()
+        {
+            MainCanvas.SetActive(true);
+            dispatchUnitCanvas.SetActive(false);
+        }
+
+        public void SetActiveEntry(GameObject unitEntry)
+        {
+            activeEntry = unitEntry;
+        }
+
         /// <summary>
         /// Adds a UI element to scene based off <para>unit</para> data
         /// </summary>
         /// <param name="unit"></param>
-        public void CreateUnitEntry(IUnit unit)
+        public GameObject CreateUnitEntry(IUnit unit)
         {
             UnitEntry unitEntryData = UnitEntry.CreateInstance(unit);
 
@@ -61,6 +93,38 @@ namespace SwordAndBored.StrategyView.BaseManagement.Buildings
             unitEntryObject.GetComponent<UnitEntryDisplay>().SetDisplay();
             unitEntryObject.GetComponent<ShowEntryModel>().Initialize(displayModelController.GetModel(unit.Role.Name), displayModelController.EnableModel);
             unitEntryObject.GetComponent<ShowEntryData>().Initialize(unit, displayDataController.SetData);
+            unitEntryObject.GetComponent<SetActiveEntry>().Initialize(SetActiveEntry);
+
+            return unitEntryObject;
+        }
+
+        public void DispatchUnit(ITown town)
+        {
+            UnitEntry entryData = activeEntry.GetComponent<UnitEntryDisplay>().unitEntry;
+            IUnit dispatchableUnit = entryData.unit;
+            dispatchableUnit.Town = town;
+            dispatchableUnit.Save();
+        }
+
+        /// <summary>
+        /// Sorts list of gameobject entries based off provided metric
+        /// </summary>
+        public void SortEntries(string metric)
+        {
+            foreach (Transform child in unitListPanel.transform)
+            { 
+                /*
+                 * Destroy all children
+                 */
+            }
+
+            /*
+             * Sort based off metric
+             */
+
+            /*
+             * Re-add children
+             */
         }
     }
 }
