994f32ed6ef3492a10e334a09e53d53f9366799c
cthomas68 <c.l.thomas68@gmail.com>
2020-03-05 17:08:53 -0500

    Update town canvas controller

--- Assets/Scripts/Strategy/BaseManagement/Towns/TownCanvasController.cs
+++ Assets/Scripts/Strategy/BaseManagement/Towns/TownCanvasController.cs
@@ -1,12 +1,27 @@
 ﻿using SwordAndBored.GameData.Units;
+using SwordAndBored.Strategy.BaseManagement.Units;
+using SwordAndBored.Strategy.ProceduralTerrain;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using SwordAndBored.Strategy.Squads;
+using SwordAndBored.Utilities.Random;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SwordAndBored.Strategy.BaseManagement.Towns
 {
-    class TownCanvasController :MonoBehaviour
+    class TownCanvasController : MonoBehaviour
     {
+        [SerializeField] private GameObject unitEntryPrefab;
+        [SerializeField] private SquadManager squadController;
+        [SerializeField] private TileManager tileManager;
+        [SerializeField] private Button deploySquadButton;
+        [SerializeField] private Button cancelDeployment;
+        [SerializeField] private Transform entryContainer;
+
         private Canvas canvas;
         private ITown displayedTown;
+        private List<IUnit> deployedSquad;
 
         public ITown DisplayedTown
         {
@@ -20,12 +35,67 @@ namespace SwordAndBored.Strategy.BaseManagement.Towns
 
         private void Start()
         {
+            deployedSquad = new List<IUnit>();
             canvas = GetComponent<Canvas>();
+
+            deploySquadButton.onClick.AddListener(DeploySquad);
+            cancelDeployment.onClick.AddListener(ExitTownCanvas);
         }
 
         private void UpdateDisplay()
         {
-            
+            foreach (GameObject child in entryContainer.transform)
+            {
+                Destroy(child);
+            }
+
+            if (displayedTown is null) return;
+
+            foreach (IUnit unit in displayedTown.Units)
+            {
+                CreateTownUnitEntry(unit);
+            }
+
+            gameObject.SetActive(true);
+        }
+
+        private GameObject CreateTownUnitEntry(IUnit unit)
+        {
+            UnitEntry unitEntryData = UnitEntry.CreateInstance(unit);
+
+            GameObject unitEntryObject = Instantiate(unitEntryPrefab) as GameObject;
+            unitEntryObject.transform.SetParent(entryContainer.transform);
+            unitEntryObject.transform.localRotation = Quaternion.identity;
+            unitEntryObject.transform.localScale = new Vector3(1, 1, .8f);
+
+            unitEntryObject.GetComponent<UnitEntryDisplay>().unitEntry = unitEntryData;
+            unitEntryObject.GetComponent<UnitEntryDisplay>().SetDisplay();
+            unitEntryObject.GetComponent<ToggleUnitToSquad>().Initialize(AddUnitToSquad, RemoveUnitFromSquad);
+
+            return unitEntryObject;
+        }
+
+        public void ExitTownCanvas()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void AddUnitToSquad(IUnit unit)
+        {
+            deployedSquad.Add(unit);
+        }
+
+        public void RemoveUnitFromSquad(IUnit unit)
+        {
+            deployedSquad.Remove(unit);
+        }
+
+        public void DeploySquad()
+        {
+            IList<IHexGridCell> cellNeighbors = tileManager.HexTiling.CellNeighbors(displayedTown.X, displayedTown.Y);
+            squadController.DeploySquad("Name", deployedSquad, Odds.SelectAtRandom<IHexGridCell>(cellNeighbors));
+            deployedSquad.Clear();
+            gameObject.SetActive(false);
         }
     }
 }
