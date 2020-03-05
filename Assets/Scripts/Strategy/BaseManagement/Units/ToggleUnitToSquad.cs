aa871eea297fd2b063469b87f06635a7adf26710
cthomas68 <c.l.thomas68@gmail.com>
2020-03-05 17:06:36 -0500

    Create unit to squad listener script

---
+++ Assets/Scripts/Strategy/BaseManagement/Units/ToggleUnitToSquad.cs
@@ -0,0 +1,42 @@
+﻿using SwordAndBored.GameData.Units;
+using SwordAndBored.Strategy.BaseManagement.Units;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleUnitToSquad : MonoBehaviour
+{
+    private Action<IUnit> addAction;
+    private Action<IUnit> removeAction;
+
+    private bool added;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(HandleButtonClick);
+        added = false;
+    }
+
+    public void Initialize(Action<IUnit> addAction, Action<IUnit> removeAction)
+    {
+        this.addAction = addAction;
+        this.removeAction = removeAction;
+    }
+
+    private void HandleButtonClick()
+    {
+        IUnit unit = gameObject.GetComponent<UnitEntryDisplay>().unitEntry.unit;
+        if (!added)
+        {
+            addAction(unit);
+            added = true;
+            GetComponent<Button>().image.color = Color.red;
+        }
+        else
+        {
+            removeAction(unit);
+            added = false;
+            GetComponent<Button>().image.color = Color.white;
+        }
+    }
+}
