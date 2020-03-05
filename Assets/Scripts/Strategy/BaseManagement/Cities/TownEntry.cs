6dee884c4dd33140fc2e26215403e2cbfadff6a5
cthomas68 <c.l.thomas68@gmail.com>
2020-03-04 21:35:48 -0500

    Rename city to town

---
+++ Assets/Scripts/Strategy/BaseManagement/Cities/TownEntry.cs
@@ -0,0 +1,24 @@
+﻿using SwordAndBored.GameData.Units;
+using UnityEngine;
+
+namespace SwordAndBored.StrategyView.BaseManagement.Towns
+{
+    public class TownEntry : ScriptableObject
+    {
+        public ITown town;
+        public string townName;
+
+        public void Init(ITown town)
+        {
+            this.town = town;
+            this.townName = town.Name;
+        }
+
+        public static TownEntry CreateInstance(ITown town)
+        {
+            var data = ScriptableObject.CreateInstance<TownEntry>();
+            data.Init(town);
+            return data;
+        }
+    }
+}
