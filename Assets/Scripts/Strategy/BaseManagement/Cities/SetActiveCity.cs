9e0fa9a226926060918deff0ceb7517a30b50525
cthomas68 <c.l.thomas68@gmail.com>
2020-03-04 21:09:18 -0500

    Create active city setter script

---
+++ Assets/Scripts/Strategy/BaseManagement/Cities/SetActiveCity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetActiveCity : MonoBehaviour
+{
+    private CityEntry entry;
+    private Action<GameObject> clickAction;
+
+    private void Awake()
+    {
+        entry = GetComponent<CityEntryDisplay>().cityEntry;
+    }
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(HandleButtonClick);
+    }
+
+    public void Initialize(Action<GameObject> clickAction)
+    {
+        this.clickAction = clickAction;
+    }
+
+    private void HandleButtonClick()
+    {
+        clickAction(gameObject);
+    }
+}
