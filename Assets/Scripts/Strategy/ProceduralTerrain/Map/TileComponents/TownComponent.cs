9147cfe2f6f67066aadb2c1554871eac0eb9a2f7
Robert J Wynn <wynn.134@osu.edu>
2020-03-05 13:35:24 -0500

    Make city components town components

---
+++ Assets/Scripts/Strategy/ProceduralTerrain/Map/TileComponents/TownComponent.cs
@@ -0,0 +1,23 @@
+﻿using SwordAndBored.GameData.Units;
+using SwordAndBored.Strategy.BaseManagement.Towns;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents
+{
+    class TownComponent : AbstractSelectionComponent
+    {
+        private readonly ITown town;
+        private readonly TownCanvasController townCanvasController;
+
+        public TownComponent(ITown town, TownCanvasController townCanvasController)
+        {
+            this.town = town;
+            this.townCanvasController = townCanvasController;
+        }
+
+        public override void Select()
+        {
+            townCanvasController.DisplayedTown = town;
+        }
+    }
+}
