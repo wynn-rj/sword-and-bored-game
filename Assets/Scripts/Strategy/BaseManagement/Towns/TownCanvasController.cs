4c4e4a39eaff5228b86e0af31df56607b99139fb
Robert J Wynn <wynn.134@osu.edu>
2020-03-05 13:35:46 -0500

    Add town canvas controller

---
+++ Assets/Scripts/Strategy/BaseManagement/Towns/TownCanvasController.cs
@@ -0,0 +1,31 @@
+﻿using SwordAndBored.GameData.Units;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.BaseManagement.Towns
+{
+    class TownCanvasController :MonoBehaviour
+    {
+        private Canvas canvas;
+        private ITown displayedTown;
+
+        public ITown DisplayedTown
+        {
+            get => displayedTown;
+            set
+            {
+                displayedTown = value;
+                UpdateDisplay();
+            }
+        }
+
+        private void Start()
+        {
+            canvas = GetComponent<Canvas>();
+        }
+
+        private void UpdateDisplay()
+        {
+            
+        }
+    }
+}
