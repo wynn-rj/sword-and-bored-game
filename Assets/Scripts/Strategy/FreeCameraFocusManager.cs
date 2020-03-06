bd5a67e7501ea0c7c6a4fd87b0cac4bda0225021
gdiago <gonzalodiago@gmail.com>
2020-03-05 20:40:48 -0500

    Add move and rotate functionality to camera

--- Assets/Scripts/Strategy/FreeCameraFocusManager.cs
+++ Assets/Scripts/Strategy/FreeCameraFocusManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class FreeCameraFocusManager : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
+    private float rotateSpeed = 60;
+    private float speed = 30;
 
-    
     void Update()
     {
-        
+        move();
+        rotate();
     }
+
+    private void move()
+    {
+        Vector3 move = new Vector3();
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.z -= speed * Time.deltaTime;
+        }
+        transform.Translate(move);
+    }
+
+    private void rotate()
+    {
+        if (Input.GetKey(KeyCode.E))
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+
 }
