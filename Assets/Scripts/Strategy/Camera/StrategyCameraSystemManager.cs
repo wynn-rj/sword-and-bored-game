d81e0387af416b31f017cbe6749cb2702dc6907a
gdiago <gonzalodiago@gmail.com>
2020-03-05 20:56:40 -0500

    Add manager for all strategy cams

---
+++ Assets/Scripts/Strategy/Camera/StrategyCameraSystemManager.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrategyCameraSystemManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera freeCam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
