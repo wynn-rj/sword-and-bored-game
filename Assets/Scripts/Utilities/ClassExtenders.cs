ed36de8f211baf49f4a80dc88fe69c6780345337
Robert J Wynn <wynn.134@osu.edu>
2020-03-05 19:05:30 -0500

    Add class extender utility

---
+++ Assets/Scripts/Utilities/ClassExtenders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwordAndBored.Utilities
+{
+    static class ClassExtenders
+    {
+        public static IEnumerable<R> EnumerateOverValues<T, R>(this IEnumerable<T> enumerable, Func<T, R> operation)
+        {
+            foreach (T item in enumerable)
+            {
+                yield return operation(item);
+            }
+        }
+    }
+}
