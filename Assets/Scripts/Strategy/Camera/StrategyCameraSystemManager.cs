fc51400928a860b18b6526f65402bddf770c5620
gdiago <gonzalodiago@gmail.com>
2020-03-06 01:12:11 -0500

    Finish Cam

--- Assets/Scripts/Strategy/Camera/StrategyCameraSystemManager.cs
+++ Assets/Scripts/Strategy/Camera/StrategyCameraSystemManager.cs
@@ -11,21 +11,21 @@ public class StrategyCameraSystemManager : MonoBehaviour
     public SquadManager squadManager;
     private CinemachineVirtualCamera squadCam;
 
-    void Start()
-    {
-        freeCam.Priority = 1;
-    }
+    //void Start()
+    //{
+    //    freeCam.Priority = 1;
+    //}
 
-    void Update()
-    {
-        if (squadManager.SelectedSquad)
-        {
-            freeCam.m_Follow = squadManager.SelectedSquad.gameObject.transform;
-            freeCam.m_LookAt = squadManager.SelectedSquad.gameObject.transform;
-        } else
-        {
-            freeCam.m_Follow = freeCamFocus.transform;
-            freeCam.m_LookAt = freeCamFocus.transform;
-        }
-    }
+    //void Update()
+    //{
+    //    //if (squadManager.SelectedSquad)
+    //    //{
+    //    //    freeCam.m_Follow = squadManager.SelectedSquad.gameObject.transform;
+    //    //    freeCam.m_LookAt = squadManager.SelectedSquad.gameObject.transform;
+    //    //} else
+    //    //{
+    //    //    freeCam.m_Follow = freeCamFocus.transform;
+    //    //    freeCam.m_LookAt = freeCamFocus.transform;
+    //    //}
+    //}
 }
