67365c5d76a87a3a91c02f52e1567affed7edb30
gdiago <gonzalodiago@gmail.com>
2020-03-05 21:21:34 -0500

    Add virtual cams to squads and code to switch

--- Assets/Scripts/Strategy/Camera/StrategyCameraSystemManager.cs
+++ Assets/Scripts/Strategy/Camera/StrategyCameraSystemManager.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using SwordAndBored.Strategy.Squads;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,25 @@ using UnityEngine;
 public class StrategyCameraSystemManager : MonoBehaviour
 {
     public CinemachineVirtualCamera freeCam;
+    public SquadManager squadManager;
+    public CinemachineVirtualCamera squadCam;
 
     void Start()
     {
-        
+        freeCam.Priority = 1;
     }
 
     void Update()
     {
-        
+        if (squadManager.SelectedSquad)
+        {
+            squadCam = squadManager.SelectedSquad.GetComponent<CinemachineVirtualCamera>();
+            freeCam.Priority = 0;
+            squadCam.Priority = 1;
+        } else
+        {
+            squadCam.Priority = 0;
+            freeCam.Priority = 1;
+        }
     }
 }
