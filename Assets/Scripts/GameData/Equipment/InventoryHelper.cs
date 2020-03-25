4a4c7c5c130b5cc3ab7657707342b2c22c9bfe69
Toeecko <rorygilliland@ameritech.net>
2020-03-25 17:02:10 -0400

    creates inventory helper skeleton, adds extra constructor for inventory item

---
+++ Assets/Scripts/GameData/Equipment/InventoryHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.GameData.Equipment
+{
+    public static class InventoryHelper
+    {
+        public static List<IInventoryItem> GetAllInventoryItems()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetAllInventoryItemsWithOne()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetWeapons()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetWeaponsWithOne()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetArmors()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetArmorsWithOne()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetSpellBooks()
+        {
+
+        }
+
+        public static List<IInventoryItem> GetSpellBooksWithOne()
+        {
+
+        }
+    }
+}
