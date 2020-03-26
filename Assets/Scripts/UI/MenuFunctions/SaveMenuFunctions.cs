a1744b4bff1b35b044cf8f33060f7cad39b1b21d
Toeecko <rorygilliland@ameritech.net>
2020-03-26 12:17:26 -0400

    adds missing / to paths

--- Assets/Scripts/UI/MenuFunctions/SaveMenuFunctions.cs
+++ Assets/Scripts/UI/MenuFunctions/SaveMenuFunctions.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using SwordAndBored.GameData.Database;
 
 namespace SwordAndBored.UI.MenuFunctions
 {
     public class SaveMenuFunctions : MonoBehaviour
     {
-        public Dropdown previousSaves;
-        public InputField fileInput;
+        public TMP_Dropdown previousSaves;
+        public TMP_InputField fileInput;
         public Button saveButton, overwriteButton;
         // Start is called before the first frame update
         void Start()
         {
-
+            List<string> oldFilenames = DatabaseManager.GetPreviousSaveNames();
+            previousSaves.AddOptions(oldFilenames);
         }
 
         // Update is called once per frame
