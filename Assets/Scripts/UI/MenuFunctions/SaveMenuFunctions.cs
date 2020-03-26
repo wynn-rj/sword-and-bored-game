b7117473ea4e7f515f806b4b7392270f68f1a0d3
Toeecko <rorygilliland@ameritech.net>
2020-03-26 11:50:51 -0400

    creates saveMenuFunctions script

---
+++ Assets/Scripts/UI/MenuFunctions/SaveMenuFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwordAndBored.UI.MenuFunctions
+{
+    public class SaveMenuFunctions : MonoBehaviour
+    {
+        public Dropdown previousSaves;
+        public InputField fileInput;
+        public Button saveButton, overwriteButton;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
