8326bdeddb06022e12b3cd11f470cb326f3be7ff
Toeecko <rorygilliland@ameritech.net>
2020-03-26 16:54:02 -0400

    reload new files

--- Assets/Scripts/UI/MenuFunctions/LoadMenuFunctions.cs
+++ Assets/Scripts/UI/MenuFunctions/LoadMenuFunctions.cs
@@ -14,9 +14,16 @@ namespace SwordAndBored.UI.MenuFunctions
         public Canvas returnCanvas;
 
         private List<string> oldFileNames;
+        private List<string> emptyOption;
         // Start is called before the first frame update
-        void Start()
+        void Awake()
         {
+            emptyOption = new List<string>
+            {
+                "----------Select A File----------"
+            };
+            previousSaves.ClearOptions();
+            previousSaves.AddOptions(emptyOption);
             oldFileNames = DatabaseManager.GetPreviousSaveNames();
             if (oldFileNames.Count > 0)
             {
@@ -43,13 +50,29 @@ namespace SwordAndBored.UI.MenuFunctions
         public void LoadButtonPressed()
         {
             DatabaseManager.LoadData(previousSaves.options[previousSaves.value].text);
-            returnCanvas.gameObject.SetActive(true);
-            gameObject.SetActive(false);
+            LeaveCanvas();
             SceneManager.LoadScene(SceneManagement.GameScenes.BASEVIEW);
         }
 
         public void CancelButtonPressed()
         {
+            LeaveCanvas();
+        }
+
+        public void LeaveCanvas()
+        {
+            previousSaves.ClearOptions();
+            previousSaves.AddOptions(emptyOption);
+            oldFileNames = DatabaseManager.GetPreviousSaveNames();
+            if (oldFileNames.Count > 0)
+            {
+                previousSaves.AddOptions(oldFileNames);
+            }
+            else
+            {
+                previousSaves.options[0].text = "No Previous Save Files";
+            }
+
             returnCanvas.gameObject.SetActive(true);
             gameObject.SetActive(false);
         }
