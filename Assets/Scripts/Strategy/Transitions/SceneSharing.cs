6adb52cb85ab61a454845cc8533dc43c95a5997f
Robert J Wynn <wynn.134@osu.edu>
2020-03-27 02:13:53 -0400

    Add new saved settings

--- Assets/Scripts/Strategy/Transitions/SceneSharing.cs
+++ Assets/Scripts/Strategy/Transitions/SceneSharing.cs
@@ -6,5 +6,9 @@ namespace SwordAndBored.Strategy.Transitions
     {
         public static Vector3 cameraPosition;
         public static int squadID = -1;
+        public static float enemyMood = 0;
+        public static float enemyProductivity = 0;
+        public static float enemyProductivityDirection = 0;
+        public static int enemyControlledLocations = 1;
     }
 }
