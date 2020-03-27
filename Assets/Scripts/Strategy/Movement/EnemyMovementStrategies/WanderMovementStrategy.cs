f910701e1d57c647071456a7dc7e6f8dfabb012c
Robert J Wynn <wynn.134@osu.edu>
2020-03-26 22:55:41 -0400

    Create enemy movement strategies

---
+++ Assets/Scripts/Strategy/Movement/EnemyMovementStrategies/WanderMovementStrategy.cs
@@ -0,0 +1,32 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using SwordAndBored.Strategy.ProceduralTerrain.Map.TileComponents;
+using SwordAndBored.Utilities.Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordAndBored.Strategy.Movement.EnemyMovementStrategies
+{
+    class WanderMovementStrategy : IEnemyMovementStrategy
+    {
+        private const int MAX_ITERATIONS = 10;
+
+        public List<IHexGridCell> GetPath(IHexGridCell currentLocation, int speed)
+        {
+            IHexGridCell newLocation;
+            int iterations = 0;
+            do {
+                Point<int> start = currentLocation.Position.GridPoint;
+                Point<int> randomOffset = new Point<int>(start.X - speed + Odds.DiceRoll(2 * speed), start.Y - speed + Odds.DiceRoll(2 * speed));
+                newLocation = currentLocation.ParentGrid[randomOffset];
+            } while (!newLocation.HasComponent<UnselectableComponent>() && iterations++ < MAX_ITERATIONS);
+            if (iterations == MAX_ITERATIONS)
+            {
+                return new List<IHexGridCell>();
+            }
+            return AStarModule.FindPath(currentLocation, newLocation);
+        }
+    }
+}
