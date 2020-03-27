43cf7525f6ba65f0287349ae72708950b0e1d817
Robert J Wynn <wynn.134@osu.edu>
2020-03-26 22:55:41 -0400

    Create enemy movement strategies

---
+++ Assets/Scripts/Strategy/Movement/EnemyMovementStrategies/IEnemyMovementStrategy.cs
@@ -0,0 +1,10 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using System.Collections.Generic;
+
+namespace SwordAndBored.Strategy.Movement.EnemyMovementStrategies
+{
+    public interface IEnemyMovementStrategy
+    {
+        public List<IHexGridCell> GetPath(IHexGridCell currentLocation, int speed);
+    }
+}
