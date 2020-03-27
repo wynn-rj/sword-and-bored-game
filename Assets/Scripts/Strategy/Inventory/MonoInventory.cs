c80ce46ee59143607725601c6aaa15c54004b265
Subash Chebolu <3270276+subashch6@users.noreply.github.com>
2020-03-26 23:42:11 -0400

    stuffy stuff with lots of stuff...sort of

---
+++ Assets/Scripts/Strategy/Inventory/MonoInventory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordAndBored.GameData.Equipment;
+
+namespace SwordAndBored.Strategy.Inventory
+{
+    public class MonoInventory : MonoBehaviour 
+    {
+        public static MonoInventory Instance;
+
+        private List<IInventoryItem> equipmentList = new List<IInventoryItem>();
+
+        private void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Debug.LogWarning("Only one MonoInventory is allowed per scene!");
+            }
+        }
+
+        void OnEnable()
+        {
+            ReadInventoryFromDatabaseAndProcess();
+        }
+
+        private void ReadInventoryFromDatabaseAndProcess()
+        {
+            equipmentList = InventoryHelper.GetAllInventoryItemsWithOne();
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
