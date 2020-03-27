d614b0b5eba7ebd43fa1fedaa7b07fc817043058
Toeecko <rorygilliland@ameritech.net>
2020-03-26 23:22:26 -0400

    adds enemy squad to ORM

---
+++ Assets/Scripts/GameData/Units/IEnemySquad.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SwordAndBored.GameData.Units
+{
+    public interface IEnemySquad : IDatabaseObject
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        int Save();
+    }
+}
