6ef83c953c8262c8649ccd2cd2f961811084be62
Robert J Wynn <wynn.134@osu.edu>
2020-03-27 15:51:27 -0400

    Add gizmo for displaying where creature is going

---
+++ Assets/Scripts/Strategy/Movement/CreatureGoalGizmo.cs
@@ -0,0 +1,34 @@
+﻿using SwordAndBored.Strategy.ProceduralTerrain.Map.Grid.Cells;
+using System.Reflection;
+using UnityEngine;
+
+namespace SwordAndBored.Strategy.Movement
+{
+    class CreatureGoalGizmo : MonoBehaviour
+    {
+        [SerializeField] private CreatureMovementController creature;
+        [SerializeField] private Vector3 boxSize = new Vector3(2, 20, 2);
+        private object creaturePath;
+
+
+        private void Start()
+        {
+            creaturePath = creature.GetType().GetField("path", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(creature);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (creaturePath == null)
+            {
+                 creaturePath = creature.GetType().GetField("path", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(creature);
+            }
+            IHexGridCell destination = (IHexGridCell)(creaturePath.GetType().GetProperty("Last", BindingFlags.Public | BindingFlags.Instance).GetValue(creaturePath));
+            if (destination == null)
+            {
+                destination = creature.Location;
+            }
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(destination.Position.CenterAsVector3(boxSize.y / 2), boxSize);
+        }
+    }
+}
