dccbcdfafdf65831f6f525989932cfc47af97926
Robert J Wynn <wynn.134@osu.edu>
2020-04-17 14:11:59 -0400

    Create battle resolver script

---
+++ Assets/Scripts/Strategy/Transitions/BattleResolver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using SwordAndBored.SceneManagement;
+using SwordAndBored.GameData.Units;
+
+namespace SwordAndBored.Strategy.Transitions
+{
+    class BattleResolver : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (SceneSharing.squadID == -1)
+            {
+                return;
+            }
+
+            ISquad squad = new Squad(SceneSharing.squadID);
+            if (GameScenes.battleWin)
+            {
+                foreach (EnemySquad enemy in EnemySquad.GetAllEnemySquads())
+                {
+                    if (enemy.X == squad.X && enemy.Y == squad.Y)
+                    {
+                        enemy.Delete();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                squad.Delete();
+            }
+
+            SceneSharing.squadID = -1;
+        }
+    }
+}
