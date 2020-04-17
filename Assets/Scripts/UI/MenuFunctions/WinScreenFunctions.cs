c21dd042b85e9a31e7001494a346eb529a1151c8
Robert J Wynn <wynn.134@osu.edu>
2020-04-17 15:43:15 -0400

    Add win screen functions

---
+++ Assets/Scripts/UI/MenuFunctions/WinScreenFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+using SwordAndBored.Strategy.Transitions;
+
+namespace SwordAndBored.UI.MenuFunctions
+{
+    public class WinScreenFunctions : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+        private void Awake()
+        {
+            if (SceneSharing.playerWonGame)
+            {
+                text.text = "You WIN!";
+            }
+            else
+            {
+                text.text = "You LOSE!";
+            }
+        }
+    }
+}
