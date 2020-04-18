8cce70d6ee8cff2efabb6e17127ba66543f7a7d9
Toeecko <rorygilliland@ameritech.net>
2020-04-17 21:46:09 -0400

    Adds buttons for win and lose panel to return to world map

---
+++ Assets/Scripts/UI/Battlefield/LosePanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SwordAndBored.SceneManagement;
+namespace SwordAndBored.UI.Battlefield
+{
+    public class LosePanel : MonoBehaviour
+    {
+        public void ClickWorldMap()
+        {
+            SceneManager.LoadSceneAsync(GameScenes.STRATEGYMAP);
+        }
+    }
+}
