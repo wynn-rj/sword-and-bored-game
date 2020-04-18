6668236f892ac3c93362f84b997233adc0e91898
cthomas68 <c.l.thomas68@gmail.com>
2020-04-18 16:06:54 -0400

    Fix buying units bug

--- Assets/Scripts/Strategy/BaseManagement/Units/RoleEntryDisplay.cs
+++ Assets/Scripts/Strategy/BaseManagement/Units/RoleEntryDisplay.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using SwordAndBored.Strategy.GameResources;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace SwordAndBored.Strategy.BaseManagement.Units
@@ -10,6 +11,8 @@ namespace SwordAndBored.Strategy.BaseManagement.Units
         public Text entryDescription;
         public Text entryCost;
         public Image entryImage;
+        public Button trainButton;
+        [SerializeField] private ResourceManager resourceManager;
 
         void Start()
         {
@@ -17,6 +20,18 @@ namespace SwordAndBored.Strategy.BaseManagement.Units
             entryDescription.text = roleEntry.roleDescription;
             entryImage.sprite = roleEntry.roleArtwork;
             entryCost.text = "Cost:" + roleEntry.roleCost.ToString();
+        }
+
+        private void Update()
+        {
+            if (resourceManager.CanAffordPurchase(new Payment("Gold", roleEntry.roleCost)))
+            {
+                trainButton.interactable = true;
+            }
+            else
+            {
+                trainButton.interactable = false;
+            }
         }
     }
 }
