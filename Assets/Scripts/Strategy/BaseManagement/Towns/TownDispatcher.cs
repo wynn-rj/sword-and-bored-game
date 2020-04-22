c96d4dbc4b83c71319f87e1e283404bc52554ac3
cthomas68 <c.l.thomas68@gmail.com>
2020-04-21 22:27:52 -0400

    Fix

--- Assets/Scripts/Strategy/BaseManagement/Towns/TownDispatcher.cs
+++ Assets/Scripts/Strategy/BaseManagement/Towns/TownDispatcher.cs
@@ -35,7 +35,7 @@ namespace SwordAndBored.StrategyView.BaseManagement.Towns
 
             foreach (ITown town in townsList)
             {
-                if (town.PlayerOwned)
+                if (town.PlayerOwned && town.Name != "Player Base")
                 {
                     ownedTowns.Add(town);
                     townEntriesList.Add(CreateTownEntry(town));
